1b8e7f34822097b4b220e77aa4a5e0f6a1a67423
Sergei Maertens <sergei@maykinmedia.nl>
2013-10-15 18:03:32 +0200

    Extra ember library, verified tests
    Conflicts:
    	static/global/js/vendor/ember-data-drf2-adapter.js

--- bluebottle/bluebottle_utils/serializers.py
+++ bluebottle/bluebottle_utils/serializers.py
@@ -1,4 +1,8 @@
+from django.core.exceptions import FieldError
+from django.template.defaultfilters import truncatechars
+
 from rest_framework import serializers
+from taggit.managers import _TaggableManager
 
 from bluebottle.bluebottle_utils.validators import validate_postal_code
 from bluebottle.bluebottle_utils.models import Address
@@ -22,3 +26,63 @@ class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ('id', 'line1', 'line2', 'city', 'state', 'country', 'postal_code')
+
+
+
+class MetaModelSerializer(serializers.ModelSerializer):
+    """ Serializer which fills meta data based on model attributes """
+
+    page_title_field_name = 'title'
+    page_description_field_name = 'projectplan__description'
+    page_keywords_field_name = 'projectplan__tags'
+
+    def __init__(self, *args, **kwargs):
+        """ Call the default __init__ and then add fields with meta-data """
+        super(MetaModelSerializer, self).__init__(*args, **kwargs)
+
+        # if the field names don't collide, add the fields
+        if not self.fields.get('page_title', None):
+            self.fields['page_title'] = serializers.SerializerMethodField('get_page_title')
+
+        if not self.fields.get('page_description', None):
+            self.fields['page_description'] = serializers.SerializerMethodField('get_page_description')
+
+        if not self.fields.get('page_keywords', None):
+            self.fields['page_keywords'] = serializers.SerializerMethodField('get_page_keywords')
+
+    def _get_field(self, obj, field_name):
+        """ Allow traversing the relations tree for fields """
+        attrs = field_name.split('__')
+        
+        field = obj
+        for attr in attrs:
+            try:
+                field = getattr(field, attr)
+            except AttributeError:
+                raise FieldError('Unknown field "%s" in "%s"' % (attr, self.page_keywords_field_name))
+        return field
+
+    def get_page_title(self, obj):
+        """ Get the page title based on a model attribute """
+        title = self._get_field(obj, self.page_title_field_name)
+        return title
+
+    def get_page_description(self, obj):
+        """ Get the page description based on a model attribute """
+        desc = self._get_field(obj, self.page_description_field_name)
+        return truncatechars(desc, 150)
+
+    def get_page_keywords(self, obj):
+        field = self._get_field(obj, self.page_keywords_field_name)
+
+        # we're dealing with taggit.Tag's here
+        if isinstance(field, _TaggableManager):
+            keywords = [tag.name.lower() for tag in field.all()]
+        else:
+            # try to split the keywords
+            try:
+                keywords = field.lower().split()
+            except AttributeError:
+                keywords = ''
+        
+        return ", ".join(keywords)

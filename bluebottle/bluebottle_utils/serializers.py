184a6a55510acbeb4791fae57e4e36baedab7c7b
Sergei Maertens <sergei@maykinmedia.nl>
2013-10-23 12:28:53 +0200

    Slightly modified serializers to be a bit more flexible

--- bluebottle/bluebottle_utils/serializers.py
+++ bluebottle/bluebottle_utils/serializers.py
@@ -10,6 +10,18 @@ from bluebottle.bluebottle_drf2.serializers import ImageSerializer
 from bluebottle.bluebottle_utils.validators import validate_postal_code
 from bluebottle.bluebottle_utils.models import Address
 
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    """ Used to strip HTML tags for meta fields (e.g. description) """
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
 
 class AddressSerializer(serializers.ModelSerializer):
 
@@ -37,8 +49,51 @@ class MetaField(serializers.Field):
     Serializer field which fills meta data based on model attributes
     Init with `field = None` to disable the field.
 
-    To override the attribute, provide a (callable) keyword argument,
-    see tests for examples.
+    Callables need to accept **kwargs, as the request is sent by default.
+
+    Usage example:
+
+    #models.py
+    class Example(models.Model):
+        title = models.CharField(max_length=50)
+
+        def get_description(self, **kwargs):
+            return self.title + ' (description)'
+
+    
+    # serializers.py
+    class ExampleSerializer(serializers.ModelSerializer):
+
+        meta_data = MetaField(
+                title = 'title',
+                description = 'get_description',
+                keywords = None,
+                image_source = None
+            )
+
+        class Meta:
+            model = Example
+            fields = ('title', 'meta_data')
+
+
+    This returns API JSON as:
+
+    json = {
+        'title': 'Sample title',
+        'meta_data': {
+            'title': 'Sample title',
+            'description': 'Sample title (description)',
+            'keywords': null,
+            'image': null
+            }
+        }
+    }
+
+    When image_source is provided, you get a JSON object with keys 'large',
+    'small', 'full' and 'square'.
+
+    # TODO: unittests
+
     """
 
     def __init__(self, 
@@ -70,27 +125,30 @@ class MetaField(serializers.Field):
             'keywords': None,
         }
 
+        # get the meta title from object callable or object property
         if self.title:
             title = self._get_callable(obj, self.title)
             if title is None:
                 title = self._get_field(obj, self.title)
             value['title'] = title
                 
-
+        # get the meta description from object callable or object property
         if self.description:
             description = self._get_callable(obj, self.description)
             if description is None:
                 description = self._get_field(obj, self.description)
-                description = truncatechars(description, 150)
+                description = truncatechars(description, 200)
             value['description'] = description
 
-
+        # keywords: either from object callable or property, if property,
+        # check if we are referring to taggit tags.
         if self.keywords:
             keywords = self._get_callable(obj, self.keywords)
             if keywords is None:
                 # Get the keywords
                 keywords = self._get_field(obj, self.keywords)
                 
+                # usually tags as keywords
                 if isinstance(keywords, _TaggableManager):
                     keywords = [tag.name.lower() for tag in keywords.all()]
                 else:
@@ -136,7 +194,8 @@ class MetaField(serializers.Field):
         try:
             _attr = getattr(obj, attr)
             if callable(_attr): 
-                return _attr() # Call it, and return the result
-        except AttributeError: # not a model attribute
+                # return _attr() # Call it, and return the result
+                return _attr(request=self.context['request'])
+        except AttributeError: # not a model/object attribute
             pass
         return None

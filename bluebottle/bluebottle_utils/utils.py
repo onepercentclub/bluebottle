14477a15cc6d5fde0d5db49bff162b9b4b3840a7
Sergei Maertens <sergei@maykinmedia.nl>
2013-11-05 18:43:35 +0100

    Added a function to clean strings intended for hashtags

--- bluebottle/bluebottle_utils/utils.py
+++ bluebottle/bluebottle_utils/utils.py
@@ -19,3 +19,21 @@ def set_author_editor_ip(request, obj):
     else:
         obj.editor = request.user
     obj.ip_address = get_client_ip(request)
+
+
+def clean_for_hashtag(text):
+    """
+    Strip non alphanumeric charachters.
+
+    Sometimes, text bits are made up of two parts, sepated by a slash. Split
+    those into two tags. Otherwise, join the parts separated by a space.
+    """
+    tags = []
+    bits = text.split('/')
+    for bit in bits:
+        # keep the alphanumeric bits and capitalize the first letter
+        _bits = [_bit.title() for _bit in bit.split() if _bit.isalnum()]
+        tag = "".join(_bits)
+        tags.append(tag)
+
+    return " #".join(tags)

64dcdc302fad64a72ae032146576b54f3d6898c3
José L. Patiño <jose.lpa@gmail.com>
2013-12-06 16:45:32 +0100

    Django version upgraded (1.5.5).

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ changes = ''
 install_requires = [
 #    'Babel==1.3',
 #    'BeautifulSoup==3.2.1',
-    'Django==1.5.4',
+    'Django==1.5.5',
 #    'Jinja2==2.7',
     'Pillow==2.1.0',
     'South==0.8.1',

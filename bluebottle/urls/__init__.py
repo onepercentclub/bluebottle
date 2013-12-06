7c04ca1fdb2347a10f868c8653f4d36caaad9eb7
José L. Patiño <jose.lpa@gmail.com>
2013-12-06 17:53:08 +0100

    URLs definitions splitted into two files.

---
+++ bluebottle/urls/__init__.py
@@ -0,0 +1,5 @@
+from core import urlpatterns
+from i18n_urls import i18n_urlpatterns
+
+
+urlpatterns += i18n_urlpatterns
--- bluebottle/urls/core.py
+++ bluebottle/urls/core.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
-from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
-
-
-from .views import HomeView
 
 
 urlpatterns = patterns('',
@@ -18,30 +13,6 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('loginas.views',
     url(r"^login/user/(?P<user_id>.+)/$", "user_login", name="loginas-user-login"),
-)
-
-
-urlpatterns += i18n_patterns('',
-
-    # account login/logout, password reset, and password change
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-
-    # These URL's will be automatically prefixed with the locale (e.g. '/nl/')
-    url(r'^$', HomeView.as_view(), name='home'),
-
-    # Django Admin, docs and password reset
-    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
-    url(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-
-    # Other modules that need URLs exposed
-    url(r'^admin/utils/taggit-autocomplete/', include('taggit_autocomplete_modified.urls')),
-    url(r'^admin/utils/tinymce/', include('tinymce.urls')),
-    url(r'^admin/utils/admintools/', include('admin_tools.urls')),
-
-    # account login/logout, password reset, and password change
-    url(r'^accounts/', include('django.contrib.auth.urls', namespace='accounts')),
 )
 
 js_info_dict = {

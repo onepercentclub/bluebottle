6bb4c0db1778b2aceb17eede00fb9f9eeed4cfba
José L. Patiño <jose.lpa@gmail.com>
2013-12-12 14:28:17 +0100

    Temporary fix to solve internationalization in `App.Page`.

--- bluebottle/pages/static/js/bluebottle/pages/models.js
+++ bluebottle/pages/static/js/bluebottle/pages/models.js
@@ -2,7 +2,7 @@
 
 
 App.Page = DS.Model.extend({
-    url: 'pages',
+    url: 'pages/en/pages', // TODO: Temporary fix. Solve it to handle internationalization.
     title: DS.attr('string'),
     body: DS.attr('string')
 });

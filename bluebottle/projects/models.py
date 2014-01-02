76846e11b251dc1e3a107a39673d945d8c88df9f
Loek van Gent <loek@1procentclub.nl>
2014-01-02 10:39:50 +0100

    Add dynamic fields to projects

--- bluebottle/projects/models.py
+++ bluebottle/projects/models.py
@@ -29,6 +29,28 @@ class ProjectTheme(models.Model):
         verbose_name_plural = _("project themes")
 
 
+class ProjectPhase(models.Model):
+    """ Phase of a project """
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=400, blank=True)
+    sequence = models.IntegerField(unique=True, help_text=_("For ordering phases."))
+
+    active = models.BooleanField(default=True, help_text=_("Whether this phase is in use or has been discarded."))
+
+    editable = models.BooleanField(default=True,
+                                   help_text=_("Whether the project owner can change the details of the project."))
+
+    viewable = models.BooleanField(default=True,
+                                   help_text=_("Whether this phase, and projects in it show up at the website"))
+
+    def Meta(self):
+        ordering = ['sequence']
+
+    def __unicode__(self):
+        return "{0} - {1}".format(self.sequence,  self.name)
+
+
 class ProjectManager(models.Manager):
 
     def order_by(self, field):
@@ -163,6 +185,48 @@ class Project(models.Model):
         if not self.phase:
             self.phase = 'plan-new'
         super(Project, self).save(*args, **kwargs)
+
+
+class ProjectDetailField(models.Model):
+
+    class Types(DjangoChoices):
+        text = ChoiceItem('text', label=_("Text field (one line)"))
+        textarea = ChoiceItem('textarea', label=_("Text area (multiple lines)"))
+        checkbox = ChoiceItem('checkbox', label=_("Checkbox"))
+        radio = ChoiceItem('radio', label=_("Radio buttons"))
+        custom = ChoiceItem('custom', label=_("Custom field"))
+
+    name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    description = models.CharField(max_length=300, blank=True)
+    type = models.CharField(max_length=100, choices=Types.choices)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ProjectDetailFieldValue(models.Model):
+
+    field = models.ForeignKey('ProjectDetailField')
+    value = models.CharField(max_length=200)
+    text = models.CharField(max_length=200, blank=True)
+
+
+class ProjectDetailFieldAttribute(models.Model):
+
+    field = models.ForeignKey('ProjectDetailField')
+    attribute = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+
+
+class ProjectDetail(models.Model):
+
+    project = models.ForeignKey(Project)
+    field = models.ForeignKey(ProjectDetailField)
+    value = models.TextField()
+
+    class Meta:
+        unique_together = ('project', 'field')
 
 
 class ProjectBudgetLine(models.Model):

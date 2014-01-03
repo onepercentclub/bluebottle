275c8ea944280bda4b2f2c502d3ffd6389512d1c
José L. Patiño <jose.lpa@gmail.com>
2014-01-03 13:05:46 +0100

    `ProjectPreviewList` view test case.

--- bluebottle/projects/tests/test_api.py
+++ bluebottle/projects/tests/test_api.py
@@ -74,3 +74,35 @@ class TestProjectDetail(TestCase):
         self.assertIn('meta_data', data)
         self.assertIn('owner', data)
         self.assertIn('phase', data)
+
+
+class TestProjectPreviewList(TestCase):
+    """
+    Test case for the ``ProjectPreviewList API view.
+
+    Endpoint: /api/projects/previews
+    """
+    def setUp(self):
+        self.user = BlueBottleUserFactory.create()
+        self.project_1 = ProjectFactory.create(owner=self.user)
+        self.project_2 = ProjectFactory.create(owner=self.user)
+        self.project_3 = ProjectFactory.create(owner=self.user)
+
+    def test_api_project_preview_list_endpoint(self):
+        """
+        Test the API endpoint for Project preview list.
+        """
+        response = self.client.get(reverse('project_preview_list'))
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+
+        self.assertEqual(data['count'], 3)
+
+        for item in data['results']:
+            self.assertIn('id', item)
+            self.assertIn('title', item)
+            self.assertIn('image', item)
+            self.assertIn('phase', item)
+            self.assertIn('country', item)

c81fba559344012dc5ce698b1a9bdb1de58ec3c1
Loek van Gent <loek@1procentclub.nl>
2014-01-14 00:18:57 +0100

    Add news

---
+++ bluebottle/news/views.py
@@ -0,0 +1,38 @@
+from rest_framework import generics
+from .models import NewsItem
+from .serializers import NewsItemSerializer, NewsItemPreviewSerializer
+
+
+class NewsItemPreviewList(generics.ListAPIView):
+    model = NewsItem
+    serializer_class = NewsItemPreviewSerializer
+    paginate_by = 5
+    filter_fields = ('language', )
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(NewsItemPreviewList, self).get_queryset()
+        qs = qs.published()
+        return qs
+
+
+class NewsItemList(generics.ListAPIView):
+    model = NewsItem
+    serializer_class = NewsItemSerializer
+    paginate_by = 5
+    filter_fields = ('language', )
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(NewsItemList, self).get_queryset()
+        qs = qs.published()
+        return qs
+
+
+class NewsItemDetail(generics.RetrieveAPIView):
+    model = NewsItem
+    serializer_class = NewsItemSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(NewsItemDetail, self).get_queryset()
+        qs = qs.published()
+        return qs
+

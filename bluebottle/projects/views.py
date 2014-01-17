5b6b57837395ceaf52704db123ed1c2ead92235b
Loek van Gent <loek@1procentclub.nl>
2014-01-17 16:29:44 +0100

    Fix some issues with projects

--- bluebottle/projects/views.py
+++ bluebottle/projects/views.py
@@ -137,6 +137,7 @@ class ManageProjectList(generics.ListCreateAPIView):
         return queryset
 
     def pre_save(self, obj):
+        obj.status = ProjectPhase.objects.order_by('sequence').all()[0]
         obj.owner = self.request.user
 
 

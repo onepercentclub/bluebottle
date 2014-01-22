34bdf325c4f35eab33338785ed5ff7cb69525753
Joeri Bekker <joeri@maykinmedia.nl>
2014-01-22 12:37:18 +0100

    Fixed current and added generic task and project dashboard modules.

--- bluebottle/projects/dashboard.py
+++ bluebottle/projects/dashboard.py
@@ -1,63 +1,74 @@
 from django.utils.translation import ugettext_lazy as _
 
 from admin_tools.dashboard.modules import DashboardModule
-
 from . import get_project_model
+
 
 PROJECT_MODEL = get_project_model()
 
 
-class RecentProjects(DashboardModule):
+class ProjectModule(DashboardModule):
+    """
+    Generic project module for the django admin tools dashboard.
+    """
+    title = _('Projects')
+    template = 'admin_tools/dashboard/project_module.html'
+    limit = 10
+
+    def __init__(self, title=None, limit=10, filter_kwargs=None, order_by=None, **kwargs):
+        if order_by is None:
+            order_by = '-created'
+        self.order_by = order_by
+
+        if filter_kwargs is None:
+            filter_kwargs = {}
+        self.filter_kwargs = filter_kwargs
+
+        kwargs.update({'limit': limit})
+        super(ProjectModule, self).__init__(title, **kwargs)
+
+    def init_with_context(self, context):
+        qs = PROJECT_MODEL.objects.filter(**self.filter_kwargs).order_by(self.order_by)
+
+        self.children = qs[:self.limit]
+        if not len(self.children):
+            self.pre_content = _('No projects found.')
+        self._initialized = True
+
+
+class RecentProjects(ProjectModule):
+    """
+    Simple implementation of ``ProjectModule`` to show the most recent projects.
+    """
     title = _('Recently Created Projects')
     template = 'admin_tools/dashboard/recent_projects.html'
-    limit = 10
-
-    def __init__(self, title=None, limit=10, **kwargs):
-        kwargs.update({'limit': limit})
-        super(RecentProjects, self).__init__(title, **kwargs)
-
-    def init_with_context(self, context):
-        qs = PROJECT_MODEL.objects.order_by('-created')
-        self.children = qs[:self.limit]
-        if not len(self.children):
-            self.pre_content = _('No recent projects.')
-        self._initialized = True
 
 
-class SubmittedProjects(DashboardModule):
+class SubmittedProjects(ProjectModule):
+    """
+    Simple implementation of ``ProjectModule``
+    TODO: This will not work for all projects. Left here for backwards-compat.
+    """
     title = _('Recently Submitted Plans')
     template = 'admin_tools/dashboard/submitted_plans.html'
-    limit = 10
 
-    def __init__(self, title=None, limit=10, **kwargs):
-        kwargs.update({'limit': limit})
-        super(SubmittedProjects, self).__init__(title, **kwargs)
-
-    def init_with_context(self, context):
-        qs = PROJECT_MODEL.objects.order_by('created')
-        qs = qs.filter(phase='plan-submitted')
-
-        self.children = qs[:self.limit]
-        if not len(self.children):
-            self.pre_content = _('No submitted plans.')
-        self._initialized = True
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            'filter_kwargs': {'status__name': 'Plan - Submitted'}
+        })
+        super(SubmittedProjects, self).__init__(*args, **kwargs)
 
 
-class StartedCampaigns(DashboardModule):
+class StartedCampaigns(ProjectModule):
+    """
+    Simple implementation of ``ProjectModule``
+    TODO: This will not work for all projects. Left here for backwards-compat.
+    """
     title = _('Recently Started Campaigns')
     template = 'admin_tools/dashboard/started_campaigns.html'
-    limit = 10
 
-    def __init__(self, title=None, limit=10, **kwargs):
-        kwargs.update({'limit': limit})
-        super(StartedCampaigns, self).__init__(title, **kwargs)
-
-    def init_with_context(self, context):
-        qs = PROJECT_MODEL.objects.order_by('-created')
-        qs = qs.filter(phase='campaign-running')
-
-        self.children = qs[:self.limit]
-        if not len(self.children):
-            self.pre_content = _('No campaigns.')
-        self._initialized = True
-
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            'filter_kwargs': {'status__name': 'Campaign'}
+        })
+        super(StartedCampaigns, self).__init__(*args, **kwargs)
--- bluebottle/tasks/dashboard.py
+++ bluebottle/tasks/dashboard.py
@@ -2,26 +2,42 @@ from admin_tools.dashboard.modules import DashboardModule
 from django.utils.translation import ugettext_lazy as _
 from bluebottle.tasks import get_task_model
 
+
 BB_TASK_MODEL = get_task_model()
 
-class RecentTasks(DashboardModule):
-    """
-    """
-    title = _('Recently Created Tasks')
-    template = 'admin_tools/dashboard/recent_tasks.html'
-    limit = 10
-    include_list = None
-    exclude_list = None
 
-    def __init__(self, title=None, limit=10, **kwargs):
+class TaskModule(DashboardModule):
+    """
+    Generic task module for the django admin tools dashboard.
+    """
+    title = _('Tasks')
+    template = 'admin_tools/dashboard/task_module.html'
+    limit = 10
+
+    def __init__(self, title=None, limit=10, filter_kwargs=None, order_by=None, **kwargs):
+        if order_by is None:
+            order_by = '-created'
+        self.order_by = order_by
+
+        if filter_kwargs is None:
+            filter_kwargs = {}
+        self.filter_kwargs = filter_kwargs
+
         kwargs.update({'limit': limit})
-        super(RecentTasks, self).__init__(title, **kwargs)
+        super(TaskModule, self).__init__(title, **kwargs)
 
     def init_with_context(self, context):
-        qs = BB_TASK_MODEL.objects.order_by('-created')
+        qs = BB_TASK_MODEL.objects.filter(**self.filter_kwargs).order_by(self.order_by)
 
         self.children = qs[:self.limit]
         if not len(self.children):
-            self.pre_content = _('No recent projects.')
+            self.pre_content = _('No tasks found.')
         self._initialized = True
 
+
+class RecentTasks(TaskModule):
+    """
+    Simple implementation of ``TaskModule`` to keep existing BlueBottle projects working.
+    """
+    title = _('Recently Created Tasks')
+    template = 'admin_tools/dashboard/recent_tasks.html'

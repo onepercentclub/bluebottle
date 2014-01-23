2ba54dfd0db0bf1ff5761af8c2c394d8d63560e2
Loek van Gent <loek@1procentclub.nl>
2014-01-23 13:56:42 +0100

    Update comment about fix

--- bluebottle/common/static/js/vendor/ember-data-drf2-adapter.js
+++ bluebottle/common/static/js/vendor/ember-data-drf2-adapter.js
@@ -371,7 +371,7 @@ DS.DRF2Adapter = DS.RESTAdapter.extend({
         Taken from Stackoverflow to mark changed hasMany Ember relations as dirty
     */
     dirtyRecordsForHasManyChange: function(dirtySet, record, relationship) {
-        // FIXME: Dirty trick to keep things working for adding TaskkMember
+        // FIXME: Dirty trick to keep things working for adding TaskMember / WallPostPhotos
         if(record.constructor.toString() == 'App.User') {
             relationship.childReference.parent = relationship.parentReference;
             this._dirtyTree(dirtySet, record);

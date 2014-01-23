76bb25ba7204c8aef0b44c1b80b622b4c7d4fa67
Aksel <aksel.ethem@gmail.com>
2014-01-23 11:50:10 +0100

    added another hack to Ember hasMany func for saving documents in legal

--- bluebottle/common/static/js/vendor/ember-data-drf2-adapter.js
+++ bluebottle/common/static/js/vendor/ember-data-drf2-adapter.js
@@ -372,7 +372,8 @@ DS.DRF2Adapter = DS.RESTAdapter.extend({
     */
     dirtyRecordsForHasManyChange: function(dirtySet, record, relationship) {
         // FIXME: Dirty trick to keep things working for adding TaskMember
-        if(record.constructor.toString() != 'App.Task') {
+        console.log(record.constructor.toString());
+        if(record.constructor.toString() != 'App.Task' && record.constructor.toString() != 'App.MyOrganization') {
             relationship.childReference.parent = relationship.parentReference;
             this._dirtyTree(dirtySet, record);
         }

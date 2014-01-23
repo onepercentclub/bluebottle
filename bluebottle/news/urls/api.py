8a700b220a168d0058739ff3a65aba717bf98f65
José L. Patiño <jose.lpa@gmail.com>
2014-01-23 18:08:17 +0100

    `news` app URLs reviewed.

--- bluebottle/news/urls/api.py
+++ bluebottle/news/urls/api.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, url
-from surlex.dj import surl
+
 from ..views import NewsItemPreviewList, NewsItemList, NewsItemDetail
 
-urlpatterns = patterns('',
-
+urlpatterns = patterns(
+    '',
     # News Items
-    url(r'^preview-items/$', NewsItemPreviewList.as_view(), name='news-preview-list'),
-    url(r'^items/$', NewsItemList.as_view(), name='news-item-list'),
-    surl(r'^items/<slug:s>$', NewsItemDetail.as_view(), name='news-post-detail'),
-
+    url(r'^preview-items/$', NewsItemPreviewList.as_view(), name='news_preview_list'),
+    url(r'^items/$', NewsItemList.as_view(), name='news_item_list'),
+    url(r'^items/(?P<slug>[-\w]+)$', NewsItemDetail.as_view(), name='news_post_detail'),
 )

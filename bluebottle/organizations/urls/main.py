939d9a2c7abe2e9e595ca6de389d9716509e5aa2
José L. Patiño <jose.lpa@gmail.com>
2014-01-24 13:54:46 +0100

    `utils` URLs fixed.

--- bluebottle/organizations/urls/main.py
+++ bluebottle/organizations/urls/main.py
@@ -5,5 +5,5 @@ from ..views import RegistrationDocumentDownloadView
 urlpatterns = patterns(
     '',
     url(r'^organizations/(?P<pk>\d+)/$', RegistrationDocumentDownloadView.as_view(),
-        name='organization-registration-download'),
+        name='organization_registration_download'),
 )

f97f8d47aad23f8c9d104229c444beff78e05a25
José L. Patiño <jose.lpa@gmail.com>
2014-01-27 11:42:15 +0100

    `Organization` model made generic (+ migrations).

--- bluebottle/organizations/models.py
+++ bluebottle/organizations/models.py
@@ -1,105 +1,103 @@
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
+from django.core.files.storage import FileSystemStorage
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.translation import ugettext as _
 from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext as _
 
 from django_extensions.db.fields import ModificationDateTimeField, CreationDateTimeField
-from django_iban.fields import IBANField, SWIFTBICField
 from djchoices import DjangoChoices, ChoiceItem
 from taggit_autocomplete_modified.managers import TaggableManagerAutocomplete as TaggableManager
 
-from bluebottle.utils.models import Address
-from django.core.files.storage import FileSystemStorage
 
-
-class Organization(models.Model):
+class BaseOrganization(models.Model):
     """
     Organizations can run Projects. An organization has one or more members.
     """
-    name = models.CharField(_("name"), max_length=255)
-    slug = models.SlugField(_("slug"), max_length=100, unique=True)
+    name = models.CharField(_('name'), max_length=255)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
 
-    description = models.TextField(_("description"), blank=True)
-    legal_status = models.TextField(_("legal status"), blank=True, help_text=_("The legal status of the organization (e.g. Foundation)."))
-    phone_number = models.CharField(_("phone number"), max_length=40, blank=True)
-    website = models.URLField(_("website"), blank=True)
+    created = CreationDateTimeField(_('created'))
+    updated = ModificationDateTimeField(_('updated'))
+    deleted = models.DateTimeField(_('deleted'), null=True, blank=True)
 
-    email = models.EmailField(blank=True)
-    twitter = models.CharField(_("twitter"), max_length=255, blank=True)
-    facebook = models.CharField(_("facebook"), max_length=255, blank=True)
-    skype = models.CharField(_("skype"), max_length=255, blank=True)
-
-    legal_status = models.CharField(max_length=255, blank=True)
-    registration = models.FileField(upload_to='organizations/registrations', storage=FileSystemStorage(location=settings.PRIVATE_MEDIA_ROOT), null=True, blank=True)
-
-    created = CreationDateTimeField(_("created"))
-    updated = ModificationDateTimeField(_("updated"))
-    deleted = models.DateTimeField(_("deleted"), null=True, blank=True)
-
-    partner_organizations = models.TextField(_("partner organizations"), blank=True)
-
-    account_bank_name = models.CharField(_("account bank name"), max_length=255, blank=True)
-    account_bank_address = models.CharField(_("account bank address"), max_length=255, blank=True)
-    account_bank_country = models.ForeignKey('geo.Country', blank=True, null=True, related_name="account_bank_country")
-    account_iban = IBANField(_("account IBAN"), blank=True)
-    account_bic = SWIFTBICField(_("account SWIFT-BIC"), blank=True)
-    account_number = models.CharField(_("account number"), max_length=255, blank=True)
-    account_name = models.CharField(_("account name"), max_length=255, blank=True)
-    account_city = models.CharField(_("account city"), max_length=255, blank=True)
-    account_other = models.CharField(_("account information that doesn't fit in the other field"), max_length=255, blank=True)
+    partner_organizations = models.TextField(_('partner organizations'), blank=True)
 
     # Address
     address_line1 = models.CharField(max_length=100, blank=True)
     address_line2 = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100, blank=True)
     state = models.CharField(max_length=100, blank=True)
-    country = models.ForeignKey('geo.Country', blank=True, null=True, related_name="country")
+    country = models.ForeignKey('geo.Country', blank=True, null=True, related_name='country')
     postal_code = models.CharField(max_length=20, blank=True)
 
-    tags = TaggableManager(blank=True, verbose_name=_("tags"))
+    # Contact
+    phone_number = models.CharField(_('phone number'), max_length=40, blank=True)
+    website = models.URLField(_('website'), blank=True)
+
+    email = models.EmailField(blank=True)
+    twitter = models.CharField(_('twitter'), max_length=255, blank=True)
+    facebook = models.CharField(_('facebook'), max_length=255, blank=True)
+    skype = models.CharField(_('skype'), max_length=255, blank=True)
+
+    tags = TaggableManager(blank=True, verbose_name=_('tags'))
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+        verbose_name = _('organization')
+        verbose_name_plural = _('organizations')
 
     def __unicode__(self):
         return self.name
-
-    class Meta:
-        ordering = ['name']
-        verbose_name = _("organization")
-        verbose_name_plural = _("organizations")
 
     def full_clean(self, exclude=None):
         if not self.slug:
             original_slug = slugify(self.name)
             slug = original_slug
-            next = 2
-            while not slug or Organization.objects.filter(slug=slug):
-                slug = '%s%s%s' % (original_slug, '-', next)
-                next += 1
+            next_slug = 2
+            while not slug or self.__class__.objects.filter(slug=slug):
+                slug = '{0}{1}{2}'.format(original_slug, '-', next_slug)
+                next_slug += 1
             self.slug = slug
+
+
+class Organization(BaseOrganization):
+    """
+    Standard Project model. If there are any extra fields required, provide
+    your own Project model by extending ``BaseProject``.
+    """
+    description = models.TextField(_('description'), blank=True)
+    legal_status = models.TextField(
+        _('legal status'), blank=True, help_text=_('The legal status of the organization (e.g. Foundation).'))
+
+    class Meta:
+        swappable = 'ORGANIZATIONS_ORGANIZATION_MODEL'
 
 
 class OrganizationMember(models.Model):
     """ Members from a Organization """
 
     class MemberFunctions(DjangoChoices):
-        owner = ChoiceItem('owner', label=_("Owner"))
-        editor = ChoiceItem('editor', label=_("Editor"))
+        owner = ChoiceItem('owner', label=_('Owner'))
+        editor = ChoiceItem('editor', label=_('Editor'))
 
-    organization = models.ForeignKey(Organization, verbose_name=_("organization"))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("user"))
-    function = models.CharField(_("function"), max_length=20, choices=MemberFunctions.choices)
+    organization = models.ForeignKey(Organization, verbose_name=_('organization'))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'))
+    function = models.CharField(_('function'), max_length=20, choices=MemberFunctions.choices)
 
     class Meta:
-        verbose_name = _("organization member")
-        verbose_name_plural = _("organization members")
+        verbose_name = _('organization member')
+        verbose_name_plural = _('organization members')
 
 
 class OrganizationDocument(models.Model):
     """ Document for an Organization """
 
-    organization = models.ForeignKey(Organization, verbose_name=_("organization"))
-    file = models.FileField(upload_to='organizations/documents', storage=FileSystemStorage(location=settings.PRIVATE_MEDIA_ROOT))
+    organization = models.ForeignKey(Organization, verbose_name=_('organization'))
+    file = models.FileField(
+        upload_to='organizations/documents', storage=FileSystemStorage(location=settings.PRIVATE_MEDIA_ROOT))
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('author'), blank=True, null=True)
 
     @property
@@ -108,6 +106,6 @@ class OrganizationDocument(models.Model):
         return reverse('document-download-detail', kwargs={'content_type': content_type, 'pk': self.pk})
 
     class Meta:
-        verbose_name = _("organization document")
-        verbose_name_plural = _("organization documents")
+        verbose_name = _('organization document')
+        verbose_name_plural = _('organization documents')
 

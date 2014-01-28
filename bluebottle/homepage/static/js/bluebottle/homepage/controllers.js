8d7a6f00e4a6818b91500caa0613682126a2e514
Jakob Laegdsmand <info@jakoblaegdsmand.com>
2014-01-28 10:54:55 +0100

    fixed slider and quotes

--- bluebottle/homepage/static/js/bluebottle/homepage/controllers.js
+++ bluebottle/homepage/static/js/bluebottle/homepage/controllers.js
@@ -4,81 +4,24 @@ App.HomeController = Ember.ObjectController.extend({
 	project: null,
     isCampaignHomePage: false,
 	projectIndex: 0,
-    nextProject: null,
-    width: 0,
+    quoteIndex: 0,
     actions: {
-        nextProject: function() { // TODO: similar thing for fundraisers?
-            var projects = this.get('projects');
-
+        nextProject: function() {
             this.incrementProperty('projectIndex');
-
-            if (this.get('projectIndex') >= projects.get('length')) {
-                this.set('projectIndex', 0);
-            }
-
-            this.loadProject();
         },
+    
+        prevProject: function() {
+            this.decrementProperty('projectIndex');        
+        }        
     },
-	
-    projectTransition: function(){
-
-	var project = this.get('project');
-	var nextProject = this.get('nextProject');
-
-	if(nextProject){
-
-	    this.slideProject();
-
-	}else{
-        
-	    $('#project-slide-b').animate({opacity: 'toggle'},0);
-	    this.set('nextProject', project);
-
-	}
-
-    }.observes('project'),
-
-    slideProject: function() {
-
-    	var slide = $('#project-slide-a');
-    	var nextSlide = $('#project-slide-b');
-    	var duration = 500;
-
-    	var project = this.get('project');
-	
-    	controller = this;
-
-    	var callback = function(){
-
-    	    var project = controller.get('project');
-
-    	    controller.set('nextProject', project);
-	    
-    	    slide.animate({opacity: 'toggle'}, 0);
-    	    slide.animate({'margin-left': '0px'}, 0);
-    	    nextSlide.animate({width: 'toggle'}, 0);
-    	};
-
-        slide.css("width", $(slide).width());
-    	slide.animate(
-    	    {'margin-left': '+=1200'},
-    	    duration,
-    	    function(){
-    		    slide.animate({opacity: 'toggle'},0);
-    		    nextSlide.animate({width: 'toggle'},duration,callback);
-    	    });
-	
-	
-	
-    },
-
-    loadProject: function() {
-        var controller = this;
-        var projectId = this.get('projects').objectAt(this.get('projectIndex')).get('id');
-        App.Project.find(projectId).then(function(project) {
-            controller.set('project', project);
-        });
-    },
+    
+    animateSlider: function() {
+        console.log('animate')
+        var index = this.get('projectIndex');
+        var project_count = this.get("projects").get("length");
+        console.log(index, project_count);
+        $(".project-slides").animate({'margin-left': -(1100 * (index % project_count))})
+    }.observes('projectIndex'),
 
     loadQuote: function() {
         this.set('quote', this.get('quotes').objectAt(this.get('quoteIndex')));

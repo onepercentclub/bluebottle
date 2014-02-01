683d10ab341a3b6a6767557f0496fb1e0e2f8cff
Loek van Gent <loek@1procentclub.nl>
2014-02-01 10:03:24 +0100

    Simplify how to determine if it's first slide

--- bluebottle/slides/static/js/bluebottle/slides/models.js
+++ bluebottle/slides/static/js/bluebottle/slides/models.js
@@ -15,12 +15,6 @@ App.Slide = DS.Model.extend({
     link_text: DS.attr('string'),
     link_url: DS.attr('string'),
     isFirst: function() {
-        var lowestValue = null;
-        App.Slide.find().forEach(function(slide) {
-            var sequence = slide.get("sequence");
-            if(lowestValue == null || sequence < lowestValue)
-                lowestValue = sequence;
-        });
-        return (this.get('sequence') === lowestValue);
-    }.property('@each.sequence')
+        return (this.get('sequence') == 1);
+    }.property('sequence')
 });

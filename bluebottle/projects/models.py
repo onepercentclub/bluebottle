235256a7820a19d07aa427c31ee1f4c8be78cc83
José L. Patiño <jose.lpa@gmail.com>
2014-02-11 12:50:53 +0100

    `projects` app models refactored.

--- bluebottle/projects/models.py
+++ bluebottle/projects/models.py
@@ -2,17 +2,12 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.template.defaultfilters import slugify
-from django.utils import translation
 from django.utils.http import urlquote
 from django.utils.translation import ugettext as _
 
-from babel.numbers import format_currency
 from django_extensions.db.fields import (
     ModificationDateTimeField, CreationDateTimeField)
-from djchoices import DjangoChoices, ChoiceItem
 from sorl.thumbnail import ImageField
-from taggit_autocomplete_modified.managers import (
-    TaggableManagerAutocomplete as TaggableManager)
 
 
 class ProjectTheme(models.Model):
@@ -123,6 +118,7 @@ class BaseProject(models.Model):
         url = self.get_absolute_url()
         bits = url.split('/')
         url = '/'.join(bits[:2] + ['#!'] + bits[2:])
+
         return url
 
     # TODO: move to mixin
@@ -164,98 +160,3 @@ class BaseProject(models.Model):
     @property
     def viewable(self):
         return self.status.viewable
-
-
-class Project(BaseProject):
-    """
-    Standard Project model. If there are any extra fields required, provide
-    your own Project model by extending ``BaseProject``.
-    """
-    tags = TaggableManager(
-        blank=True, verbose_name=_('tags'), help_text=_('Add tags'))
-
-    # Location
-    latitude = models.DecimalField(
-        _('latitude'), max_digits=21, decimal_places=18, null=True, blank=True)
-    longitude = models.DecimalField(
-        _('longitude'), max_digits=21, decimal_places=18, null=True, blank=True)
-
-
-    reach = models.PositiveIntegerField(
-        _('Reach'), help_text=_('How many people do you expect to reach?'),
-        blank=True, null=True)
-
-    video_url = models.URLField(
-        _('video'), max_length=100, blank=True, null=True, default='',
-        help_text=_('Do you have a video pitch or a short movie that '
-                    "explains your project? Cool! We can't wait to see it! "
-                    "You can paste the link to YouTube or Vimeo video here"))
-
-    class Meta:
-        swappable = 'PROJECTS_PROJECT_MODEL'
-
-
-class ProjectDetailField(models.Model):
-    class Types(DjangoChoices):
-        text = ChoiceItem('text', label=_('Text field (one line)'))
-        textarea = ChoiceItem('textarea', label=_('Text area (multiple lines)'))
-        checkbox = ChoiceItem('checkbox', label=_('Checkbox'))
-        radio = ChoiceItem('radio', label=_('Radio buttons'))
-        select = ChoiceItem('select', label=_('Select menu'))
-
-    name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=30, unique=True)
-    active = models.BooleanField(default=True)
-    description = models.CharField(max_length=300, blank=True)
-    type = models.CharField(max_length=100, choices=Types.choices)
-
-    def __unicode__(self):
-        return self.name
-
-
-class ProjectDetailFieldValue(models.Model):
-    field = models.ForeignKey('ProjectDetailField')
-    value = models.CharField(max_length=200)
-    text = models.CharField(max_length=200, blank=True)
-
-
-class ProjectDetailFieldAttribute(models.Model):
-    field = models.ForeignKey('ProjectDetailField')
-    attribute = models.CharField(max_length=200)
-    value = models.CharField(max_length=200)
-
-
-class ProjectDetail(models.Model):
-    project = models.ForeignKey(settings.PROJECTS_PROJECT_MODEL)
-    field = models.ForeignKey(ProjectDetailField)
-    value = models.TextField()
-
-    class Meta:
-        unique_together = ('project', 'field')
-
-
-class ProjectBudgetLine(models.Model):
-    """
-    BudgetLine: Entries to the Project Budget sheet.
-    This is the budget for the amount asked from this
-    website.
-    """
-    project = models.ForeignKey(settings.PROJECTS_PROJECT_MODEL)
-    description = models.CharField(_('description'), max_length=255, default='')
-    currency = models.CharField(max_length=3, default='EUR')
-    amount = models.PositiveIntegerField(_('amount (in cents)'))
-
-    created = CreationDateTimeField()
-    updated = ModificationDateTimeField()
-
-    class Meta:
-        verbose_name = _('budget line')
-        verbose_name_plural = _('budget lines')
-
-    def __unicode__(self):
-        language = translation.get_language().split('-')[0]
-        if not language:
-            language = 'en'
-        return u'{0} - {1}'.format(
-            self.description,
-            format_currency(self.amount / 100.0, self.currency, locale=language))

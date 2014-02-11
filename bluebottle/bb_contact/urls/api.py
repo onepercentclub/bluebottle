35a5a5029fc3f5d608ddd06a4b59dd1e3408e230
José L. Patiño <jose.lpa@gmail.com>
2014-02-11 15:32:38 +0100

    `contact` app renamed.

--- bluebottle/bb_contact/urls/api.py
+++ bluebottle/bb_contact/urls/api.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from ..views import ContactRequestCreate
 
 urlpatterns = patterns('',
-    url(r'^contact/$', ContactRequestCreate.as_view(), name='contact-request-create'),
+    url(r'^contact/$', ContactRequestCreate.as_view(), name='contact_request_create'),
 )

e651cb241e78e49d9029167ae1665f8729f9d6ad
José L. Patiño <jose.lpa@gmail.com>
2014-02-14 14:38:24 +0100

    Fixed test model for `bb_accounts`.

--- bluebottle/bb_accounts/tests/__init__.py
+++ bluebottle/bb_accounts/tests/__init__.py
@@ -1,2 +1,12 @@
+from bluebottle.bb_accounts.models import BlueBottleBaseUser
+
+
+class TestBaseUser(BlueBottleBaseUser):
+    """
+    Instantiate the abstract base model for a user with no additional
+    attributes. Used only for testing.
+    """
+    pass
+
 from .test_models import *
 # from .unittests import *

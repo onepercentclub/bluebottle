06384c8bb661d426ef4519261c03e16db73a6296
Mark Gallop <mark@rollick.id.au>
2014-02-18 11:18:09 +0100

    fix tests for bb_organizations #359

--- bluebottle/bb_organizations/serializers.py
+++ bluebottle/bb_organizations/serializers.py
@@ -30,16 +30,14 @@ class ManageOrganizationSerializer(OrganizationSerializer):
 
     slug = serializers.SlugField(required=False)
 
-    documents = OrganizationDocumentSerializer(many=True, source='organizationdocument_set', read_only=True)
+    documents = OrganizationDocumentSerializer(many=True, source='documents', read_only=True)
 
     name = serializers.CharField(required=True)
-    description = serializers.CharField(required=False)
     website = URLField(required=False)
     email = serializers.EmailField(required=False)
     twitter = serializers.CharField(required=False)
     facebook = serializers.CharField(required=False)
     skype = serializers.CharField(required=False)
-    legal_status = serializers.CharField(required=False)
 
     class Meta:
         model = ORGANIZATION_MODEL
--- bluebottle/bb_organizations/tests/test_api.py
+++ bluebottle/bb_organizations/tests/test_api.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from bluebottle.bb_organizations.models import BaseOrganization
+from bluebottle.test.models import TestOrganization
 from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
 from bluebottle.test.factory_models.organizations_factories import (
     OrganizationFactory, OrganizationMemberFactory)
@@ -24,10 +25,13 @@ class OrganizationsEndpointTestCase(TestCase):
         self.organization_2 = OrganizationFactory.create()
         self.organization_3 = OrganizationFactory.create()
 
-        self.member_1 = OrganizationMemberFactory(organization=self.organization_1, user=self.user_1)
-        self.member_2 = OrganizationMemberFactory(organization=self.organization_2, user=self.user_1)
-        self.member_3 = OrganizationMemberFactory(organization=self.organization_3, user=self.user_2)
+        self.member_1 = OrganizationMemberFactory.create(user=self.user_1)
+        self.member_2 = OrganizationMemberFactory.create(user=self.user_1)
+        self.member_3 = OrganizationMemberFactory.create(user=self.user_2)
 
+        self.organization_1.members.add(self.member_1)
+        self.organization_2.members.add(self.member_2)
+        self.organization_3.members.add(self.member_3)
 
 class OrganizationListTestCase(OrganizationsEndpointTestCase):
     """
@@ -108,7 +112,7 @@ class ManageOrganizationListTestCase(OrganizationsEndpointTestCase):
         self.assertEqual(response.status_code, 201)
 
         # Check the data.
-        organization = BaseOrganization.objects.latest('pk')
+        organization = TestOrganization.objects.latest('pk')
         self.assertEqual(organization.name, post_data['name'])
         self.assertEqual(organization.slug, post_data['slug'])
         self.assertEqual(organization.address_line1, post_data['address_line1'])
@@ -193,7 +197,7 @@ class ManageOrganizationDetailTestCase(OrganizationsEndpointTestCase):
         self.assertEqual(response.status_code, 200)
 
         # Check the data.
-        organization = Organization.objects.get(pk=self.organization_1.pk)
+        organization = TestOrganization.objects.get(pk=self.organization_1.pk)
         self.assertEqual(organization.name, put_data['name'])
         self.assertEqual(organization.slug, put_data['slug'])
         self.assertEqual(organization.address_line1, put_data['address_line1'])

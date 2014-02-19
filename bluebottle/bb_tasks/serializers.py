9536100130b2aa8cedd3cf08b08c59445d873363
Marco Giannone <marco.giannone@gmail.com>
2014-02-19 16:16:49 +0100

    adding fixes for bluebottle in order to fix also booking tests - this also contains the changes made by jose' in refaktor - we were working and fixing the same things

--- bluebottle/bb_tasks/serializers.py
+++ bluebottle/bb_tasks/serializers.py
@@ -25,34 +25,33 @@ class TaskPreviewSerializer(serializers.ModelSerializer):
 
 class TaskMemberSerializer(serializers.ModelSerializer):
     member = UserPreviewSerializer()
-    task = serializers.PrimaryKeyRelatedField()
+    # task = serializers.PrimaryKeyRelatedField()
     status = serializers.ChoiceField(choices=TaskMember.TaskMemberStatuses.choices, required=False, default=TaskMember.TaskMemberStatuses.applied)
     motivation = serializers.CharField(required=False)
 
     class Meta:
         model = TaskMember
-        fields = ('id', 'member', 'task', 'status', 'created', 'motivation')
+        fields = ('id', 'member', 'status', 'created', 'motivation') #'task'
 
 
 class TaskFileSerializer(serializers.ModelSerializer):
     author = UserPreviewSerializer()
-    task = serializers.PrimaryKeyRelatedField()
     file = FileSerializer()
 
     class Meta:
         model = TaskFile
-        fields = ('id', 'author', 'task', 'file', 'created', 'title')
+        fields = ('id', 'author', 'file', 'created', 'title') #'task'
 
 
-class TaskSerializer(TaggableSerializerMixin, serializers.ModelSerializer):
-    # members = TaskMemberSerializer(many=True, source='members', read_only=True)
-    # files = TaskFileSerializer(many=True, source='files', read_only=True)
+class TaskSerializer(serializers.ModelSerializer):
+    members = TaskMemberSerializer(many=True, source='members', read_only=True)
+    files = TaskFileSerializer(many=True, source='files', read_only=True)
     project = serializers.SlugRelatedField(slug_field='slug')
     skill = serializers.PrimaryKeyRelatedField()
     author = UserPreviewSerializer()
     status = HumanReadableChoiceField(choices=BB_TASK_MODEL.TaskStatuses.choices, default=BB_TASK_MODEL.TaskStatuses.open)
 
-    tags = TagSerializer()
+    # tags = TagSerializer()
     meta_data = MetaField(
         title = 'get_meta_title',
         fb_title = 'get_fb_title',
@@ -62,9 +61,8 @@ class TaskSerializer(TaggableSerializerMixin, serializers.ModelSerializer):
 
     class Meta:
         model = BB_TASK_MODEL
-        fields = ('id', 'title', 'project', 'description', 'end_goal', 'members', 'files', 'location', 'skill',
-                  'time_needed', 'author', 'status', 'created', 'deadline', 'tags', 'meta_data',
-                  'people_needed'
+        fields = ('id', 'title', 'project', 'description', 'members', 'files', 'skill',
+                  'time_needed', 'author', 'status', 'created', 'deadline', 'meta_data',
         )
 
 

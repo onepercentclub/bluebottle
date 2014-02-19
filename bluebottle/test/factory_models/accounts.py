9489cc23795ff38d539736f5b0577d04b8ba92b6
José L. Patiño <jose.lpa@gmail.com>
2014-02-19 18:53:57 +0100

    Fixed factory model for user accounts.

--- bluebottle/test/factory_models/accounts.py
+++ bluebottle/test/factory_models/accounts.py
@@ -1,10 +1,13 @@
 import factory
 
 from bluebottle.test.models import TestBaseUser
+from django.contrib.auth import get_user_model
+
+BB_USER_MODEL = get_user_model()
 
 
 class BlueBottleUserFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = TestBaseUser
+    FACTORY_FOR = BB_USER_MODEL
 
     username = factory.Sequence(lambda n: u'user_{0}'.format(n))
     email = factory.Sequence(lambda o: u'user_{0}@onepercentclub.com'.format(o))

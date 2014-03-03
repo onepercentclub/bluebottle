fae640dd1f6ce02644aa840a3c980f34a43cc0fe
Aksel <aksel.ethem@gmail.com>
2014-03-03 12:35:32 +0100

    uncommented the task notification code, updated TaskMember references to dynamic model

--- bluebottle/bb_tasks/models.py
+++ bluebottle/bb_tasks/models.py
@@ -128,6 +128,12 @@ class BaseTask(models.Model):
         return self.title
 
 
+
+from . import get_task_model, get_taskmember_model
+
+TASK_MODEL = get_task_model()
+TASK_MEMBER_MODEL = get_taskmember_model
+
 class SupportedProjectsManager(models.Manager):
     """
     Manager to retrieve user statistics related to supported projects through
@@ -141,7 +147,7 @@ class SupportedProjectsManager(models.Manager):
         Usage: Task.supported_projects.by_user(user) returns the projects
         queryset.
         """
-        statuses = TaskMember.TaskMemberStatuses
+        statuses = TASK_MEMBER_MODEL.TaskMemberStatuses
 
         valid_statuses = [
             statuses.applied, statuses.accepted, statuses.realized]
@@ -152,131 +158,126 @@ class SupportedProjectsManager(models.Manager):
         return projects
 
 
-# from . import get_task_model
-#
-# TASK_MODEL = get_task_model()
-#
-#
-# @receiver(m2m_changed, weak=False, sender=TASK_MODEL.members.through)
-# def new_reaction_notification(sender, instance, **kwargs):
-#     """
-#     Signal receiver for new additions to the members of a task. It will set up
-#     the ID of the related task in the ``TaskMember`` model and also send an
-#     email to the task author to notify that a new member applied for the task.
-#     """
-#     if kwargs['action'] == 'post_add':
-#         task_member_pk = kwargs['pk_set'].pop()
-#         task_member = TaskMember.objects.get(pk=task_member_pk)
-#         task = instance
-#
-#         # Set up the ID of the related task here.
-#         if not task_member.task_id:
-#             task_member.task_id = task.pk
-#             task_member.save()
-#
-#         site = 'https://' + Site.objects.get_current().domain
-#
-#         # Project Wall Post
-#         if task_member.status == TaskMember.TaskMemberStatuses.applied:
-#             receiver = task.author
-#             sender = task_member.member
-#             link = '/go/tasks/{0}'.format(task.id)
-#
-#             # Compose the mail
-#             # Set the language for the receiver
-#             translation.activate(receiver.primary_language)
-#             subject = _('{sender} applied for your task.'.format(sender=sender.get_short_name()))
-#             ctx = Context({'task': task, 'receiver': receiver, 'sender': sender, 'link': link, 'site': site,
-#                            'motivation': task_member.motivation})
-#             text_content = render_to_string('task_member_applied.mail.txt', context_instance=ctx)
-#             html_content = render_to_string('task_member_applied.mail.html', context_instance=ctx)
-#             translation.deactivate()
-#             msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
-#             msg.attach_alternative(html_content, "text/html")
-#             msg.send()
-#
-#
-# @receiver(post_save, weak=False, sender=TaskMember)
-# def accepted_member_notification(sender, instance, created, **kwargs):
-#     """
-#     Signal handler for existing members status changes. When a task member is
-#     accepted or refused by the task author/manager, an email will be sent to
-#     inform the member by this function.
-#     """
-#     if not created:
-#         task_member = instance
-#
-#         if task_member.task_id:
-#             task = TASK_MODEL.objects.get(pk=task_member.task_id)
-#
-#             site = 'https://' + Site.objects.get_current().domain
-#
-#             if task_member.status == TaskMember.TaskMemberStatuses.accepted:
-#                 sender = task.author
-#                 receiver = task_member.member
-#                 link = '/go/tasks/{0}'.format(task.id)
-#
-#                 # Compose the mail
-#                 # Set the language for the receiver
-#                 translation.activate(receiver.primary_language)
-#                 subject = _('{sender} accepted you to complete the tasks you applied for.'.format(
-#                     sender=sender.get_short_name()))
-#                 context = Context({'task': task, 'receiver': receiver, 'sender': sender, 'link': link, 'site': site})
-#                 text_content = get_template('task_member_accepted.mail.txt').render(context)
-#                 html_content = get_template('task_member_accepted.mail.html').render(context)
-#                 translation.deactivate()
-#                 msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
-#                 msg.attach_alternative(html_content, "text/html")
-#                 msg.send()
-#
-#             if task_member.status == TaskMember.TaskMemberStatuses.rejected:
-#                 sender = task.author
-#                 receiver = task_member.member
-#                 link = '/go/tasks/{0}'.format(task.id)
-#
-#                 # Compose the mail
-#                 # Set the language for the receiver
-#                 translation.activate(receiver.primary_language)
-#                 subject = _('{sender} found someone else to do the task you applied for.'.format(
-#                     sender=sender.get_short_name()))
-#                 context = Context({'task': task, 'receiver': receiver, 'sender': sender, 'link': link, 'site': site})
-#                 text_content = get_template('task_member_rejected.mail.txt').render(context)
-#                 html_content = get_template('task_member_rejected.mail.html').render(context)
-#                 translation.deactivate()
-#                 msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
-#                 msg.attach_alternative(html_content, "text/html")
-#                 msg.send()
-#
-#
-# @receiver(post_save, weak=False, sender=TASK_MODEL)
-# def send_mail_task_realized(sender, instance, created, **kwargs):
-#     """
-#     Send (multiple) e-mails when a task is realized.
-#     The task members that weren't rejected are the receivers.
-#     """
-#     if not created and instance.status == 'realized':
-#         task = instance
-#         mail_sender = task.author
-#         link = '/go/tasks/{0}'.format(task.id)
-#         site = 'https://' + Site.objects.get_current().domain
-#
-#         qs = task.members.exclude(status=TaskMember.TaskMemberStatuses.rejected)
-#         receivers = [taskmember.member for taskmember in qs]
-#
-#         emails = []
-#
-#         for receiver in receivers:
-#             translation.activate(receiver.primary_language)
-#             subject = _('Good job! "{task}" is realized!.'.format(task=task.title))
-#             context = Context({'task': task, 'receiver': receiver, 'sender': mail_sender, 'link': link, 'site': site})
-#             text_content = get_template('task_realized.mail.txt').render(context)
-#             html_content = get_template('task_realized.mail.html').render(context)
-#             translation.deactivate()
-#             msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
-#             msg.attach_alternative(html_content, "text/html")
-#             emails.append(msg)
-#
-#         if emails:
-#             connection = get_connection()
-#             connection.send_messages(emails)
-#             connection.close()
+@receiver(m2m_changed, weak=False, sender=TASK_MODEL.members.through)
+def new_reaction_notification(sender, instance, **kwargs):
+    """
+    Signal receiver for new additions to the members of a task. It will set up
+    the ID of the related task in the ``TaskMember`` model and also send an
+    email to the task author to notify that a new member applied for the task.
+    """
+    if kwargs['action'] == 'post_add':
+        task_member_pk = kwargs['pk_set'].pop()
+        task_member = TASK_MEMBER_MODEL.objects.get(pk=task_member_pk)
+        task = instance
+
+        # Set up the ID of the related task here.
+        if not task_member.task_id:
+            task_member.task_id = task.pk
+            task_member.save()
+
+        site = 'https://' + Site.objects.get_current().domain
+
+        # Project Wall Post
+        if task_member.status == TASK_MEMBER_MODEL.TaskMemberStatuses.applied:
+            receiver = task.author
+            sender = task_member.member
+            link = '/go/tasks/{0}'.format(task.id)
+
+            # Compose the mail
+            # Set the language for the receiver
+            translation.activate(receiver.primary_language)
+            subject = _('{sender} applied for your task.'.format(sender=sender.get_short_name()))
+            ctx = Context({'task': task, 'receiver': receiver, 'sender': sender, 'link': link, 'site': site,
+                           'motivation': task_member.motivation})
+            text_content = render_to_string('task_member_applied.mail.txt', context_instance=ctx)
+            html_content = render_to_string('task_member_applied.mail.html', context_instance=ctx)
+            translation.deactivate()
+            msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+
+@receiver(post_save, weak=False, sender=TASK_MEMBER_MODEL)
+def accepted_member_notification(sender, instance, created, **kwargs):
+    """
+    Signal handler for existing members status changes. When a task member is
+    accepted or refused by the task author/manager, an email will be sent to
+    inform the member by this function.
+    """
+    if not created:
+        task_member = instance
+
+        if task_member.task_id:
+            task = TASK_MODEL.objects.get(pk=task_member.task_id)
+
+            site = 'https://' + Site.objects.get_current().domain
+
+            if task_member.status == TASK_MEMBER_MODEL.TaskMemberStatuses.accepted:
+                sender = task.author
+                receiver = task_member.member
+                link = '/go/tasks/{0}'.format(task.id)
+
+                # Compose the mail
+                # Set the language for the receiver
+                translation.activate(receiver.primary_language)
+                subject = _('{sender} accepted you to complete the tasks you applied for.'.format(
+                    sender=sender.get_short_name()))
+                context = Context({'task': task, 'receiver': receiver, 'sender': sender, 'link': link, 'site': site})
+                text_content = get_template('task_member_accepted.mail.txt').render(context)
+                html_content = get_template('task_member_accepted.mail.html').render(context)
+                translation.deactivate()
+                msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
+                msg.attach_alternative(html_content, "text/html")
+                msg.send()
+
+            if task_member.status == TASK_MEMBER_MODEL.TaskMemberStatuses.rejected:
+                sender = task.author
+                receiver = task_member.member
+                link = '/go/tasks/{0}'.format(task.id)
+
+                # Compose the mail
+                # Set the language for the receiver
+                translation.activate(receiver.primary_language)
+                subject = _('{sender} found someone else to do the task you applied for.'.format(
+                    sender=sender.get_short_name()))
+                context = Context({'task': task, 'receiver': receiver, 'sender': sender, 'link': link, 'site': site})
+                text_content = get_template('task_member_rejected.mail.txt').render(context)
+                html_content = get_template('task_member_rejected.mail.html').render(context)
+                translation.deactivate()
+                msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
+                msg.attach_alternative(html_content, "text/html")
+                msg.send()
+
+
+@receiver(post_save, weak=False, sender=TASK_MODEL)
+def send_mail_task_realized(sender, instance, created, **kwargs):
+    """
+    Send (multiple) e-mails when a task is realized.
+    The task members that weren't rejected are the receivers.
+    """
+    if not created and instance.status == 'realized':
+        task = instance
+        mail_sender = task.author
+        link = '/go/tasks/{0}'.format(task.id)
+        site = 'https://' + Site.objects.get_current().domain
+
+        qs = task.members.exclude(status=TASK_MEMBER_MODEL.TaskMemberStatuses.rejected)
+        receivers = [taskmember.member for taskmember in qs]
+
+        emails = []
+
+        for receiver in receivers:
+            translation.activate(receiver.primary_language)
+            subject = _('Good job! "{task}" is realized!.'.format(task=task.title))
+            context = Context({'task': task, 'receiver': receiver, 'sender': mail_sender, 'link': link, 'site': site})
+            text_content = get_template('task_realized.mail.txt').render(context)
+            html_content = get_template('task_realized.mail.html').render(context)
+            translation.deactivate()
+            msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
+            msg.attach_alternative(html_content, "text/html")
+            emails.append(msg)
+
+        if emails:
+            connection = get_connection()
+            connection.send_messages(emails)
+            connection.close()

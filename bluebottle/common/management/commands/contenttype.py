7a6553d468b60a71eac7a725dbe0419e4bc9e43f
Marco Giannone <marco.giannone@gmail.com>
2014-03-03 15:20:48 +0100

    Fixed contenttype command

--- bluebottle/common/management/commands/contenttype.py
+++ bluebottle/common/management/commands/contenttype.py
@@ -5,88 +5,130 @@ from django.contrib.contenttypes.management import update_all_contenttypes, upda
 from optparse import make_option
 
 '''
-    Cleaning ContentType
-    options:
-        update contenttype
-        clean single contenttype
-        clean all contenttypes
+    The following command allow you to easily manage the Content Type of the project
+
+    - Delete the content types for a single model or for app_label:
+        [--delete-model; --delete-label]
+
+    - Delete content types for obsolete applications:
+        [--delete-obsolete]
+
+    - Update content types for a single application:
+        [--uS, --update-single]
+
+    - Update content types for all the applications:
+        [--uA, --update-all]
+
+    - Show the content types:
+        [-s, --show]    #all
+        [--in]          #shows the ones in INSTALLED_APPS
+        [--out]         #shows the ones not in INSTALLED_APPS
 '''
 
 class Command(BaseCommand):
-
     option_list = BaseCommand.option_list + (
+
+        make_option('--delete-model', metavar='ct',
+                    dest='deleteModel', default=False,
+                    help='Delete the contenttypes for the specified model'
+                    ),
+
+        make_option('--delete-label', metavar='ct',
+                    dest='deleteLabel', default=False,
+                    help='Delete the contenttypes for the specified app,'
+                    ),
 
         make_option('--delete-obsolete', action='store_true',
                     dest='deleteObsolete', default=False,
                     help='Delete all the content types for old application,'
-                         'remove any model which has no reference in the INSTALLED_APP'
+                         'remove any model which has no reference (app_label) in the INSTALLED_APP'
+                    ),
+
+        make_option('--uS', '--update-single', metavar='ct',
+                    dest='updateSingle', default=False,
+                    help='Creates content types for models in the given app, '
+                         'removing any model' \
+                         'entries that no longer have a matching model class'
                     ),
 
         make_option('--uA', '--update-all', action='store_true',
                     dest='updateAll', default=False,
                     help='Creates content types for models in all the installed app, '
-                         'removing any model entries that no longer have a matching model class.'
-                    ),
-
-        make_option('--uS', '--update-single', action='store_true',
-                    dest='updateSingle', default=False,
-                    help='Creates content types for models in the given app, '
-                         'removing any model' \
-                         'entries that no longer have a matching model class.'
+                         'removing any model entries that no longer have a matching model class'
                     ),
 
         make_option('-s', '--show', action='store_true',
                     dest='show', default=False,
-                    help='Return the list of all the ContentTypes'),
+                    help='Return the list of all the ContentTypes ordered by model'
+                    ),
 
         make_option('--in', action='store_true',
                     dest='showIn', default=False,
-                    help="used with show: shows contentTypes related to apps in INSTALLED_APPS"),
+                    help="used with show: shows contentTypes related to apps in INSTALLED_APPS"
+                    ),
 
         make_option('--out', action='store_true',
                     dest='showOut', default=False,
-                    help="used with show: shows contentTypes not related to apps in INSTALLED_APPS"),
-
+                    help="used with show: shows contentTypes not related to apps in INSTALLED_APPS"
+                    ),
     )
 
+
     def handle(self, *args, **options):
+
+        if options['deleteModel']:
+            modelToDelete = options['deleteModel']
+            self.stdout.write("Going to delete the contenttype for the %s model" %modelToDelete)
+            # ContentType.objects.filter(model__in=model_to_delete).delete()
+            mt = ContentType.objects.filter(model=modelToDelete)
+
+        if options['deleteLabel']:
+            labelToDelete = options['deleteLabel']
+            self.stdout.write("Going to delete the contenttypes for the '%s' app" %labelToDelete)
+            # ContentType.objects.filter(app_label__in=label_to_delete).delete()
+            lt = ContentType.objects.filter(app_label=labelToDelete)
 
         if options['deleteObsolete']:
             self.stdout.write("Going to Deleted all obsolete ContentType")
             for ct in ContentType.objects.exclude(app_label__in=settings.INSTALLED_APPS):
-                self.stdout.write("Deleting %s FAKEMSG" %ct.model)
-                ct.delete()
-            self.stdout.write("Done")
+                self.stdout.write("Deleting %s" %ct.model)
+                # ct.delete()
 
         if options['updateSingle']:
-            contentToBeDeleted = options['updateSingle']
+            contentToUpdate = options['updateSingle']
             self.stdout.write("Going to Update the content type for the models "
-                              "in the %s app" %contentToBeDeleted)
-            update_contenttypes(contentToBeDeleted)
-            self.stdout.write("Done")
+                              "in the %s app" %contentToUpdate)
+            update_contenttypes(contentToUpdate)
 
         if options['updateAll']:
             self.stdout.write("Going to Update the content type for all the apps")
             update_all_contenttypes()
-            self.stdout.write("Done")
 
         if options['show']:
-            queryset_dict = {'all': ContentType.objects.all(),
-                    'in': ContentType.objects.filter(app_label__in=settings.INSTALLED_APPS),
-                    'out': ContentType.objects.exclude(app_label__in=settings.INSTALLED_APPS)}
-            if options['showIn']:
-                queryset = queryset_dict['in']
-            if options['showOut']:
-                queryset = queryset_dict['in']
-            else:
-                queryset = queryset_dict['all']
 
-            self.stdout.write("here is the list ordered per app_label")
+            queryset = ContentType.objects.all()
+            if options['showIn']:
+                queryset = ContentType.objects.filter(app_label__in=settings.INSTALLED_APPS)
+            elif options['showOut']:
+                queryset = ContentType.objects.exclude(app_label__in=settings.INSTALLED_APPS)
+
+            self.stdout.write("Here is the list of contenttypes ordered per app_label: ")
+            self.stdout.write("\n")
             app = []
+            modelCounter = 0
+            labelCounter = 0
             for content in queryset:
                 if content.app_label in app:
                     self.stdout.write('    - %s'%content.model)
+                    modelCounter += 1
                 else:
                     app.append(content.app_label)
                     self.stdout.write('- %s'%content.app_label)
                     self.stdout.write('    - %s'%content.model)
+                    labelCounter += 1
+                    modelCounter += 1
+            self.stdout.write("\n")
+            self.stdout.write("%s of apps" %labelCounter)
+            self.stdout.write("%s of models" %modelCounter)
+
+        self.stdout.write("Done")

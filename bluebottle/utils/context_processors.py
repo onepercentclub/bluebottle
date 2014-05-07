686872b89af898b56fe8f74b5f44ab902a03be99
Loek van Gent <loek@1procentclub.nl>
2014-05-07 21:36:52 +0200

    Make DEBUG setting available in templates/js

--- bluebottle/utils/context_processors.py
+++ bluebottle/utils/context_processors.py
@@ -25,6 +25,7 @@ def google_analytics_code(request):
         context ={}
     return context
 
+
 def google_maps_api_key(request):
     """
     Add Google Maps API key from settings file to general request context.
@@ -35,12 +36,26 @@ def google_maps_api_key(request):
         context = {}
     return context
 
+
 def git_commit(request):
     """
     Make the git commit hash available in the templates.
     """
     try:
         context = {'GIT_COMMIT': settings.GIT_COMMIT}
+    except AttributeError:
+        context = {}
+    return context
+
+
+def conf_settings(request):
+    """
+    Some settings we want to make available in templates.
+    """
+    try:
+        context = {'settings': {
+            'DEBUG': settings.DEBUG,
+        }}
     except AttributeError:
         context = {}
     return context

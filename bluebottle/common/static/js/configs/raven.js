84aea6cc00f8248016ee8b995ae9f21b197c3f2e
Mark Gallop <mark@rollick.id.au>
2014-05-28 19:48:11 +0200

    add raven.js for development and config

---
+++ bluebottle/common/static/js/configs/raven.js
@@ -0,0 +1,14 @@
+if (SENTRY_DSN) {
+    Raven.config(SENTRY_DSN, {
+        whitelistUrls: ['/static/assets/js'],
+        ignoreErrors: [
+            // Random plugins/extensions
+            'top.GLOBALS',
+        ],
+        ignoreUrls: [
+            // Chrome extensions
+            /extensions\//i,
+            /^chrome:\/\//i,
+        ]
+    }).install();
+}

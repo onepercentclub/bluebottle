bd3a98408996d286073e4ac12e2f283d516a0718
Aksel <aksel.ethem@gmail.com>
2014-06-04 14:17:01 +0200

    Update permissions.py

--- bluebottle/bb_tasks/permissions.py
+++ bluebottle/bb_tasks/permissions.py
@@ -38,8 +38,6 @@ class IsTaskAuthorOrReadOnly(permissions.BasePermission):
             return task.author == request.user
         return False
 
-
-
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request, so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:

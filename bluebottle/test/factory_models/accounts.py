739fbcc35da08d688e3c056c028feaec015a5f5a
Marco Giannone <marco.giannone@gmail.com>
2014-06-10 12:22:40 +0200

    BOOK-595, tests

--- bluebottle/test/factory_models/accounts.py
+++ bluebottle/test/factory_models/accounts.py
@@ -11,4 +11,7 @@ class BlueBottleUserFactory(factory.DjangoModelFactory):
     username = factory.Sequence(lambda n: u'user_{0}'.format(n))
     email = factory.Sequence(lambda o: u'user_{0}@onepercentclub.com'.format(o))
     password = factory.PostGenerationMethodCall('set_password', 'testing')
+    first_name = factory.Sequence(lambda f: u'user_{0}'.format(f))
+    last_name = factory.Sequence(lambda l: u'user_{0}'.format(l))
+    full_name = factory.LazyAttribute(lambda a: '{0} {1}'.format(a.first_name, a.last_name).lower())
     is_active = True
--- bluebottle/wallposts/tests/test_api.py
+++ bluebottle/wallposts/tests/test_api.py
@@ -59,9 +59,9 @@ class WallPostReactionApiIntegrationTest(TestCase):
     def setUp(self):
         self.some_wallpost = TextWallPostFactory.create()
         self.another_wallpost = TextWallPostFactory.create()
-
-        self.some_user = BlueBottleUserFactory.create(password='testing', full_name='full name')
-        self.another_user = BlueBottleUserFactory.create(password='testing', full_name='full name')
+        import ipdb; ipdb.set_trace()
+        self.some_user = BlueBottleUserFactory.create(password='testing', first_name='someName', last_name='someLast')
+        self.another_user = BlueBottleUserFactory.create(password='testing', first_name='anotherName', last_name='anotherLast')
 
         self.wallpost_reaction_url = reverse('wallpost_reaction_list')
         self.wallpost_url = reverse('wallpost_list')

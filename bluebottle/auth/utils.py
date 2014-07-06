5c0a0ba89217c82b9daa79218f9fef1c8d9b28f6
Aksel Ethem <aksel.ethem@gmail.com>
2014-07-06 15:06:52 +0200

    added storing of extra fields with FB

--- bluebottle/auth/utils.py
+++ bluebottle/auth/utils.py
@@ -1,3 +1,5 @@
+import time
+from datetime import datetime
 from requests import request, HTTPError
 from django.contrib.auth import get_user_model
 from django.core.files.base import ContentFile
@@ -29,10 +31,32 @@ def get_extra_facebook_data(strategy, user, response, details, is_new=False, *ar
         id, name, first_name, last_name, link, gender, locale, age_range
     """
 
+#    url = "https://graph.facebook.com/10152102266140933"
+#    result = request('GET', url, params={'access_token': response.get('access_token', None)})
+    print "Extra", user.social_auth.get(provider='facebook').extra_data
+    print kwargs
+    print response
     user.first_name = response.get('first_name', None)
     user.last_name = response.get('last_name', None)
-    user.gender = response.get('gender', None)
+    if not user.gender:
+        user.gender = response.get('gender', None)
     fb_link = response.get('link', None)
+
+    birthday = response.get('birthday', None)
+    if birthday and not user.birthdate:
+        birthdate = time.strptime(birthday,"%m%d%Y")
+        user.birthdate = datetime.fromtimestamp(time.mktime(birthdate))
+
+    if user.about and response.get("about", None):
+        user.about = response.get("about", None)
+
+    if not user.location and response.get("location", None):
+        user.location = response.get("location", None)
+
+    if not user.website and response.get("website", None):
+        user.website = response.get("website", None)
+
     if len(fb_link) < 50:
         user.facebook = fb_link
+
     user.save()

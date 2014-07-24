a28f719321782f84809a62832ef2b946be68b025
Aksel Ethem <aksel.ethem@gmail.com>
2014-07-24 15:03:18 +0200

    move mixpanel to own code and change base.html for import

--- bluebottle/common/static/js/bluebottle/bb_mixpanel.js
+++ bluebottle/common/static/js/bluebottle/bb_mixpanel.js
@@ -26,3 +26,38 @@ Ember.Application.initializer({
     application.inject("controller:tracker", 'currentUser', "controller:currentUser");
   }
 });
+
+/*
+    Specialized controller for tracking (still coupled to Mixpanel at the moment)
+ */
+App.TrackerController = Em.ObjectController.extend({
+   needs: "currentUser",
+
+   init: function(){
+       this._super();
+       if (MIXPANEL_KEY && mixpanel) {
+           this.set('_tracker', mixpanel);
+       }
+
+
+   }.observes('window'),
+
+   trackEvent: function(name, properties) {
+        if (Em.typeOf(properties) == 'undefined') properties = {};
+
+        if (Em.typeOf(name) == 'string' && Em.typeOf(properties) == 'object') {
+            this.get('_tracker').track(name, properties);
+        }
+    },
+
+    setUserDetails: function(){
+        if (this.get('controllers.currentUser.isAuthenticated')) {
+            var user = this.get('controllers.currentUser');
+            this.get('_tracker').register({
+                "email": user.get('email'),
+                "name": user.get('full_name')
+            });
+        }
+    }.observes('controllers.currentUser.isAuthenticated')
+
+});

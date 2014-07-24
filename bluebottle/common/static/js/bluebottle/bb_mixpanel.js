31cd353288f993d301605e7c077293a4b0dfbd87
Aksel Ethem <aksel.ethem@gmail.com>
2014-07-24 16:46:35 +0200

    removed currentuser line

--- bluebottle/common/static/js/bluebottle/bb_mixpanel.js
+++ bluebottle/common/static/js/bluebottle/bb_mixpanel.js
@@ -16,12 +16,13 @@ Ember.Application.initializer({
         // Calling the lookup function seems to have the side-effect of instantiating the tracker controller.
         // Perhaps the call to lookup also registers the controller with the container so that it may be injected
         container.lookup('controller:tracker');
-        container.lookup('controller:currentUser');
 
         // Without the previous lookup the injection fails. We inject on the application object, rather than the container.
         // See: http://balinterdi.com/2014/05/01/dependency-injection-in-ember-dot-js.html
         application.inject("route", "tracker", "controller:tracker");
         application.inject("controller", "tracker", "controller:tracker");
+
+
   }
 });
 
@@ -59,3 +60,5 @@ App.TrackerController = Em.ObjectController.extend({
     }.observes('controllers.currentUser.isAuthenticated')
 
 });
+
+

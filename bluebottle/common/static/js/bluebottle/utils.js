582682b4d747b6db094d664a4c0bec1b9965b54e
Aksel Ethem <aksel.ethem@gmail.com>
2014-07-24 17:38:09 +0200

    update to activate track mixin

--- bluebottle/common/static/js/bluebottle/utils.js
+++ bluebottle/common/static/js/bluebottle/utils.js
@@ -656,9 +656,9 @@ App.TrackRouteActivateMixin = Ember.Mixin.create({
     activate: function() {
         this._super();
 
-        Em.assert(this.toString() + ' must define tracker property', !Em.isEmpty(this.get('tracker')));
-        Em.assert(this.toString() + ' must define trackEventName property', !Em.isEmpty(this.get('trackEventName')));
-
-        this.get('tracker').trackEvent(this.get('trackEventName'));
+        if (this.get('tracker')){
+            Em.assert(this.toString() + ' must define trackEventName property', !Em.isEmpty(this.get('trackEventName')));
+            this.get('tracker').trackEvent(this.get('trackEventName'));
+        }
     }
 });

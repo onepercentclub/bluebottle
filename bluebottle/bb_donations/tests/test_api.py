71b7596c8b8dfad50d11225d3b40f588c6b0191c
Loek van Gent <loek@1procentclub.nl>
2014-08-14 21:58:58 +0200

    Add another user to donation tests

--- bluebottle/bb_donations/tests/test_api.py
+++ bluebottle/bb_donations/tests/test_api.py
@@ -11,8 +11,11 @@ from rest_framework import status
 class OrderApiTestCase(InitProjectDataMixin, TestCase):
 
     def setUp(self):
-        self.user = BlueBottleUserFactory.create()
-        self.user_token = "JWT {0}".format(self.user.get_jwt_token())
+        self.user1 = BlueBottleUserFactory.create()
+        self.user1_token = "JWT {0}".format(self.user1.get_jwt_token())
+
+        self.user2 = BlueBottleUserFactory.create()
+        self.user2_token = "JWT {0}".format(self.user2.get_jwt_token())
 
         self.country = CountryFactory.create(alpha2_code='NL')
 
@@ -32,19 +35,19 @@ class TestCreateDonation(OrderApiTestCase):
     def test_create_donation(self):
 
         # Check that there's no orders
-        response = self.client.get(self.manage_order_list_url, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.get(self.manage_order_list_url, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 0)
 
         # Create an order
-        response = self.client.post(self.manage_order_list_url, {}, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.post(self.manage_order_list_url, {}, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['status'], 'cart')
         self.assertEqual(response.data['total'], 0)
         order_id = response.data['id']
 
         # Check that there's one order
-        response = self.client.get(self.manage_order_list_url, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.get(self.manage_order_list_url, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 1)
 
@@ -54,26 +57,26 @@ class TestCreateDonation(OrderApiTestCase):
             "amount": 35
         }
 
-        response = self.client.post(self.manage_donation_list_url, donation1, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.post(self.manage_donation_list_url, donation1, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['status'], 'new')
         donation_id = response.data['id']
 
         # Check that the order total is equal to the donation amount
         order_url = "{0}{1}".format(self.manage_order_list_url, order_id)
-        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['total'], 35)
 
         # Check that this user can change the amount
         donation_url = "{0}{1}".format(self.manage_donation_list_url, donation_id)
         donation1['amount'] = 50
-        response = self.client.put(donation_url, json.dumps(donation1), 'application/json', HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.put(donation_url, json.dumps(donation1), 'application/json', HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # Check that the order total is equal to the increased donation amount
         order_url = "{0}{1}".format(self.manage_order_list_url, order_id)
-        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['total'], 50)
 
@@ -83,25 +86,27 @@ class TestCreateDonation(OrderApiTestCase):
             "order": order_id,
             "amount": 47
         }
-        response = self.client.post(self.manage_donation_list_url, donation2, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.post(self.manage_donation_list_url, donation2, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['status'], 'new')
 
         # Check that the order total is equal to the two donations
         order_url = "{0}{1}".format(self.manage_order_list_url, order_id)
-        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['donations']), 2)
         self.assertEqual(response.data['total'], 97)
 
         # remove the first donation
-        response = self.client.delete(donation_url, 'application/json', HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.delete(donation_url, 'application/json', HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         # Check that the order total is equal to second donation
         order_url = "{0}{1}".format(self.manage_order_list_url, order_id)
-        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user_token)
+        response = self.client.get(order_url, HTTP_AUTHORIZATION=self.user1_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['donations']), 1)
         self.assertEqual(response.data['total'], 47)
+
+
 

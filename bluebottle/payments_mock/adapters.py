042c46254d3852e14129237b68d00da2a29021bb
Loek van Gent <loek@1procentclub.nl>
2014-08-19 00:24:16 +0200

    Empty Mock classes

--- bluebottle/payments_mock/adapters.py
+++ bluebottle/payments_mock/adapters.py
@@ -1,56 +1,17 @@
 # coding=utf-8
-import time
-import unicodedata
 from bluebottle.payments.adapters import AbstractPaymentAdapter
-from bluebottle.payments.models import PaymentStatuses, PaymentLogLevels
-from django.conf import settings
-from django.utils.http import urlencode
-from .models import MolliePayment, MollieTransaction
-import Mollie
+from .models import MockPayment
 
 
-class MolliePaymentAdapter(AbstractPaymentAdapter):
+class MockPaymentAdapter(AbstractPaymentAdapter):
 
-    status_mapping = {
-        'NEW': PaymentStatuses.new,
-        'STARTED': PaymentStatuses.in_progress,
-        'REDIRECTED_FOR_AUTHENTICATION': PaymentStatuses.in_progress,
-        'AUTHORIZED': PaymentStatuses.pending,
-        'AUTHORIZATION_REQUESTED': PaymentStatuses.pending,
-        'PAID': PaymentStatuses.pending,
-        'CANCELED': PaymentStatuses.cancelled,
-        'CHARGED-BACK': PaymentStatuses.chargedback,
-        'CONFIRMED_PAID': PaymentStatuses.paid,
-        'CONFIRMED_CHARGEDBACK': PaymentStatuses.chargedback,
-        'CLOSED_SUCCESS': PaymentStatuses.paid,
-        'CLOSED_CANCELED': PaymentStatuses.cancelled,
-    }
-
-    def get_return_url(self, payment):
-        return 'http://localhost:8000/#!/orders/1/success'
-
+    @staticmethod
     def create_payment(self, order_payment, meta_data=None, **kwargs):
-        payment = MolliePayment(**meta_data)
-        payment.payment_order = order_payment
-        payment.save()
+        payment = MockPayment(**meta_data)
         return payment
 
+    @staticmethod
     def get_authorization_action(self, order_payment):
-        if order_payment.paymen_method == 'mockIdeal':
-            payment = MockIdealPayment()
-
-        mollie = Mollie.API.Client()
-        mollie.setApiKey(settings.MOLLIE_API_KEY)
-
-        # Generate Mollie Payment
-        mollie_payment = mollie.payments.create({
-            'amount': payment.amount,
-            'description': payment.description,
-            'redirectUrl': self.get_return_url(payment)
-        })
-        payment.payment_url = mollie_payment.paymentUrl
-        payment.status = mollie_payment.status
-
-        return mollie_payment.paymentUrl
+        return {}
 
 

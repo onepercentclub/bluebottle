ad029c0dc499d8f50a5e167f181408c84e621273
Mark Gallop <mark@rollick.id.au>
2014-08-21 16:40:24 +0200

    add fundraiser to donations api

--- bluebottle/bb_donations/serializers.py
+++ bluebottle/bb_donations/serializers.py
@@ -6,22 +6,24 @@ DONATION_MODEL = get_model_class('DONATIONS_DONATION_MODEL')
 
 
 class ManageDonationSerializer(serializers.ModelSerializer):
-    project = serializers.SlugRelatedField(source='project', slug_field='slug')
+    project = serializers.SlugRelatedField(slug_field='slug')
+    fundraiser = serializers.PrimaryKeyRelatedField()
     status = serializers.ChoiceField(read_only=True)
     order = serializers.PrimaryKeyRelatedField()
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = DONATION_MODEL
-        fields = ('id', 'project', 'user', 'amount', 'status', 'order')
+        fields = ('id', 'project', 'fundraiser', 'user', 'amount', 'status', 'order')
 
     # FIXME Add validations for amount and project phase
 
 
 class DonationSerializer(serializers.ModelSerializer):
     project = get_serializer_class('PROJECTS_PROJECT_MODEL', 'preview')
+    fundraiser = serializers.PrimaryKeyRelatedField()
     user = get_serializer_class('AUTH_USER_MODEL', 'preview')
 
     class Meta:
         model = DONATION_MODEL
-        fields = ('id', 'project', 'user', 'created')
+        fields = ('id', 'project', 'fundraiser', 'user', 'created')

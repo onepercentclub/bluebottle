c7a7557e2abb93c820947fca2dde82915050160f
Loek van Gent <loek@1procentclub.nl>
2014-08-25 22:58:55 +0200

    Add default fundraiser and task implementations

---
+++ bluebottle/tasks/models.py
@@ -0,0 +1,14 @@
+from bluebottle.bb_tasks.models import BaseTask, BaseTaskMember, BaseTaskFile
+
+
+class Task(BaseTask):
+    pass
+
+
+class TaskMember(BaseTaskMember):
+    pass
+
+
+class TaskFile(BaseTaskFile):
+    pass
+

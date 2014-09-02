d5c77706e3e9d83cef85e7c118cd7bf9d95ad590
rollick <mark@onepercentclub.com>
2014-09-02 18:43:36 +0200

    add a basic test for docdata

---
+++ bluebottle/payments_docdata/tests/test_models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from django.test import TestCase
+from django_fsm.db.fields import TransitionNotAllowed
+
+from bluebottle.test.factory_models.payments import PaymentFactory, OrderPaymentFactory
+from bluebottle.test.factory_models.orders import OrderFactory
+from bluebottle.test.models import TestBaseUser
+from bluebottle.test.utils import FsmTestMixin
+
+from bluebottle.payments.services import PaymentService
+from bluebottle.payments_docdata.gateway import DocdataClient
+from bluebottle.payments_docdata.adapters import DocdataPaymentAdapter
+
+from bluebottle.utils.utils import StatusDefinition
+
+from mock import patch
+
+
+class PaymentsDocdataTestCase(TestCase, FsmTestMixin):
+    @patch.object(DocdataClient, 'create')
+    def setUp(self, mock_client_create):
+        # Mock response to creating the payment at docdata
+        mock_client_create.return_value = {'order_key': 123, 'order_id': 123}
+
+        self.order = OrderFactory.create()
+        self.order_payment = OrderPaymentFactory.create(order=self.order, payment_method='docdata')
+        self.service = PaymentService(order_payment=self.order_payment)
+
+    @patch.object(DocdataClient, 'status')
+    @patch.object(DocdataPaymentAdapter, '_store_payment_transaction')
+    def test_check_authorized_status(self, mock_transaction, mock_client_status):
+        # Mock the status check with docdata
+        mock_client_status.return_value = self.create_status_response('AUTHORIZED')
+
+        self.service.check_payment_status()
+
+        # Check that the status propagated through to order
+        self.assert_status(self.order_payment.payment, StatusDefinition.AUTHORIZED)
+        self.assert_status(self.order_payment, StatusDefinition.AUTHORIZED)
+        self.assert_status(self.order, StatusDefinition.SUCCESS)

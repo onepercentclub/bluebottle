dc0225b0a4cdae0c0ee74cfabcec72da909bdce4
rollick <mark@onepercentclub.com>
2014-09-02 18:42:14 +0200

    the status field and choices are defined on the Payment class

--- bluebottle/payments_mock/models.py
+++ bluebottle/payments_mock/models.py
@@ -5,18 +5,8 @@ from bluebottle.payments.models import Payment
 
 
 class MockPaymentStatuses(DjangoChoices):
-    created = ChoiceItem('created', label=_("Created"))
-    started = ChoiceItem('started', label=_("Started"))
-    authorized = ChoiceItem('authorized', label=_("Authorized"))
-    settled = ChoiceItem('settled', label=_("Settled"))
-    failed = ChoiceItem('failed', label=_("Failed"))
-    cancelled = ChoiceItem('cancelled', label=_("Cancelled"))
-    chargedback = ChoiceItem('charged_back', label=_("Charged back"))
-    refunded = ChoiceItem('refunded', label=_("Refunded"))
-    unknown = ChoiceItem('unknown', label=_("Unknown"))
+    pass
 
 
 class MockPayment(Payment):
-
-    status = models.CharField(_("Status"), max_length=20, choices=MockPaymentStatuses.choices,
-                          default=MockPaymentStatuses.created, db_index=True)
+    pass

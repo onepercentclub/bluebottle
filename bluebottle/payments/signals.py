a5bd59ab81e71b8ae556793598ee45c71bcf46cc
Marco <marco@onepercentclub.com>
2014-09-04 11:36:55 +0200

    adding raven, fixing imports

--- bluebottle/payments/signals.py
+++ bluebottle/payments/signals.py
@@ -1,5 +1,7 @@
+from django.db.models.signals import post_save
 from django.dispatch import Signal
 from django_fsm.signals import post_transition
+from django.dispatch import receiver
 
 from .models import Payment, OrderPayment
 

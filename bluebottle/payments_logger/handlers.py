ae41fd27cb666ad6a3dbffcd38cce101784b1f64
rollick <mark@onepercentclub.com>
2014-09-09 15:00:16 +0200

    add some comments for the payments log handler class BOOK-719

--- bluebottle/payments_logger/handlers.py
+++ bluebottle/payments_logger/handlers.py
@@ -3,7 +3,12 @@ from .models import PaymentLogEntry
 
 
 class PaymentLogHandler(logging.Handler, object):
+    """
+    Log handler for storing payment events as PaymentLogEntry entries in the db
+    """
     def emit(self, record):
+        # TODO: we should use the formatting features of the logging library
+        #       the format the message rather than doing it explicitly here.
         payment = record.args.get('payment')
         message = "{0} - {1}".format(payment, record.msg)
 

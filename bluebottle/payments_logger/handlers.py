2376c3d28a95aa28c83ab35c944078f59cb00462
rollick <mark@onepercentclub.com>
2014-09-10 10:32:22 +0200

    refactor payment logger handler a little to import model when required

--- bluebottle/payments_logger/handlers.py
+++ bluebottle/payments_logger/handlers.py
@@ -1,12 +1,18 @@
 import logging
-from .models import PaymentLogEntry
 
 
-class PaymentLogHandler(logging.Handler, object):
+class PaymentLogHandler(logging.Handler):
     """
     Log handler for storing payment events as PaymentLogEntry entries in the db
     """
+    def __init__(self):
+        logging.Handler.__init__(self)
+
     def emit(self, record):
+        # NOTE: need to import this here otherwise it causes a circular reference
+        #       i.e. settings imports loggers imports models imports settings...
+        from bluebottle.payments_logger.models import PaymentLogEntry
+
         # TODO: we should use the formatting features of the logging library
         #       the format the message rather than doing it explicitly here.
         payment = record.args.get('payment')

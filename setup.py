edecad9c961716828c1d9d3d79d269becab2ce33
Aksel Ethem <aksel.ethem@gmail.com>
2014-09-18 13:01:44 +0200

    update for nose

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ tests_require = [
     'factory-boy==2.3.1',
     'micawber==0.2.6',
     'mock==1.0.1',
-    'nose==1.3.3',
+    'nose==1.3.4',
     'pylint==0.28.0',
     'selenium==2.43.0',
     'South==0.8.1', # Functional testing libraries

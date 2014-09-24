b2dcc9086eceff2aa1b344fd9091645f0e682a35
Marco <marco@onepercentclub.com>
2014-09-24 14:23:26 +0200

    BOOK-852, fixes

--- bluebottle/bb_donations/dashboard.py
+++ bluebottle/bb_donations/dashboard.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.utils.translation import ugettext_lazy as _
 
 from admin_tools.dashboard.modules import DashboardModule
@@ -13,24 +14,27 @@ class DonationModule(DashboardModule):
 
     def __init__(self, **kwargs):
         donation_url = '/admin/donations/donation'
+
+        # import ipdb;ipdb.set_trace()
+        number_of_donations = DONATION_MODEL.objects.count()
+        total_amount = DONATION_MODEL.objects.aggregate(Sum('amount'))['amount__sum']
+
         self.children = (
             # Metrics Donations
-            {'title': 'Total amount donated', 'value': DONATION_MODEL.objects.count(), 'url': donation_url},
-
-            # TODO
-            {'title': 'Total amount matched by Booking', 'value': DONATION_MODEL.objects.count(), 'url': donation_url},
-            {'title': 'Total number of donations', 'value': DONATION_MODEL.objects.count(), 'url': donation_url},
-            {'title': 'Total number of employees who donated', 'value': DONATION_MODEL.objects.count(), 'url': donation_url},
+            {'title': 'Total amount donated', 'value': total_amount, 'url': donation_url},
+            {'title': 'Total amount matched by Booking', 'value': 'TODO', 'url': donation_url},
+            {'title': 'Total number of employees who donated', 'value': 'TODO', 'url': donation_url},
+            {'title': 'Total number of donations', 'value': number_of_donations, 'url': donation_url},
 
             # Sort metrics per region (for loop?)
-            {'title': 'Africa', 'value': DONATION_MODEL.objects.count()},
-            {'title': 'Americas', 'value': DONATION_MODEL.objects.count()},
-            {'title': 'Asia', 'value': DONATION_MODEL.objects.count()},
-            {'title': 'Europe', 'value': DONATION_MODEL.objects.count()},
-            {'title': 'Oceania', 'value': DONATION_MODEL.objects.count()},
+            {'title': 'Africa', 'value': 'TODO'},
+            {'title': 'Americas', 'value': 'TODO'},
+            {'title': 'Asia', 'value': 'TODO'},
+            {'title': 'Europe', 'value': 'TODO'},
+            {'title': 'Oceania', 'value': 'TODO'},
 
             # Sort metrics per project (for loop?)
-            
+            # donation order user office
         )
 
 

aed356f9962af64c6a94be7407dcdbb4621a32cd
Marco Giannone <marco.giannone@gmail.com>
2014-09-24 18:26:46 +0200

    BOOK-852, wip

--- bluebottle/bb_donations/dashboard.py
+++ bluebottle/bb_donations/dashboard.py
@@ -1,3 +1,4 @@
+from bluebottle.utils.utils import StatusDefinition
 from django.db.models import Sum
 from django.utils.translation import ugettext_lazy as _
 
@@ -15,15 +16,21 @@ class DonationModule(DashboardModule):
     def __init__(self, **kwargs):
         donation_url = '/admin/donations/donation'
 
-        # import ipdb;ipdb.set_trace()
-        number_of_donations = DONATION_MODEL.objects.count()
-        total_amount = DONATION_MODEL.objects.aggregate(Sum('amount'))['amount__sum']
+        import ipdb;ipdb.set_trace()
 
+        successful_donations = DONATION_MODEL.objects.filter(order__status=StatusDefinition.SUCCESS)
+        number_of_donations = successful_donations.count()
+        total_amount = successful_donations.aggregate(Sum('amount'))['amount__sum']
+        number_of_employees = successful_donations.distinct('order__user').count()
+        #cd.filter(order__user__office__country__subregion__region='Europe')
+        # cd[1].order.user.office.country.subregion.region
+
+        # office country subregio regio
         self.children = (
             # Metrics Donations
             {'title': 'Total amount donated', 'value': total_amount, 'url': donation_url},
             {'title': 'Total amount matched by Booking', 'value': 'TODO', 'url': donation_url},
-            {'title': 'Total number of employees who donated', 'value': 'TODO', 'url': donation_url},
+            {'title': 'Total number of employees who donated', 'value': number_of_employees, 'url': donation_url},
             {'title': 'Total number of donations', 'value': number_of_donations, 'url': donation_url},
 
             # Sort metrics per region (for loop?)

80906d9d5e225ab6cdfe4d4d06883dd24539da04
Jhon Majoor <jhon@onepercentclub.com>
2014-10-06 15:04:07 +0200

    shows cc icon if card is selected and can select card via icon

--- bluebottle/payments/static/js/bluebottle/payments/views.js
+++ bluebottle/payments/static/js/bluebottle/payments/views.js
@@ -18,3 +18,35 @@ App.CreditcardView = Em.View.extend({
     expirationYear: gettext('Year'),
     cvcCode: gettext('CVC')
 });
+
+App.OrderPaymentView = Em.View.extend({
+    templateName: 'order_payment',
+
+    matchCreditcardChange: function(e) {
+        var x = e.target.className,
+            prntElm = $('.card-item .icon-card'),
+            elmValue = '.icon-card' + '.' + e.target.value;
+
+        if (x.match("creditcard-select")) {
+            prntElm.removeClass('is-selected');
+            $(elmValue).addClass('is-selected');
+        }
+    }.on('change'),
+
+    matchCreditcardClick: function(e){
+        var x = e.target.className,
+            elm = $(e.target),
+            prntElm = $('.card-item .icon-card'),
+            select = prntElm.parents().find('select'),
+            cardName;
+
+        if (x.match("icon-card")) {
+            prntElm.removeClass('is-selected');
+            elm.addClass('is-selected');
+            cardName = x.split(" ").pop();
+
+            $(".creditcard-select option[value=" + cardName +"]").attr('selected','selected');
+        }
+    }.on('click')
+});
+

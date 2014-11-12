f83a2c1b82415c8cb94ac96f50a8c7f791791ef2
Aksel Ethem <aksel.ethem@gmail.com>
2014-11-12 15:09:53 +0100

    added signal handler to create follow objects

--- bluebottle/bb_follow/models.py
+++ bluebottle/bb_follow/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.conf import settings
+from django.db.models.signals import post_save
 from django.contrib.contenttypes import generic
+from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
 from bluebottle.utils.model_dispatcher import get_user_model
+from bluebottle.wallposts.models import WallPost, Reaction
+from bluebottle.bb_projects.models import BaseProject
+from bluebottle.bb_tasks.models import BaseTask
+from bluebottle.bb_donations.models import BaseDonation
 
 USER_MODEL = get_user_model()
 
@@ -19,7 +23,32 @@ class Follow(models.Model):
     followed_object = generic.GenericForeignKey('content_type', 'object_id')
 
     def __unicode__(self):
-        if self.content_object:
-            return str(self.content_object)
+        if self.followed_object:
+            return str(self.followed_object)
         return self.id
 
+
+@receiver(post_save)
+def create_follow(sender, instance, created, **kwargs):
+    """ 
+        Create a Follow object when a user follows a Project or Task. A user starts following a project/task when 
+        he /she creates a WallPost or Reaction, or does a donation.
+    """
+
+    # A WallPost or Reaction is created by user
+    if created and (isinstance(instance, WallPost) or isinstance(instance, Reaction)):
+
+        # Create a Follow to the specific Project or Task if a WallPost or Reaction was created
+        if isinstance(instance.content_object, BaseProject) or isinstance(instance.content_object, BaseTask):
+            user = instance.author
+            if user and instance.content_object:
+                follow = Follow(user=user, followed_object=instance.content_object)
+                follow.save()        
+
+    # A user does a donation
+    if created and isinstance(instance, BaseDonation):
+        # Create a Follow to the specific Project or Task if a donation was made
+        user = instance.user
+        if user and instance.project:
+            follow = Follow(user=user, followed_object=instance.project)    
+            follow.save()        

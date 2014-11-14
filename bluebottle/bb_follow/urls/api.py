91d0edd4be8fbfee894ce4072b6469e12e554171
Aksel <aethemba@users.noreply.github.com>
2014-11-14 16:27:47 +0100

    2 lines

--- bluebottle/bb_follow/urls/api.py
+++ bluebottle/bb_follow/urls/api.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url, include
 from rest_framework.routers import DefaultRouter
 from ..views import FollowViewSet
 
+
 router = DefaultRouter()
 router.register(r'follows', FollowViewSet)
 

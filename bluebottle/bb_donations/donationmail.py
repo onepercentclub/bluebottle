524392c9aa6a9fb3140a3e3dbd7c88729a80eba5
Aksel Ethem <aksel.ethem@gmail.com>
2014-11-26 14:39:53 +0100

    fundraiser code refactoring

--- bluebottle/bb_donations/donationmail.py
+++ bluebottle/bb_donations/donationmail.py
@@ -1,12 +1,5 @@
-from django.dispatch.dispatcher import receiver
-from django.core.mail import EmailMultiAlternatives
-from django.db.models.signals import pre_save
-from django.template import Context
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext as _
-from django.utils import translation
-from django.template.loader import get_template
-from django.utils import translation
 from bluebottle.utils.email_backend import send_mail
 
 
@@ -18,20 +11,17 @@ def successful_donation_fundraiser_mail(instance):
     except:
         # donation it's not coming from a fundraiser
         return
+
     fundraiser_link = '/go/fundraisers/{0}'.format(instance.fundraiser.id)
-    context = Context({'amount': instance.amount,
-                       'site': 'https://' + Site.objects.get_current().domain,
-                       'link': fundraiser_link,
-                       'receiver': receiver.get_short_name()
-                       })
-    subject = _('You received a new donation')
-    translation.activate(receiver.primary_language)
-    translation.deactivate()
-    text_content = get_template('bb_donations/mails/new_oneoff_donation_fundraiser.mail.txt').render(context)
-    html_content = get_template('bb_donations/mails/new_oneoff_donation_fundraiser.mail.html').render(context)
-    msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
+
+    send_mail(
+        template_name='bb_donations/mails/new_oneoff_donation_fundraiser.mail',
+        amount=instance.amount,
+        site = 'https://' + Site.objects.get_current().domain,
+        subject=_('You received a new donation'),
+        to=receiver,
+        link=fundraiser_link,
+    )
 
 
 def new_oneoff_donation(instance):

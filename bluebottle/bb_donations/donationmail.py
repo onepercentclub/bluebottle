9da17bf19c55e19e88eb54033918b41b76375458
Aksel Ethem <aksel.ethem@gmail.com>
2014-11-26 14:35:43 +0100

    moved send_mail to bb utils

--- bluebottle/bb_donations/donationmail.py
+++ bluebottle/bb_donations/donationmail.py
@@ -7,30 +7,8 @@ from django.utils.translation import ugettext as _
 from django.utils import translation
 from django.template.loader import get_template
 from django.utils import translation
+from bluebottle.utils.email_backend import send_mail
 
-
-def send_mail(template_name, subject, to, **kwargs):
-    if hasattr(to, 'primary_language') and to.primary_language:
-        translation.activate(to.primary_language)
-
-    kwargs.update({
-        'receiver': to,
-        'site': 'https://{0}'.format(Site.objects.get_current().domain)
-    })
-
-    context = Context(kwargs)
-    subject = unicode(subject)  # Unlazy the translatable string subject within activated language.
-
-    text_content = get_template('{0}.txt'.format(template_name)).render(context)
-    html_content = get_template('{0}.html'.format(template_name)).render(context)
-
-    if hasattr(to, 'primary_language') and to.primary_language:
-        translation.deactivate()
-
-    msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[to.email])
-    msg.attach_alternative(html_content, "text/html")
-
-    return msg.send()
 
 def successful_donation_fundraiser_mail(instance):
 

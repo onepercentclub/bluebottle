7cf3a5840bcfad2c7700b7cca7a0b62ce35d4923
Loek van Gent <loek@1procentclub.nl>
2014-12-01 15:32:33 +0100

    Fix task list on project detail

--- bluebottle/bb_projects/static/js/bluebottle/bb_projects/controllers.js
+++ bluebottle/bb_projects/static/js/bluebottle/bb_projects/controllers.js
@@ -178,6 +178,10 @@ App.ProjectController = Em.ObjectController.extend({
         return false;
     }.property('model.owner', 'currentUser.username'),
 
+    tasks: function(){
+        return App.Task.find({project: this.get('id')});
+    }.property('id'),
+
     availableTasks: function () {
         return this.get('tasks').filter(function(task) {
             return task.get("isAvailable");

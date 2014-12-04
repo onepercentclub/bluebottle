ede55105511dcc5bab21b095a6bd70497e6e59f2
Aksel Ethem <aksel.ethem@gmail.com>
2014-12-04 12:39:55 +0100

    add creation of system wallpost for project and fu ndraiser walls

--- bluebottle/bb_donations/signals.py
+++ bluebottle/bb_donations/signals.py
@@ -1,8 +1,9 @@
+from django.dispatch.dispatcher import receiver
 from django_fsm.signals import post_transition
 from bluebottle.bb_donations.donationmail import new_oneoff_donation, successful_donation_fundraiser_mail
 from bluebottle.utils.model_dispatcher import get_order_model
 from bluebottle.utils.utils import StatusDefinition
-from django.dispatch.dispatcher import receiver
+from bluebottle.wallposts.models import SystemWallPost
 
 ORDER_MODEL = get_order_model()
 
@@ -25,3 +26,20 @@ def _order_status_changed(sender, instance, **kwargs):
             for donation in instance.donations.all():
                 successful_donation_fundraiser_mail(donation)
                 new_oneoff_donation(donation)
+
+        # Create Wallpost on project wall
+        post = SystemWallPost()
+        post.content_object = donation.project
+        post.related_object = donation
+        post.author = donation.order.user
+        post.ip = '127.0.0.1'
+        post.save()
+
+        # Create Wallpost on fundraiser wall (if FR present)
+        if donation.fundraiser:
+            fr_post = SystemWallPost()
+            fr_post.content_object = donation.fundraiser
+            fr_post.related_object = donation
+            fr_post.author = donation.order.user
+            fr_post.ip = '127.0.0.1'
+            fr_post.save()

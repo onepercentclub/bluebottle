dea45f9ea3ad8d1fbcf3174ba1165b698a0cba56
rollick <mark@onepercentclub.com>
2014-12-05 11:13:54 +0100

    correctly update payment status when notified by docdata

--- bluebottle/payments_docdata/views.py
+++ bluebottle/payments_docdata/views.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse
 from django.views.generic import View
 from rest_framework import status
 from rest_framework import response
+
 from bluebottle.payments.services import PaymentService
 from bluebottle.payments_docdata.models import DocdataPayment, DocdataTransaction
 from bluebottle.payments_logger.views import GenericStatusChangedNotificationView
 from bluebottle.payments_logger.models import PaymentLogLevels, PaymentLogEntry
 from bluebottle.payments_logger.adapters import PaymentLogAdapter
+from bluebottle.payments.models import OrderPayment
+
 from .models import DocdataPayment
 
 import logging
@@ -27,33 +30,35 @@ class DocdataStatusChangedNotificationView( ):
 class PaymentStatusUpdateView(View):
 
     def get(self, request, **kwargs):
-        payment_cluster_id = kwargs['payment_cluster_id']
+        merchant_order_id = kwargs['merchant_order_id']
         try:
-            payment = DocdataPayment.objects.get(payment_cluster_id=payment_cluster_id)
-        except DocdataPayment.DoesNotExist:
-            raise Exception("Couldn't find DocdataPayment with payment cluster id: {0}".format(payment_cluster_id))
+            order_id = merchant_order_id.split('-')[0]
+            payment = OrderPayment.objects.get(order_id=order_id)
+        except OrderPayment.DoesNotExist:
+            raise Exception("Couldn't find OrderPayment with order id: {0}".format(order_id))
 
-        service = PaymentService(payment.order_payment)
+        service = PaymentService(order_payment)
         service.check_payment_status()
 
         # Get the docdata transactions
         try:
             transaction = DocdataTransaction.objects.get(payment=payment)
+    
+            # Map Docdata payment method naming to our naming convention
+            if transaction:
+                original_method = payment.order_payment.payment_method
+                new_method = service.adapter.PAYMENT_METHODS.get(transaction.payment_method, None)
+                if not new_method:
+                    payment_logger = PaymentLogAdapter()
+                    payment_logger.log(payment, 'warn', 'Payment method \'{0}\' not found for payment with id {1} and order payment with id {2}.'.format(transaction.payment_method, payment.id, payment.order_payment.id))
+                elif new_method != original_method:
+                    payment.order_payment.payment_method = new_method
+                    payment.order_payment.save()
+
+                    payment_logger = PaymentLogAdapter()
+                    payment_logger.log(payment, 'info', 'Payment method changed for payment with id {0} and order payment with id {1}.'.format(payment.id, payment.order_payment.id))
+
         except DocdataPayment.DoesNotExist:
-             raise Exception("Couldn't find DocdataTransaction for payment: {0}".format(payment))
-
-        # Map Docdata payment method naming to our naming convention
-        if transaction:
-            original_method = payment.order_payment.payment_method
-            new_method = service.adapter.PAYMENT_METHODS.get(transaction.payment_method, None)
-            if not new_method:
-                payment_logger = PaymentLogAdapter()
-                payment_logger.log(payment, 'warn', 'Payment method \'{0}\' not found for payment with id {1} and order payment with id {2}.'.format(transaction.payment_method, payment.id, payment.order_payment.id))
-            elif new_method != original_method:
-                payment.order_payment.payment_method = new_method
-                payment.order_payment.save()
-
-                payment_logger = PaymentLogAdapter()
-                payment_logger.log(payment, 'info', 'Payment method changed for payment with id {0} and order payment with id {1}.'.format(payment.id, payment.order_payment.id))
+            logger.error('Could not find transation for payment: {0}'.format(payment.pk))
 
         return HttpResponse('success')

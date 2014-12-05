e3707da1e9586dccbd359a43f85ae06ce0dd5242
rollick <mark@onepercentclub.com>
2014-12-05 11:28:34 +0100

    fix docdata status update to handle order payment id

--- bluebottle/payments_docdata/views.py
+++ bluebottle/payments_docdata/views.py
@@ -32,10 +32,10 @@ class PaymentStatusUpdateView(View):
     def get(self, request, **kwargs):
         merchant_order_id = kwargs['merchant_order_id']
         try:
-            order_id = merchant_order_id.split('-')[0]
-            payment = OrderPayment.objects.get(order_id=order_id)
+            payment_id = merchant_order_id.split('-')[0]
+            payment = OrderPayment.objects.get(pk=payment_id)
         except OrderPayment.DoesNotExist:
-            raise Exception("Couldn't find OrderPayment with order id: {0}".format(order_id))
+            raise Exception("Couldn't find OrderPayment with pk: {0}".format(payment_id))
 
         service = PaymentService(order_payment)
         service.check_payment_status()

a776d3e4b98cc4bb764803767e53cbc3fcf2b77b
rollick <mark@onepercentclub.com>
2014-12-05 12:12:45 +0100

    cleanup docdata status update code

--- bluebottle/payments_docdata/urls/api.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from ..views import DocdataStatusChangedNotificationView
-
-urlpatterns = patterns('',
-    url(r'^$', DocdataStatusChangedNotificationView.as_view(), name='payment-docdata-status-changed'),
-)
--- bluebottle/payments_docdata/views.py
+++ bluebottle/payments_docdata/views.py
@@ -16,17 +16,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class DocdataStatusChangedNotificationView( ):
-
-    def _find_payment(self, order_id):
-        try:
-            payment = DocdataPayment.objects.get(merchant_order_id=order_id)
-        except DocdataPayment.DoesNotExist:
-            logger.error('Could not find order {0} to update payment status.'.format(order_id))
-            return response.Response(status=status.HTTP_403_FORBIDDEN)
-        return payment
-
-
 class PaymentStatusUpdateView(View):
 
     def get(self, request, **kwargs):
@@ -39,27 +28,5 @@ class PaymentStatusUpdateView(View):
 
         service = PaymentService(order_payment)
         service.check_payment_status()
-
-        # Get the docdata transactions
-        try:
-            payment = Payment.objects.get(order_payment=order_payment)
-            transaction = DocdataTransaction.objects.get(payment=payment)
-    
-            # Map Docdata payment method naming to our naming convention
-            if transaction:
-                original_method = payment.order_payment.payment_method
-                new_method = service.adapter.PAYMENT_METHODS.get(transaction.payment_method, None)
-                if not new_method:
-                    payment_logger = PaymentLogAdapter()
-                    payment_logger.log(payment, 'warn', 'Payment method \'{0}\' not found for payment with id {1} and order payment with id {2}.'.format(transaction.payment_method, payment.id, payment.order_payment.id))
-                elif new_method != original_method:
-                    payment.order_payment.payment_method = new_method
-                    payment.order_payment.save()
-
-                    payment_logger = PaymentLogAdapter()
-                    payment_logger.log(payment, 'info', 'Payment method changed for payment with id {0} and order payment with id {1}.'.format(payment.id, payment.order_payment.id))
-
-        except DocdataPayment.DoesNotExist:
-            logger.error('Could not find transation for payment: {0}'.format(payment.pk))
 
         return HttpResponse('success')

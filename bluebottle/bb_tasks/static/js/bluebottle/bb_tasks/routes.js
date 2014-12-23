c2045e50c67c77f73e969619ae4ea5d4c37e3a2a
rollick <rollick@users.noreply.github.com>
2014-12-23 13:24:25 +0100

    Fix isDirty reference - it was being called on the bootstrap modal

--- bluebottle/bb_tasks/static/js/bluebottle/bb_tasks/routes.js
+++ bluebottle/bb_tasks/static/js/bluebottle/bb_tasks/routes.js
@@ -141,7 +141,7 @@ App.TaskRoute = Em.Route.extend(App.ScrollToTop, App.WallRouteMixin, {
                     if (opts.primary) {
                         taskMember.save();
                     }
-                    if (opts.secondary && this.get('isDirty')) {
+                    if (opts.secondary && taskMember.get('isDirty')) {
                         taskMember.rollback();
                     }
                 }

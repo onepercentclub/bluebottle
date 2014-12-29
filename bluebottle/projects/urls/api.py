6efdb5a852ed95325b16a26988ac248050d577ef
Loek van Gent <loek@1procentclub.nl>
2014-12-29 14:31:30 +0100

    Move Projects from Onepercent

---
+++ bluebottle/projects/urls/api.py
@@ -0,0 +1,9 @@
+from bluebottle.projects.views import ManageProjectBudgetLineDetail, ManageProjectBudgetLineList
+from django.conf.urls import patterns, url
+from surlex.dj import surl
+
+
+urlpatterns = patterns('',
+    url(r'^budgetlines/$', ManageProjectBudgetLineList.as_view(), name='project-budgetline-list'),
+    surl(r'^budgetlines/<pk:#>$', ManageProjectBudgetLineDetail.as_view(), name='project-budgetline-detail'),
+)

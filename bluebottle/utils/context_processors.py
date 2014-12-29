f4e231b13c4687839d59e45c52ef5029767a011e
Loek van Gent <loek@1procentclub.nl>
2014-12-29 14:00:59 +0100

    Get BB_APPS from settings

--- bluebottle/utils/context_processors.py
+++ bluebottle/utils/context_processors.py
@@ -4,17 +4,9 @@ from django.conf import settings
 
 
 def installed_apps_context_processor(request):
-    bb_apps = []
-    for app in settings.INSTALLED_APPS:
-        if app[:11] == 'bluebottle.':
-            # Ignore some standard apps
-            if app[11:] not in ['common', 'admin_dashboard', 'contentplugins', 'auth', 'redirects',
-                                'donations', 'orders', 'fundraisers', 'tasks', 'bb_payouts', 'payouts',
-                                'payments_logger', 'widget', 'bb_follow']:
-                bb_apps.append(app[11:])
     context = {
         'installed_apps': settings.INSTALLED_APPS,
-        'bb_apps': bb_apps,
+        'bb_apps': settings.BB_APPS,
     }
     return context
 

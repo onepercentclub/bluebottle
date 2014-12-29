c40e5a3045943a77f241b258db8bc13b1d01049d
Loek van Gent <loek@1procentclub.nl>
2014-12-29 15:56:05 +0100

    Add task tests from 1%, remove Mpesae refs

--- bluebottle/bb_tasks/static/js/bluebottle/bb_tasks/models.js
+++ bluebottle/bb_tasks/static/js/bluebottle/bb_tasks/models.js
@@ -155,12 +155,11 @@ App.Task = DS.Model.extend({
         return this.get('status') == 'completed';
     }.property('status'),
 
-    ////
-    // Override this function to change what determines an available task
     isAvailable: function () {
         var now = new Date();
-        return this.get('isStatusOpen') && this.get('deadline') > now;
-    }.property('isStatusOpen', 'deadline'),
+        return (this.get('isStatusOpen') || this.get('isStatusInProgress'))
+            && this.get('people_needed') > this.get('membersCount') && this.get('deadline') > now;
+    }.property('isStatusOpen', 'isStatusInProgress', 'people_needed', 'membersCount', 'deadline'),
 
     isUnavailable: function () {
         return !this.get('isAvailable');

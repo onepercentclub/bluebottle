baa6bbae4bfd15d7138b8574f1dbb614554f661c
Loek van Gent <loek@1procentclub.nl>
2015-01-05 15:46:27 +0100

    Add some migrations to change Client model

--- bluebottle/clients/models.py
+++ bluebottle/clients/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
 from tenant_schemas.models import TenantMixin
 
 class Client(TenantMixin):
     name = models.CharField(max_length=100)
-    paid_until =  models.DateField()
-    on_trial = models.BooleanField()
-    created_on = models.DateField(auto_now_add=True)
+    client_name = models.CharField(max_length=100)
+    created = CreationDateTimeField()
+    updated = ModificationDateTimeField()
 
-    # default true, schema will be automatically created and synced when it is saved
     auto_create_schema = True
-    
+

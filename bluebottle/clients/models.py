31c68ccb0301bb0f6d38ad72560905f4e524fad9
Loek van Gent <loek@1procentclub.nl>
2015-01-08 17:39:24 +0100

    Add unique to client_name

--- bluebottle/clients/models.py
+++ bluebottle/clients/models.py
@@ -4,7 +4,7 @@ from tenant_schemas.models import TenantMixin
 
 class Client(TenantMixin):
     name = models.CharField(max_length=100)
-    client_name = models.CharField(max_length=100)
+    client_name = models.CharField(max_length=100, unique=True)
     created = CreationDateTimeField()
     updated = ModificationDateTimeField()
 

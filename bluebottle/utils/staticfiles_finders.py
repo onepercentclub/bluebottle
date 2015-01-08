02a8745ddc0e9618f79e144a92258ff8ac3f35aa
Loek van Gent <loek@1procentclub.nl>
2015-01-08 17:01:37 +0100

    Add tenant static files finder

---
+++ bluebottle/utils/staticfiles_finders.py
@@ -0,0 +1,34 @@
+from django.utils._os import safe_join
+import os
+from django.conf import settings
+from django.contrib.staticfiles.finders import FileSystemFinder
+from bluebottle.clients.models import Client
+
+
+class TenantStaticFilesFinder(FileSystemFinder):
+
+
+    def find(self, path, all=False):
+        """
+        Looks for files in the client static directories.
+        static/assets/greatbarier/images/logo.jpg
+        will translate to
+        MULTITENANT_DIR/greatbarier/static/images/logo.jpg
+
+        """
+        matches = []
+        tenants = Client.objects.all()
+        tenant_dir = getattr(settings, 'MULTITENANT_DIR', None)
+        if not tenant_dir:
+            return matches
+
+        for tenant in tenants:
+            if "{0}/".format(tenant.client_name) in path:
+                tenant_path = path.replace('{0}/'.format(tenant.client_name),
+                                           '{0}/static/'.format(tenant.client_name))
+                print tenant_path
+                local_path = safe_join(tenant_dir, tenant_path)
+                print local_path
+                if os.path.exists(local_path):
+                    return local_path
+        return

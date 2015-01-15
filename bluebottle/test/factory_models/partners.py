e4fd9ceeade9f106b4985cbefa4d842b27012a53
Loek van Gent <loek@1procentclub.nl>
2015-01-15 12:26:49 +0100

    Fix partner factory

--- bluebottle/test/factory_models/partners.py
+++ bluebottle/test/factory_models/partners.py
@@ -6,4 +6,5 @@ class PartnerFactory(factory.DjangoModelFactory):
     FACTORY_FOR = PartnerOrganization
     FACTORY_DJANGO_GET_OR_CREATE = ('name', )
 
-    name = factory.Sequence(lambda n: 'Partner_{0}'.format(n))
+    name = factory.Sequence(lambda n: 'Partner {0}'.format(n))
+    slug = factory.Sequence(lambda n: 'partner-{0}'.format(n))

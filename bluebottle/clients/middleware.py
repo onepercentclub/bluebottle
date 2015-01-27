80cb88306f719413590922185cb7eb77bccaa74c
Ivo van der Wijk <github@in.m3r.nl>
2015-01-27 11:51:24 +0100

    implement tenant specific properties with middleware

---
+++ bluebottle/clients/middleware.py
@@ -0,0 +1,63 @@
+import os
+from threading import local
+
+from django.db import connection
+from django.conf import settings
+from django.utils._os import safe_join
+
+class TenantProperties(local):
+    """
+        A tenant propery file is read from the
+        MULTI_TENANT_DIR/<tenant_name>/properties.py
+        It can contain arbitrary python expressions and a reference to
+        'settings' will be available
+    """
+    tenant_properties = {}
+
+    def set_tenant(self, tenant):
+        self.tenant = tenant
+        self.tenant_properties = {}
+        ##
+        ## Always default to standard django settings, e.g.
+        ## when tenant has no specific config, has no directory
+        ## or when no MULTI_TENANT_DIR is configured
+        try:
+            propsmod = safe_join(settings.MULTI_TENANT_DIR,
+                                         tenant.name,
+                                         "properties.py")
+            ## try to load tenant specific properties. We're using execfile since tenant
+            ## directories are not pythonpackages (e.g. no __init__.py)
+            execfile(propsmod, dict(settings=settings),
+                     self.tenant_properties)
+        except (ImportError, AttributeError, IOError):
+            pass
+
+    def __getattr__(self, k):
+        """
+            Search (in that specific order) tenant properties and settings. Raise
+            AttributeError if not found.
+        """
+        try:
+            return self.tenant_properties[k]
+        except (AttributeError, KeyError):
+            ## may raise AttributeError which is the behaviour we expect
+            return getattr(settings, k)
+
+
+properties = TenantProperties()
+
+
+class TenantPropertiesMiddleware(object):
+    def process_request(self, request):
+        """
+            generically find tenantfolder from request (or some other means)
+            load tenant specific configuration
+
+        """
+        try:
+            tenant = connection.tenant
+        except AttributeError:
+            tenant = None
+
+        if tenant:
+            properties.set_tenant(tenant)

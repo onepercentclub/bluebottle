3ff54f35b083721811ce20b44b19d588b44c84fa
Loek van Gent <loek@1procentclub.nl>
2015-01-29 12:09:25 +0100

    Fix tests for clients middleware

--- bluebottle/clients/tests/test_properties.py
+++ bluebottle/clients/tests/test_properties.py
@@ -13,7 +13,7 @@ class TestProperties(TestCase):
         """ A match found in the client properties """
         with mock.patch("bluebottle.clients.middleware.settings", foo=1):
             p = TenantProperties()
-            p.tenant_properties={'foo':2}
+            p.tenant_properties = {'foo': 2}
 
             self.failUnless(p.foo == 2)
             self.failUnless(hasattr(p, 'foo'))
@@ -36,8 +36,8 @@ class TestProperties(TestCase):
 
     def test_verify_settings(self):
         with mock.patch("bluebottle.clients.middleware.settings",
-                        MULTI_TENANT_DIR='/tmp/') as settings , \
-             mock.patch("__builtin__.execfile") as execfile:
+                        MULTI_TENANT_DIR='/tmp/') as settings, \
+                mock.patch("__builtin__.execfile") as execfile:
             properties.set_tenant(Mock(client_name='testtenant'))
             self.assertEquals(execfile.call_args[0][1]['settings'], settings)
 
@@ -57,7 +57,7 @@ class TestTenantMiddleware(TestCase):
             works """
         with mock.patch("bluebottle.clients.middleware.settings", foo=42), \
              mock.patch("bluebottle.clients.middleware.connection",
-                           Mock(**{"tenant.client_name":"dontexist"})):
+                        Mock(**{"tenant.client_name": "dontexist"})):
             self.middleware.process_request(Mock())
             self.assertEquals(properties.foo, 42)
 
@@ -66,8 +66,10 @@ class TestTenantMiddleware(TestCase):
         with mock.patch("bluebottle.clients.middleware.settings",
                         MULTI_TENANT_DIR="/some/client/path/"), \
              mock.patch("bluebottle.clients.middleware.connection",
-                        Mock(**{"tenant.client_name":"valid"})), \
+                        Mock(**{"tenant.client_name": "valid"})), \
              mock.patch("__builtin__.execfile") as execfile:
             self.middleware.process_request(Mock())
-            self.assertEquals(execfile.call_args[0][0],
+            self.assertEquals(execfile.call_args_list[0][0][0],
                               "/some/client/path/valid/properties.py")
+            self.assertEquals(execfile.call_args_list[1][0][0],
+                              "/some/client/path/valid/secrets.py")

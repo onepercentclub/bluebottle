0a0b0ed435d7257e3b3d158a8e9de35c4dbeec9f
Aksel Ethem <aksel.ethem@gmail.com>
2015-02-04 11:19:01 +0100

    added suggestions app and necessary ember and tpl updates

---
+++ bluebottle/suggestions/static/js/bluebottle/suggestions/controllers.js
@@ -0,0 +1,10 @@
+App.SuggestionListController = Em.ArrayController.extend({
+    totalSuggestions: function() {
+        return this.get('model.length');
+    }.property("@each"),
+
+    hasSuggestions: function() {
+        return this.get('totalSuggestions') > 0;
+    }
+});
+
--- bluebottle/suggestions/static/js/bluebottle/suggestions/models.js
+++ bluebottle/suggestions/static/js/bluebottle/suggestions/models.js
@@ -7,12 +7,12 @@ App.Suggestion = DS.Model.extend({
     theme: DS.attr('string'),
     destination: DS.attr('string'),
 
-    org_name = DS.attr('string'),
-    org_contactname = DS.attr('string'),
-    org_email = DS.attr('string'),
-    org_phone = DS.attr('string'),
-    org_website = DS.attr('string'),
+    org_name : DS.attr('string'),
+    org_contactname : DS.attr('string'),
+    org_email : DS.attr('string'),
+    org_phone : DS.attr('string'),
+    org_website : DS.attr('string'),
 
-    status = DS.attr('string'),
+    status : DS.attr('string'),
     project: DS.belongsTo("App.Project")
 });

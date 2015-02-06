9ab29a47f0bc31c44fdfecd4e934777e785ca5af
Aksel Ethem <aksel.ethem@gmail.com>
2015-02-06 12:38:27 +0100

    updated tpl reference

--- bluebottle/suggestions/static/js/bluebottle/suggestions/controllers.js
+++ bluebottle/suggestions/static/js/bluebottle/suggestions/controllers.js
@@ -1,14 +1,36 @@
 App.SuggestionController = Em.ObjectController.extend({
+
+    updateSuggestion: function(project) {
+        var model = this.get('model');
+        model.set('status', 'in_progress');
+        model.set('project', project);
+        model.save();
+    },
+
+    createProject: function() {
+        var model = this.get('model');
+
+        var project = App.MyProject.createRecord({
+            title: model.get('title'),
+            pitch: model.get('pitch'),
+            theme: model.get('theme')
+        });
+        project.save();
+        return project;
+    },
+
     actions: {
-        adoptSuggestion: function(suggestion) {
-            App.MyProject.createRecord({
-                title: suggestion.get('title'),
-                pitch: suggestion.get('pitch'),
-                theme: suggestion.get('theme')
-            }).save().then(function (project) {
-                // Redirect to project edit flow
-                this.transitionTo('myProject.pitch', project)
-            });
+
+        adoptSuggestion: function() {
+            
+            this.send('closeModal');
+
+            var project = this.createProject();
+            
+            this.updateSuggestion(project);
+
+            // Redirect to project edit flow
+            this.transitionTo('myProject.pitch', project)
 
         }
     }

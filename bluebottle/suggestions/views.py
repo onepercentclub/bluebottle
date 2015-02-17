ebd2907b671b423dce723c8045f21ff06368ac4d
Aksel Ethem <aksel.ethem@gmail.com>
2015-02-17 10:33:48 +0100

    return only non-expired suggestions from API

--- bluebottle/suggestions/views.py
+++ bluebottle/suggestions/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from bluebottle.suggestions.models import Suggestion
@@ -9,8 +10,8 @@ class SuggestionList(generics.ListCreateAPIView):
     serializer_class = SuggestionSerializer
 
     def get_queryset(self):
-        qs = Suggestion.objects.all()
-        
+        qs = Suggestion.objects.filter(deadline__gte=date.today())
+
         destination = self.request.QUERY_PARAMS.get('destination', None)
         status = self.request.QUERY_PARAMS.get('status', None)
         project_slug = self.request.QUERY_PARAMS.get('project_slug', None)

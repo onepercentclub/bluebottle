ef789ad97e3d9d6506a2cfaf5f0eeafa503f659d
Aksel Ethem <aksel.ethem@gmail.com>
2015-03-16 09:33:16 +0100

    activate the correct translation when sending a welcome email

--- bluebottle/bb_accounts/utils.py
+++ bluebottle/bb_accounts/utils.py
@@ -1,12 +1,22 @@
 import re
-from django.conf import settings
 from django.template import loader
+from django.utils import translation
 
 from bluebottle.clients.context import ClientContext
 from bluebottle.clients.mail import construct_from_header
 from bluebottle.clients.utils import tenant_url, tenant_name
 
+from bluebottle.clients import properties
+
+
 def send_welcome_mail(user=None):
+    cur_language = translation.get_language()
+
+    if user and user.primary_language:
+        translation.activate(user.primary_language)
+    else:
+        translation.activate(properties.LANGUAGE_CODE)
+
     c = ClientContext({
         'email': user.email,
         'site': tenant_url(),
@@ -26,7 +36,11 @@ def send_welcome_mail(user=None):
     email = loader.render_to_string(email_template_name, c)
 
     tenant_from = construct_from_header()
+
+    translation.activate(cur_language)
+
     user.email_user(subject, email, from_email=tenant_from)
+
 
 def valid_email(email=None):
     """Returns True if argument is a string with valid email adddress"""
@@ -37,4 +51,3 @@ def valid_email(email=None):
     if p.match(email):
         return True
     return False
-    

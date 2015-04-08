45e9bf77e7f234963f02f1158582141fcd305209
Ivo van der Wijk <github@in.m3r.nl>
2015-04-08 15:30:12 +0200

    fix filter; test all states

---
+++ bluebottle/bb_orders/tests/test_admin.py
@@ -0,0 +1,35 @@
+import mock
+
+from django.db.models.query import QuerySet
+
+from bluebottle.test.utils import BluebottleTestCase
+from bluebottle.utils.model_dispatcher import get_order_model
+
+from ..admin import OrderStatusFilter, BaseOrderAdmin
+
+ORDER_MODEL = get_order_model()
+
+def generate_order_test(id):
+    def generated_test(self):
+        """ if a valid state is passed, it must become part of the query """
+        filter = OrderStatusFilter(None, {'status__exact':id},
+                                   ORDER_MODEL, BaseOrderAdmin)
+        queryset = mock.Mock(spec=QuerySet)
+
+        filter.queryset({}, queryset)
+
+        self.failUnless(queryset.filter.call_args)  # has it been called?
+        self.failUnless(queryset.filter.call_args[1] == {'status':id})
+
+    return generated_test
+
+class OrderTestMeta(type):
+    def __init__(cls, what, bases=None, dict=None):
+        super(OrderTestMeta, cls).__init__(what, bases, dict)
+
+        for (id, label) in ORDER_MODEL.STATUS_CHOICES:
+            setattr(cls, 'test_query_state_' + id, generate_order_test(id))
+
+class OrderTestAdmin(BluebottleTestCase):
+    __metaclass__ = OrderTestMeta
+

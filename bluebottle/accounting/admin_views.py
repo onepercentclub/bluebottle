908103429aef436534b47654cc62ed34e790f73f
Sergei Maertens <sergei@maykinmedia.nl>
2015-04-08 11:48:16 +0200

    Three possible manual entry actions: forms in template

--- bluebottle/accounting/admin_views.py
+++ bluebottle/accounting/admin_views.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.views.generic import FormView
+from django.views.generic.detail import SingleObjectMixin
 from django.contrib import admin
 from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 
-from bluebottle.journals.models import DonationJournal, ProjectPayoutJournal, OrganizationPayoutJournal
+from bluebottle.journals.models import ProjectPayoutJournal, OrganizationPayoutJournal
+from bluebottle.utils.model_dispatcher import get_donation_model
 from .models import BankTransaction
 
 
@@ -22,23 +24,46 @@ def modelformfactory(model, rel_field):
     return form_class
 
 
-class UnknownTransactionView(FormView):
+class UnknownTransactionView(SingleObjectMixin, FormView):
+
     """
     Show the three forms at once, each one submittable to its own url/handler
     """
     model = BankTransaction
     related_models = [
-        (DonationJournal, 'donation'),
         (ProjectPayoutJournal, 'payout'),
         (OrganizationPayoutJournal, 'payout'),
     ]
     template_name = 'admin/accounting/banktransaction/unknown_transaction.html'
+    context_object_name = 'transaction'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(UnknownTransactionView, self).get(request, *args, **kwargs)
+
+    def get_initial(self):
+        return {'amount': self.object.amount}
+
+    def get_donation_form(self):
+        Donation = get_donation_model()
+        widgets = {
+            'project': ForeignKeyRawIdWidget(Donation._meta.get_field('project').rel, admin.site),
+            'fundraiser': ForeignKeyRawIdWidget(Donation._meta.get_field('fundraiser').rel, admin.site),
+            'order': ForeignKeyRawIdWidget(Donation._meta.get_field('order').rel, admin.site),
+        }
+        ModelForm = forms.models.modelform_factory(
+            Donation,
+            fields=('amount', 'project', 'fundraiser', 'order', 'anonymous'),
+            widgets=widgets
+        )
+        ModelForm.title = Donation._meta.verbose_name
+        return ModelForm(initial=self.get_initial())
 
     def get_form_class(self):
         return [modelformfactory(model, rel_field) for (model, rel_field) in self.related_models]
 
     def get_form(self, form_classes):
-        return [form_class(**self.get_form_kwargs()) for form_class in form_classes]
+        return [self.get_donation_form()] + [form_class(**self.get_form_kwargs()) for form_class in form_classes]
 
     def get_context_data(self, **kwargs):
         kwargs['opts'] = self.model._meta

3010a0b45cc5818f1594a929dac86cecbcf00798
Loek van Gent <loek@1procentclub.nl>
2015-04-27 17:23:01 +0200

    Change description on bank receipt

--- bluebottle/payments_docdata/adapters.py
+++ bluebottle/payments_docdata/adapters.py
@@ -190,7 +190,7 @@ class DocdataPaymentAdapter(BasePaymentAdapter):
 
         client = gateway.DocdataClient(testing_mode)
 
-        info_text = _("Bluebottle donation %(payment_id)s") % {'payment_id': self.order_payment.id}
+        info_text = _("Booking Cares %(payment_id)s") % {'payment_id': self.order_payment.id}
 
         response = client.create(
             merchant=merchant,

a5162622f60117a7b82fbe6c33baf64be000d376
Aksel Ethem <aksel.ethem@gmail.com>
2015-05-13 14:39:50 +0200

    added checked token model and migration

--- bluebottle/members/models.py
+++ bluebottle/members/models.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.db import models
+
 from bluebottle.bb_accounts.models import BlueBottleBaseUser
 
 
@@ -9,5 +12,22 @@ GROUP_PERMS = {
     }
 }
 
+
 class Member(BlueBottleBaseUser):
     pass
+
+
+class CheckedToken(models.Model):
+    """
+    Stores the used tokens for safety-checking purposes.
+    """
+    token = models.CharField(max_length=300)
+    timestamp = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        ordering = ('timestamp', 'user__username')
+
+    def __unicode__(self):
+        return '{0} - {1}, {2}'.format(
+            self.token, self.timestamp, self.user.username)

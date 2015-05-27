e2459b617b65b40ca25092634210d34b5bf17676
Ivo van der Wijk <github@in.m3r.nl>
2015-05-27 13:27:04 +0200

    remove unused / disabled api url

--- bluebottle/suggestions/urls/api.py
+++ bluebottle/suggestions/urls/api.py
@@ -8,8 +8,6 @@ urlpatterns = patterns(
 #     url(r'^public/$', PublicSuggestionList.as_view(), name='public_suggestion_list'),
     url(r'^(?P<pk>\d+)$', SuggestionDetail.as_view(),
         name='suggestion_detail'),
-#    url(r'^(?P<token>[a-z0-9-]+)$', SuggestionToken.as_view(),
-#        name='suggestion_token'),
     url(r'^(?P<token>[a-z0-9-]+)/validate/$', SuggestionToken.as_view(),
         name='suggestion_token_validate')
 )

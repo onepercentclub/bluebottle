ebd61a4d30464b8aa5a6bd4cbc1a3f4e9372a66b
Ernst Odolphi <ernst@onepercentclub.com>
2015-05-27 09:06:05 +0200

    Make it possible to transition payments from FAILED to SETTLED

--- bluebottle/payments/models.py
+++ bluebottle/payments/models.py
@@ -168,7 +168,7 @@ class OrderPayment(models.Model, FSMTransition):
         pass
 
     @transition(field=status, save=True, source=[StatusDefinition.AUTHORIZED,
-                StatusDefinition.STARTED, StatusDefinition.CANCELLED],
+                StatusDefinition.STARTED, StatusDefinition.CANCELLED, StatusDefinition.FAILED],
                 target=StatusDefinition.SETTLED)
     def settled(self):
         self.closed = now()

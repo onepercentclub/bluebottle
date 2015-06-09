7d4e143b7a23f7f83852a6556e5d2a798864fc18
Ivo van der Wijk <github@in.m3r.nl>
2015-06-09 17:32:09 +0200

    flyer sharing endpoint

--- bluebottle/utils/views.py
+++ bluebottle/utils/views.py
@@ -40,6 +40,97 @@ class TagSearch(views.APIView):
         data = [tag.name for tag in Tag.objects.filter(name__startswith=search).all()[:20]]
         return response.Response(data)
 
+from bluebottle.utils.email_backend import send_mail
+from django.utils.translation import ugettext as _
+from collections import namedtuple
+
+from django.template.loader import render_to_string
+from bluebottle.utils.model_dispatcher import get_project_model
+from sorl.thumbnail.shortcuts import get_thumbnail
+from bluebottle.clients.context import ClientContext
+
+PROJECT_MODEL = get_project_model()
+
+class ShareFlyer(views.APIView):
+    def project_args(self, projectid):
+        try:
+            project = PROJECT_MODEL.objects.get(slug=projectid)
+        except PROJECT_MODEL.DoesNotExist:
+            return None
+
+        ## make sure url is tenant aware
+        project_image = self.request.build_absolute_uri(
+                        settings.MEDIA_URL + unicode(get_thumbnail(project.image,
+                                                                   "400x380")))
+        args = dict(
+            project_title=project.title,
+            project_pitch=project.pitch,
+            project_image=project_image
+        )
+
+        return args
+
+    def get(self, request, slug=None):
+        """
+            return the bare email as preview. We do not (always) have access to the
+            logged in user so use fake data
+        """
+        args = self.project_args(slug)
+
+        if args is None:
+            return HttpResponseNotFound()
+
+        args['share_name'] = "John Doe"
+        args['share_email'] = "john@example.com"
+        args['sender_name'] = "Jane Doe"
+        args['sender_email'] = "jane@example.com"
+
+        args['share_motivation'] = """(sample motivation) Great to see you again this afternoon. Attached you'll find a project flyer for the big event next friday. If you care to join in, please let me know, I'll add you as my +1 on the attendee list.
+
+        Hope to hear from you soon
+
+        Cheers,
+
+        Jane"""
+        # strip outer <html> stuff??
+        result = render_to_string('utils/mails/share_flyer.mail.html', {},
+                                  ClientContext(args))
+        return response.Response({'preview': result})
+
+    def post(self, request, slug=None, **data):
+        """ Let's assume a user has to be logged in in order to share a flyer -
+            it can't be done anonymously (in which case we'd have to ask for
+            personal details """
+
+        args = self.project_args(slug)
+
+        if args is None:
+            return HttpResponseNotFound()
+
+        sender_name = self.request.user.get_full_name() or self.request.user.username
+        sender_email = self.request.user.email
+        share_name = data.get('share_name', None)
+        share_email = data.get('share_email', None)
+        share_motivation = data.get('share_motivation', None)
+        share_cc = data.get('share_cc', 'false') == 'true'
+
+        args.update(dict(
+            template_name='utils/mails/share_flyer.mail',
+            subject=_('%(name)s wants to share a project with you!') % dict(name=sender_name),
+            to=namedtuple("Receiver", "email")(email=share_email),
+            from_email=sender_email,
+            share_name=share_name,
+            share_email=share_email,
+            share_motivation=share_motivation,
+            sender_name=sender_name,
+            sender_email=sender_email,
+        ))
+        if share_cc:
+            args['cc'] = [sender_email]
+
+        result = send_mail(**args)
+
+        return response.Response(result, status=200)
 
 # Non API views
 # Download private documents based on content_type (id) and pk

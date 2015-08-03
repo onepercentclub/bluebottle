d30eb2af66838f25fc8e5ebe333424b347a1f5a6
Loek van Gent <loek@1procentclub.nl>
2015-08-03 16:12:23 +0200

    Override view to reset admin password

--- bluebottle/auth/views.py
+++ bluebottle/auth/views.py
@@ -1,4 +1,14 @@
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.views import password_reset
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.forms import PasswordResetForm
+from django.db import connection
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, QueryDict
+from django.template.response import TemplateResponse
+
+
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -7,6 +17,7 @@ from rest_framework.authentication import get_authorization_header
 from rest_framework import parsers, renderers
 from rest_framework import status
 from social.apps.django_app.utils import strategy
+
 #from social_auth.decorators import
 from datetime import datetime
 
@@ -49,3 +60,49 @@ def register_by_access_token(request, backend):
         else:
             return None
     return None
+
+@csrf_protect
+def admin_password_reset(request, is_admin_site=False,
+                         template_name='registration/password_reset_form.html',
+                         email_template_name='registration/password_reset_email.html',
+                         subject_template_name='registration/password_reset_subject.txt',
+                         password_reset_form=PasswordResetForm,
+                         token_generator=default_token_generator,
+                         post_reset_redirect=None,
+                         from_email=None,
+                         current_app=None,
+                         extra_context=None):
+
+    tenant = connection.get_tenant()
+
+    extra_context = {'domain': tenant.domain_url, 'tenant': tenant, 'site_name': tenant.name}
+
+    if post_reset_redirect is None:
+        post_reset_redirect = reverse('password_reset_done')
+    else:
+        post_reset_redirect = resolve_url(post_reset_redirect)
+
+    if request.method == "POST":
+        form = password_reset_form(request.POST)
+        if form.is_valid():
+            opts = {
+                'use_https': request.is_secure(),
+                'token_generator': token_generator,
+                'from_email': from_email,
+                'email_template_name': email_template_name,
+                'subject_template_name': subject_template_name,
+                'request': request,
+            }
+            tenant = connection.get_tenant()
+            domain_override = tenant.domain_url
+            opts = dict(opts, domain_override=domain_override)
+            form.save(**opts)
+            return HttpResponseRedirect(post_reset_redirect)
+    else:
+        form = password_reset_form()
+    context = {
+        'form': form,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context, current_app=current_app)

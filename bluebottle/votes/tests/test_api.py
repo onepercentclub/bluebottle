b4a700e556301dd5d07e91f9deed3150af4ebf0a
Ernst Odolphi <ernst@onepercentclub.com>
2015-08-11 15:35:54 +0200

    Add tests, make it possible to filter by voter

--- bluebottle/votes/tests/test_api.py
+++ bluebottle/votes/tests/test_api.py
@@ -32,10 +32,21 @@ class ProjectVotesAPITestcase(BluebottleTestCase):
         response = self.client.post(self.vote_url, {}, token=self.user_token)
         self.assertEqual(response.status_code, 201)
 
+    def test_vote_project_does_not_exist(self):
+        vote_url = reverse('project_votes_list', kwargs={'project_id': 1234})
+        response = self.client.post(vote_url, {}, token=self.user_token)
+        self.assertEqual(response.status_code, 404)
+
+    def test_vote_unauthenticated(self):
+        response = self.client.post(self.vote_url, {})
+        self.assertEqual(response.status_code, 403)
+
+
     def test_vote_twice(self):
         self.client.post(self.vote_url, {}, token=self.user_token)
         response = self.client.post(self.vote_url, {}, token=self.user_token)
         self.assertEqual(response.status_code, 400)
+
 
     def test_get_votes(self):
         for i in range(11):
@@ -50,4 +61,18 @@ class ProjectVotesAPITestcase(BluebottleTestCase):
         self.assertEqual(data['count'], 11)
         self.assertEqual(len(data['results']), 10)
 
-        import ipdb;ipdb.set_trace()
+    def test_get_user_votes(self):
+        for i in range(11):
+            user = BlueBottleUserFactory.create()
+            VoteFactory.create(voter=user, project=self.project)
+
+        VoteFactory.create(voter=self.user, project=self.project)
+
+        vote_url = self.vote_url + '?voter_id={}'.format(self.user.id)
+        response = self.client.get(vote_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+        self.assertEqual(data['count'], 1)
+        self.assertEqual(len(data['results']), 1)
--- bluebottle/votes/views.py
+++ bluebottle/votes/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, exceptions
+from django.http import Http404
+
 
 from bluebottle.projects.models import Project
 from bluebottle.votes.models import Vote
@@ -6,15 +8,25 @@ from bluebottle.votes.serializers import VoteSerializer
 
 
 class ProjectVoteList(generics.ListCreateAPIView):
+    """ Retrieve the votes belong to a project. Or cast a vote as a user.
+
+    Voting cannot happen twice.
+
+    The list can be filtered adding voter_id=<id of user>
+    """
     model = Vote
     paginate_by = 10
     serializer_class = VoteSerializer
 
     def get_queryset(self):
         """ Filter on project id"""
-        project_id = self.kwargs['project_id']
+        queryset = super(ProjectVoteList, self).get_queryset().filter(
+            project_id=self.kwargs['project_id'])
 
-        return super(ProjectVoteList, self).get_queryset().filter(project_id=project_id)
+        if 'voter_id' in self.request.QUERY_PARAMS:
+            queryset = queryset.filter(voter_id=self.request.QUERY_PARAMS['voter_id'])
+
+        return queryset
 
     def pre_save(self, obj):
         """
@@ -22,12 +34,19 @@ class ProjectVoteList(generics.ListCreateAPIView):
 
         Check that a user has not vote before
         """
-        project_id = self.kwargs['project_id']
-        obj.project = Project.objects.get(id=project_id)
+        if self.request.user.is_anonymous():
+            raise exceptions.PermissionDenied()
+
         try:
             self.get_queryset().get(voter=self.request.user)
             raise exceptions.ParseError('You cannot vote twice')
         except Vote.DoesNotExist:
             pass
+
+        try:
+            project_id = self.kwargs['project_id']
+            obj.project = Project.objects.get(id=project_id)
+        except Project.DoesNotExist:
+            raise Http404('Project not found')
 
         obj.voter = self.request.user

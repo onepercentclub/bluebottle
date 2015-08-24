cb41aab46b853fb90a0c4c3e6e32e72b8126b745
Loek van Gent <loek@1procentclub.nl>
2015-08-24 11:55:17 +0200

    Code clean up bb_accounts

--- bluebottle/bb_accounts/tests/test_api.py
+++ bluebottle/bb_accounts/tests/test_api.py
@@ -13,6 +13,7 @@ class UserApiIntegrationTest(BluebottleTestCase):
     """
     Integration tests for the User API.
     """
+
     def setUp(self):
         super(UserApiIntegrationTest, self).setUp()
 
@@ -48,8 +49,10 @@ class UserApiIntegrationTest(BluebottleTestCase):
         for field in serializer_fields:
             self.assertTrue(field in response.data)
 
-        excluded_fields = ['email', 'address', 'newsletter', 'campaign_notifications',
-                           'birthdate', 'gender', 'first_name', 'last_name', 'username', 'password']
+        excluded_fields = ['email', 'address', 'newsletter',
+                           'campaign_notifications',
+                           'birthdate', 'gender', 'first_name', 'last_name',
+                           'username', 'password']
 
         for field in excluded_fields:
             self.assertFalse(field in response.data)
@@ -73,9 +76,11 @@ class UserApiIntegrationTest(BluebottleTestCase):
                              'primary_language', 'about_me', 'location',
                              'project_count', 'donation_count', 'date_joined',
                              'fundraiser_count', 'task_count', 'time_spent',
-                             'website', 'twitter', 'facebook', 'skypename', 'email',
+                             'website', 'twitter', 'facebook', 'skypename',
+                             'email',
                              'address', 'newsletter', 'campaign_notifications',
-                             'birthdate', 'gender', 'first_name', 'last_name', 'username']
+                             'birthdate', 'gender', 'first_name', 'last_name',
+                             'username']
 
         for field in serializer_fields:
             self.assertTrue(field in response.data)
@@ -93,12 +98,16 @@ class UserApiIntegrationTest(BluebottleTestCase):
         # Profile should not be able to be updated by anonymous users.
         full_name = {'first_name': 'Nijntje', 'last_name': 'het Konijntje'}
         response = self.client.put(user_profile_url, full_name)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN, response.data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN,
+                         response.data)
 
         # Profile should be able to be updated by logged in user.
-        response = self.client.put(user_profile_url, full_name, token=self.user_1_token)
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
-        self.assertEqual(response.data['first_name'], full_name.get('first_name'))
+        response = self.client.put(user_profile_url, full_name,
+                                   token=self.user_1_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         response.data)
+        self.assertEqual(response.data['first_name'],
+                         full_name.get('first_name'))
         self.assertEqual(response.data['last_name'], full_name.get('last_name'))
 
         self.client.logout()
@@ -109,14 +118,17 @@ class UserApiIntegrationTest(BluebottleTestCase):
         """
         # Test unauthenticated user doesn't return 500 error.
         response = self.client.get(self.current_user_api_url)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         response.data)
 
     def test_current_user(self):
         """
         Test retrieving the currently logged in user after login.
         """
-        response = self.client.get(self.current_user_api_url, token=self.user_1_token)
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        response = self.client.get(self.current_user_api_url,
+                                   token=self.user_1_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         response.data)
         self.assertEqual(response.data['first_name'], self.user_1.first_name)
 
         self.client.logout()
@@ -128,15 +140,19 @@ class UserApiIntegrationTest(BluebottleTestCase):
         # Create a user.
         new_user_email = 'nijntje27@hetkonijntje.nl'
         new_user_password = 'testing'
-        response = self.client.post(self.user_create_api_url, {'email': new_user_email,
-                                                               'password': new_user_password})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+        response = self.client.post(self.user_create_api_url,
+                                    {'email': new_user_email,
+                                     'password': new_user_password})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED,
+                         response.data)
         token = "JWT {0}".format(response.data['jwt_token'])
         user_id = response.data['id']
 
         # Test that the email field is required on user create.
-        response = self.client.post(self.user_create_api_url, {'password': new_user_password})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
+        response = self.client.post(self.user_create_api_url,
+                                    {'password': new_user_password})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         response.data)
         self.assertEqual(response.data['email'][0], 'This field is required.')
 
     def test_generate_username(self):
@@ -146,52 +162,65 @@ class UserApiIntegrationTest(BluebottleTestCase):
         new_user_password = 'password'
 
         # Test username generation with duplicates.
-        response = self.client.post(self.user_create_api_url, {'first_name': first_name,
-                                                               'last_name': last_name,
-                                                               'email': new_user_email,
-                                                               'password': new_user_password})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+        response = self.client.post(self.user_create_api_url,
+                                    {'first_name': first_name,
+                                     'last_name': last_name,
+                                     'email': new_user_email,
+                                     'password': new_user_password})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED,
+                         response.data)
         self.assertEqual(response.data['username'], new_user_email)
 
     def test_password_reset(self):
         # Setup: create a user.
         new_user_email = 'nijntje94@hetkonijntje.nl'
         new_user_password = 'password'
-        response = self.client.post(self.user_create_api_url, {'email': new_user_email,
-                                                               'password': new_user_password})
+        response = self.client.post(self.user_create_api_url,
+                                    {'email': new_user_email,
+                                     'password': new_user_password})
         token = "JWT {0}".format(response.data['jwt_token'])
         user_id = response.data['id']
 
         # Test: resetting the password should be allowed.
-        response = self.client.put(self.user_password_reset_api_url, {'email': new_user_email})
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        response = self.client.put(self.user_password_reset_api_url,
+                                   {'email': new_user_email})
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         response.data)
         self.assertEqual(len(mail.outbox), 1)
 
         # Setup: get the password reset token and url.
-        c = re.compile('\/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})\/', re.DOTALL)
+        c = re.compile(
+            '\/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})\/',
+            re.DOTALL)
         m = c.search(mail.outbox[0].body)
-        password_set_url = '{0}{1}-{2}'.format(self.user_password_set_api_url, m.group(1), m.group(2))
+        password_set_url = '{0}{1}-{2}'.format(self.user_password_set_api_url,
+                                               m.group(1), m.group(2))
 
         # Test: check that non-matching passwords produce a validation error.
         passwords = {'new_password1': 'rabbit', 'new_password2': 'rabbitt'}
         response = self.client.put(password_set_url, passwords)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
-        self.assertEqual(response.data['new_password2'][0], "The two password fields didn't match.")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         response.data)
+        self.assertEqual(response.data['new_password2'][0],
+                         "The two password fields didn't match.")
 
         # Test: check that updating the password works when the passwords match.
         passwords['new_password2'] = 'rabbit'
         response = self.client.put(password_set_url, passwords)
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         response.data)
 
         # Test: check that trying to reuse the password reset link doesn't work.
         response = self.client.put(password_set_url, passwords)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, response.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         response.data)
 
 
 class LocaleMiddlewareTest(BluebottleTestCase):
     """
     Integration tests for the User API.
     """
+
     def setUp(self):
         super(LocaleMiddlewareTest, self).setUp()
 
@@ -202,11 +231,13 @@ class LocaleMiddlewareTest(BluebottleTestCase):
         self.user_1_token = "JWT {0}".format(self.user_1.get_jwt_token())
 
     def test_early_redirect_to_user_language(self):
-        response = self.client.get('/nl/', follow=False, token=self.user_1_token)
+        response = self.client.get('/nl/', follow=False,
+                                   token=self.user_1_token)
         self.assertRedirects(response, '/en/')
 
     def test_no_redirect_for_non_language_urls(self):
-        response = self.client.get('/api/', follow=False, token=self.user_1_token)
+        response = self.client.get('/api/', follow=False,
+                                   token=self.user_1_token)
         self.assertTrue(response.status_code, 200)
 
         response = self.client.get('/', follow=False, token=self.user_1_token)
@@ -221,6 +252,7 @@ class UserProfileUpdateTests(BluebottleTestCase):
     """
     Integration tests for the User API with dependencies on different bluebottle apps.
     """
+
     def setUp(self):
         super(UserProfileUpdateTests, self).setUp()
 
@@ -232,4 +264,3 @@ class UserProfileUpdateTests(BluebottleTestCase):
         self.user_activation_api_url = '/api/users/activate/'
         self.user_password_reset_api_url = '/api/users/passwordreset'
         self.user_password_set_api_url = '/api/users/passwordset/'
-

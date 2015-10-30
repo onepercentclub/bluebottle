1853565b70d7101b41b46bf75158a36ff8fee522
Loek van Gent <loek@1procentclub.nl>
2015-10-30 13:53:11 +0100

    Fix accounting migrations

--- bluebottle/payments_manual/migrations/0001_initial.py
+++ bluebottle/payments_manual/migrations/0001_initial.py
@@ -7,6 +7,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('accounting', '0001_initial'),
+    )
+
+
     def forwards(self, orm):
         # Adding model 'ManualPayment'
         db.create_table(u'payments_manual_manualpayment', (

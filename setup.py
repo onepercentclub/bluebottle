40232415849fb9bac4d880e1efe8d6d25f3ebb9d
Loek van Gent <loek@1procentclub.nl>
2015-11-23 14:01:19 +0100

    New django token auth

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ dependency_links = [
     'git+https://github.com/onepercentclub/django-fluent-contents.git@8439c7ffc1ba8877247aa7d012928c9bb170dc79#egg=fluent_contents-1.0c3',
     'git+https://github.com/onepercentclub/django-bb-salesforce.git@1.1.18#egg=django-bb-salesforce-1.1.18',
     'git+https://github.com/onepercentclub/django-tenant-extras.git@1.9.3#egg=django-tenant-extras-1.9.3',
-    'git+https://github.com/onepercentclub/django-token-auth.git@0.2.1b#egg=django-token-auth-0.2.1b'
+    'git+https://github.com/onepercentclub/django-token-auth.git@0.2.11#egg=django-token-auth-0.2.11'
 ]
 
 install_requires = [
@@ -87,7 +87,7 @@ install_requires = [
     'django-fluent-contents==1.0c3',
     'django-bb-salesforce==1.1.18',
     'django-tenant-extras==1.9.3',
-    'django-token-auth==0.2.1b'
+    'django-token-auth==0.2.11'
 ]
 
 tests_requires = [

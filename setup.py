07180fe4bc61c28f9006bf2d6eaebee9cd3f1553
Loek van Gent <loek@onepercentclub.com>
2015-12-16 12:07:48 +0100

    e a correctly tagged version of tenant extras

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dependency_links = [
     'git+https://github.com/onepercentclub/django-taggit-autocomplete-modified.git@8e7fbc2deae2f1fbb31b574bc8819d9ae7c644d6#egg=django-taggit-autocomplete-modified-0.1.1b1',
     'git+https://github.com/onepercentclub/django-fluent-contents.git@8439c7ffc1ba8877247aa7d012928c9bb170dc79#egg=fluent_contents-1.0c3',
     'git+https://github.com/onepercentclub/django-bb-salesforce.git@1.1.18#egg=django-bb-salesforce-1.1.18',
-    'git+https://github.com/onepercentclub/django-tenant-extras.git@1.9.7#egg=django-tenant-extras-1.9.7',
+    'git+https://github.com/onepercentclub/django-tenant-extras.git@1.9.8#egg=django-tenant-extras-1.9.8',
     'git+https://github.com/onepercentclub/django-token-auth.git@0.2.12#egg=django-token-auth-0.2.12'
 ]
 
@@ -88,7 +88,7 @@ install_requires = [
     'django-taggit-autocomplete-modified==0.1.1b1',
     'django-fluent-contents==1.0c3',
     'django-bb-salesforce==1.1.18',
-    'django-tenant-extras==1.9.5',
+    'django-tenant-extras==1.9.8',
     'django-token-auth==0.2.12'
 ]
 

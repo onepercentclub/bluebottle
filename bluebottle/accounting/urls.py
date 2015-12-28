813fdcfbf6e58458991f4fe570b82fbf16333dcf
Loek van Gent <loek@onepercentclub.com>
2015-12-28 16:35:12 +0100

    Only allow 1% to access multiadmin

--- bluebottle/accounting/urls.py
+++ bluebottle/accounting/urls.py
@@ -8,7 +8,6 @@ from .views import (
 urlpatterns = patterns('',
     surl(r'^overview/$', AccountingOverviewView.as_view(), name='admin-accounting-overview'),
     surl(r'^dashboard/$', AccountingDashboardView.as_view(), name='admin-accounting-dashboard'),
-
     surl(r'^multiadmin/$', MultiTenantAccountingDashboardView.as_view(), name='multiadmin-accounting-dashboard'),
     surl(r'^multiadmin/overview/$', MultiTenantAccountingOverviewView.as_view(), name='multiadmin-accounting-overview'),
 )

a84f17a451dd8c06acf127be3564602fd86af923
Loek van Gent <loek@onepercentclub.com>
2016-02-01 23:09:42 +0100

    Change Statistics model

---
+++ bluebottle/statistics/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from djchoices import DjangoChoices, ChoiceItem
+from django_extensions.db.fields import CreationDateTimeField, \
+    ModificationDateTimeField
+
+from bluebottle.statistics.statistics import Statistics
+
+
+class Statistic(models.Model):
+    """
+    Statistics for homepage
+    """
+
+    class StatisticType(DjangoChoices):
+        manual = ChoiceItem('manual', label=_("Manual"))
+        donated_total = ChoiceItem('donated_total', label=_("Donated total"))
+        projects_online = ChoiceItem('projects_online', label=_("Projects online"))
+        projects_realized = ChoiceItem('projects_realized', label=_("Projects realized"))
+        tasks_realized = ChoiceItem('tasks_realized', label=_("Tasks realized"))
+        people_involved = ChoiceItem('people_involved', label=_("People involved"))
+
+    title = models.CharField(_("Title"), max_length=100, blank=True)
+    type = models.CharField(_('Type'), max_length=20,
+                              choices=StatisticType.choices,
+                              default=StatisticType.manual, db_index=True)
+    sequence = models.IntegerField()
+    value = models.IntegerField(null=True, blank=True, help_text=_('This overwrites the calculated value, if available'))
+    active = models.BooleanField(help_text=_('Should this be shown or hidden.'))
+    creation_date = CreationDateTimeField(_('creation date'))
+    modification_date = ModificationDateTimeField(_('last modification'))
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def calculated_value(self):
+        if self.value:
+            return self.value
+        return getattr(Statistics(), self.type, 0)
+
+
+    class Meta:
+        ordering = ('sequence', )

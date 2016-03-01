425dae67d6b7ae59e6eb203718b2a465096adbae
Ernst Odolphi <ernst@onepercentclub.com>
2016-03-01 15:30:13 +0100

    Name celery app bluebottle

--- bluebottle/celery.py
+++ bluebottle/celery.py
@@ -7,7 +7,7 @@ from celery import Celery
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-app = Celery('reef',
+app = Celery('bluebottle',
              broker=getattr(settings, 'BROKER_URL', 'amqp://guest@localhost//'))
 
 app.config_from_object('celeryconfig')

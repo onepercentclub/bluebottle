4ca6171847703dd63246d6386dfb3f7fb90bb213
rollick <github.kcillor@recursor.net>
2016-05-03 15:58:17 +0200

    WIP: adding pledge emails BB-6766

--- bluebottle/payments_pledge/models.py
+++ bluebottle/payments_pledge/models.py
@@ -1,8 +1,11 @@
 from bluebottle.payments.models import Payment
 
 
+# TODO: do we need a pledge payment model?
 class PledgeStandardPayment(Payment):
     """ Pledge payment class."""
 
     def get_method_name(self):
         return 'Pledge'
+
+import signals

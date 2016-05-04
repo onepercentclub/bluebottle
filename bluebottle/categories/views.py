047c795f21e58d4842edc482ea8fcb92cc0ce02a
Aksel Ethem <aksel.ethem@gmail.com>
2016-05-04 14:37:28 +0200

    remove modeltranslation mixin in view

--- bluebottle/categories/views.py
+++ bluebottle/categories/views.py
@@ -1,15 +1,9 @@
 from rest_framework import generics
 
-from bluebottle.utils.views import ModelTranslationViewMixin
 from .models import Category
 from .serializers import CategorySerializer
 
 
-
-class CategoryList(ModelTranslationViewMixin, generics.ListAPIView):
+class CategoryList(generics.ListAPIView):
     model = Category
     serializer_class = CategorySerializer
-
-
-
-

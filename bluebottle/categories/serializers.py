a96dc9952b47f2c83180e6c856a6102f8dc60d12
Ernst Odolphi <ernst@onepercentclub.com>
2016-05-04 15:26:20 +0200

    Use slugs for category ids

--- bluebottle/categories/serializers.py
+++ bluebottle/categories/serializers.py
@@ -6,12 +6,12 @@ from bluebottle.categories.models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
 
+    id = serializers.CharField(source='slug', read_only=True)
     image = ImageSerializer(required=False)
     image_logo = ImageSerializer(required=False)
     description = serializers.CharField()
     title = serializers.CharField()
-    slug = serializers.CharField(read_only=True)
 
     class Meta:
         model = Category
-        fields = ('id', 'slug', 'title', 'description', 'image', 'image_logo')
+        fields = ('id', 'title', 'description', 'image', 'image_logo')

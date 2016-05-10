f13d3428dcf941c72b4797a9e75c059f30ab7119
Aksel Ethem <aksel.ethem@gmail.com>
2016-05-10 15:24:20 +0200

    remove unused serializers

--- bluebottle/utils/serializers.py
+++ bluebottle/utils/serializers.py
@@ -282,29 +282,6 @@ class MetaField(serializers.Field):
         return None
 
 
-# class DefaultSerializerMixin(object):
-#     def get_custom_serializer(self):
-#         return self.model._meta.default_serializer
-
-#     def get_serializer_class(self):
-#         dotted_path = self.get_custom_serializer()
-#         bits = dotted_path.split('.')
-#         module_name = '.'.join(bits[:-1])
-#         module = import_module(module_name)
-#         cls_name = bits[-1]
-#         return getattr(module, cls_name)
-
-
-# class ManageSerializerMixin(DefaultSerializerMixin):
-#     def get_custom_serializer(self):
-#         return self.model._meta.manage_serializer
-
-
-# class PreviewSerializerMixin(DefaultSerializerMixin):
-#     def get_custom_serializer(self):
-#         return self.model._meta.preview_serializer
-
-
 class HumanReadableChoiceField(serializers.ChoiceField):
     def field_to_native(self, obj, field_name):
         """

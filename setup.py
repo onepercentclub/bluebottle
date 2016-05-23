feeea92ba6055eee3c9a9f7e2fb7616fd1dea0c1
rollick <github.kcillor@recursor.net>
2016-05-23 21:50:56 +0200

    bump bb-salesforce BB-6926

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ changes = ''
 
 dependency_links = [
     'git+https://github.com/onepercentclub/django-taggit-autocomplete-modified.git@8e7fbc2deae2f1fbb31b574bc8819d9ae7c644d6#egg=django-taggit-autocomplete-modified-0.1.1b1',
-    'git+https://github.com/onepercentclub/django-bb-salesforce.git@aa9613a816cb40e790f054eaf59a84cc146888b0#egg=django-bb-salesforce-1.1.18b',
+    'git+https://github.com/onepercentclub/django-bb-salesforce.git@1.2.0#egg=django-bb-salesforce-1.2.0',
     'git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.2#egg=django-tenant-extras-2.0.2',
     'git+https://github.com/onepercentclub/django-token-auth.git@0.2.17#egg=django-token-auth-0.2.17',
     'hg+https://bitbucket.org/jdiascarvalho/django-filetransfers@89c8381764da217d72f1fa396ce3929f0762b8f9#egg=django-filetransfers-0.1.1'
@@ -78,7 +78,7 @@ install_requires = [
     # Github requirements
     'django-taggit-autocomplete-modified==0.1.1b1',
     'django-fluent-contents==1.1.4',
-    'django-bb-salesforce==1.1.18b',
+    'django-bb-salesforce==1.2.0',
     'django-tenant-extras==2.0.2',
     'django-token-auth==0.2.17',
 ]

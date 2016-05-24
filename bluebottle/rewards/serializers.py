5928385d695c979bfccecca00559928988bf3026
Ernst Odolphi <ernst@onepercentclub.com>
2016-05-24 08:50:59 +0200

    Another source too many

--- bluebottle/rewards/serializers.py
+++ bluebottle/rewards/serializers.py
@@ -4,9 +4,8 @@ from .models import Reward
 
 
 class RewardSerializer(serializers.ModelSerializer):
-
     project = serializers.SlugRelatedField(slug_field="slug", queryset=Reward.objects)
-    count = serializers.IntegerField(source='count', read_only=True)
+    count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Reward

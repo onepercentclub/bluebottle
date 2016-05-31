5e10da6db227b4806d77aadf072519d9973db5c3
rollick <github.kcillor@recursor.net>
2016-05-31 15:32:57 +0200

    tenant schema changes for django upgrade

--- bluebottle/test/test_runner.py
+++ bluebottle/test/test_runner.py
@@ -7,6 +7,19 @@ from tenant_schemas.utils import get_tenant_model
 class MultiTenantRunner(DiscoverRunner):
     def setup_databases(self, *args, **kwargs):
         result = super(MultiTenantRunner, self).setup_databases(*args, **kwargs)
+
+        # Create secondary tenant
+        connection.set_schema_to_public()
+        tenant_domain = 'testserver2'
+        tenant2 = get_tenant_model()(
+            domain_url=tenant_domain,
+            schema_name='test2',
+            client_name='test2')
+
+        tenant2.save(
+            verbosity=self.verbosity)
+
+        # Create main tenant
         tenant_domain = 'testserver'
         tenant = get_tenant_model()(
             domain_url=tenant_domain,
@@ -19,6 +32,3 @@ class MultiTenantRunner(DiscoverRunner):
         connection.set_tenant(tenant)
 
         return result
-
-
-

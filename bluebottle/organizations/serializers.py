216a52d52fdbfca959946434a81f4d42270bfd95
Ernst Odolphi <ernst@onepercentclub.com>
2016-06-10 15:26:53 +0200

    Make the name of an organization required

--- bluebottle/organizations/serializers.py
+++ bluebottle/organizations/serializers.py
@@ -14,7 +14,7 @@ class OrganizationSerializer(serializers.ModelSerializer):
 
 class ManageOrganizationSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(required=False, allow_null=True)
-    name = serializers.CharField(required=True, allow_blank=True)
+    name = serializers.CharField(required=True)
     website = URLField(required=False, allow_blank=True)
     email = serializers.EmailField(required=False, allow_blank=True)
 

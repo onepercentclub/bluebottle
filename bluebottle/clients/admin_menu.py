76b0f74543314b585e54a3dbc3d1b12a2566d2da
Loek van Gent <loek@onepercentclub.com>
2016-06-30 11:33:26 +0200

    Remove some items from admin menu to have more space

---
+++ bluebottle/clients/admin_menu.py
@@ -0,0 +1,14 @@
+from fluent_dashboard.menu import FluentMenu
+
+
+class AdminMenu(FluentMenu):
+
+    def init_with_context(self, context):
+        """
+        Initialize the menu items.
+        """
+        super(AdminMenu, self).init_with_context(context)
+        # Pop 'Dashboard', 'Bookamrks' and 'Return to site' items from admin menu.
+        self.children.pop(0)
+        self.children.pop(0)
+        self.children.pop(len(self.children)-1)

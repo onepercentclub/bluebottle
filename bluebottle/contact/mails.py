0a89e94b293d0e5377a21e21acbdcaa99f1add35
Aksel Ethem <aksel@onepercentclub.com>
2016-07-04 16:10:55 +0200

    strip language from reverse-generated link

--- bluebottle/contact/mails.py
+++ bluebottle/contact/mails.py
@@ -1,3 +1,5 @@
+import re
+
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext as _
@@ -13,15 +15,25 @@ def send_contact_email(message, contact_email):
     subject = _('Contact message from %(sender)s') % {'sender': message.name}
     
     from_email = properties.CONTACT_EMAIL
+
     if not from_email:
         return
 
     # add the url to the backoffice
-    link = reverse('admin:contact_contactmessage_change', args=[message.id])
+    lang_link = reverse('admin:contact_contactmessage_change', args=[message.id])
+    
+    # Strip language from reverse generated link
+    m = re.match(r'(/[^/]*)(/.*$)', lang_link)
+
+    try:
+        link = m.groups()[1]
+    except IndexError:
+        link = lang_link
 
     ctx = ClientContext(
         {'message': message, 'link': link, 'site_url': tenant_url(),
          'site': tenant_site()})
+
 
     text_content = render_to_string('contact/contact.mail.txt',
                                     context_instance=ctx)

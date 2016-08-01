2cb4a6264bb00a84bf9f571f4d323f431665a461
rollick <github.kcillor@recursor.net>
2016-08-01 18:05:24 +0200

    more tweaks for travis notifications

--- post_travis.sh
+++ post_travis.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-URL="https://api.hipchat.com/v2/$GROUPNAME/notification?auth_token=$APIKEY"
-
 if [[ $TRAVIS_BRANCH == 'develop' ]]; then
     ENV='development'
 fi
@@ -15,6 +13,13 @@ if [[ $TRAVIS_BRANCH =~ ^release//* ]]; then
 fi
 
 if [[ ($ENV && $TRAVIS_PULL_REQUEST == 'false') ]]; then
-    curl -d "{\"color\": \"green\", \"message\": \"$TRAVIS_COMMIT_MSG\" }" $URL
-    curl -d "{\"color\": \"green\", \"message\": \"hodor deploy backend $TRAVIS_COMMIT to $ENV\" }" $URL
+    MESSAGES=( $TRAVIS_COMMIT_MSG "hodor deploy backend $TRAVIS_COMMIT to $ENV" )
+
+    for m in "${MESSAGES[@]}"
+    do
+        curl -H "Content-Type: application/json" \
+             -X POST \
+             -d "{\"color\": \"green\", \"message_format\": \"html\", \"message\": \"$m\" }" \
+             https://api.hipchat.com/v2/room/$GROUPNAME/notification?auth_token=$APIKEY
+    done
 fi

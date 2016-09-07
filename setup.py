66e5b987ee13f93db975857d5ea31ae2de4757a3
Ernst Odolphi <ernst@onepercentclub.com>
2016-09-07 16:10:22 +0200

    Bump version of django-money

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ install_requires = [
     'django-lockdown==1.2',
     'django-loginas==0.1.9',
     'django-modeltranslation==0.11',
-    'django-money==0.8',
+    'django-money==0.9.1',
     'django-rest-swagger==0.3.6',
     'django-select-multiple-field==0.4.2',
     'django-taggit==0.18.3',

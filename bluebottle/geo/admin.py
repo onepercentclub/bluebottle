ee484c3bcf8fcc1fc08bf9b369efa11e16fd42bb
Loek van Gent <loek@onepercentclub.com>
2016-09-20 16:16:36 +0200

    Don't hard code location group actions

--- bluebottle/geo/admin.py
+++ bluebottle/geo/admin.py
@@ -75,7 +75,9 @@ class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'position', 'group')
     model = Location
 
-    actions = ['add_to_group_6', 'add_to_group_9', 'add_to_group_7', 'add_to_group_8', 'add_to_group_10']
+    @property
+    def actions(self):
+        return ['select_%s' % group.id for group in LocationGroup.objects.all()]
 
     def make_action(self, group):
         name = 'select_%s' % group

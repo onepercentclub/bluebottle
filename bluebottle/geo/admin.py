07b3ac865b9cb1399a237a13803fab37e3a4ee59
Loek van Gent <loek@onepercentclub.com>
2016-09-20 15:19:32 +0200

    Add location groups + filter

--- bluebottle/geo/admin.py
+++ bluebottle/geo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from bluebottle.geo.models import Location
+from bluebottle.geo.models import Location, LocationGroup
 
 from .models import Region, SubRegion, Country
 
@@ -21,6 +21,25 @@ class LocationFilter(admin.SimpleListFilter):
             return queryset.filter(location__id__exact=self.value())
         else:
             return queryset
+
+
+class LocationGroupFilter(admin.SimpleListFilter):
+    title = _('location group')
+    parameter_name = 'location_group'
+
+    def lookups(self, request, model_admin):
+        groups = [obj.location.group for obj in model_admin.model.objects.order_by(
+            'location__group__name').distinct('location__group__name').exclude(
+            location__group__isnull=True).all()]
+        return [(gr.id, gr.name) for gr in groups]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(location__group__id__exact=self.value())
+        else:
+            return queryset
+
+
 
 
 class RegionAdmin(admin.ModelAdmin):
@@ -47,8 +66,13 @@ class CountryAdmin(admin.ModelAdmin):
 admin.site.register(Country, CountryAdmin)
 
 
+class LocationGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    model = LocationGroup
+
+
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position')
+    list_display = ('name', 'position', 'group')
     model = Location
 
 

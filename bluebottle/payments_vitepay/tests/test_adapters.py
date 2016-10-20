f5674e8fc93a244dd484a6a52b6fb5dbe0b69327
rollick <github.kcillor@recursor.net>
2016-10-20 17:37:22 +0200

    Add authenticity test for vitepay BB-8244 #resolve

--- bluebottle/payments_vitepay/tests/test_adapters.py
+++ bluebottle/payments_vitepay/tests/test_adapters.py
@@ -1,3 +1,5 @@
+import json
+
 from moneyed.classes import Money, XOF, EUR
 from mock import patch
 
@@ -10,6 +12,9 @@ from bluebottle.test.factory_models.donations import DonationFactory
 from bluebottle.test.factory_models.orders import OrderFactory
 from bluebottle.test.factory_models.payments import OrderPaymentFactory
 from bluebottle.test.utils import BluebottleTestCase
+
+from .factory_models import VitepayPaymentFactory, VitepayOrderPaymentFactory
+
 
 vitepay_settings = {
     'MERCHANT_ACCOUNTS': [
@@ -24,29 +29,28 @@ vitepay_settings = {
 }
 
 
+@patch('bluebottle.payments_vitepay.adapters.get_current_host',
+        return_value='https://onepercentclub.com')
 @override_settings(**vitepay_settings)
 class VitepayPaymentAdapterTestCase(BluebottleTestCase):
+    def setUp(self):
+        super(VitepayPaymentAdapterTestCase, self).setUp()
 
-    @patch('bluebottle.payments_vitepay.adapters.get_current_host',
-           return_value='https://onepercentclub.com')
-    def test_create_payment(self, get_current_host):
         self.init_projects()
+
+    def test_create_payment(self, get_current_host):
         order = OrderFactory.create()
         DonationFactory.create(amount=Money(2000, XOF), order=order)
         order_payment = OrderPaymentFactory.create(payment_method='vitepayOrangemoney', order=order)
         adapter = VitepayPaymentAdapter(order_payment)
         self.assertEqual(adapter.payment.amount_100, 200000)
 
-    @patch('bluebottle.payments_vitepay.adapters.get_current_host',
-           return_value='https://onepercentclub.com')
     def test_create_payment_with_wrong_currency(self, get_current_host):
         with self.assertRaises(PaymentException):
             order_payment = OrderPaymentFactory.create(payment_method='vitepayOrangemoney',
                                                        amount=Money(200, EUR))
             VitepayPaymentAdapter(order_payment)
 
-    @patch('bluebottle.payments_vitepay.adapters.get_current_host',
-           return_value='https://onepercentclub.com')
     def test_create_payment_with_wrong_payment_method(self, get_current_host):
         with self.assertRaises(PaymentException):
             order_payment = OrderPaymentFactory.create(payment_method='docdataIdeal',
@@ -89,24 +93,53 @@ class VitepayPaymentAdapterTestCase(BluebottleTestCase):
         self.assertEqual(authorization_action['url'], 'https://vitepay.com/some-path-to-pay')
 
 
-    @patch('bluebottle.payments_vitepay.adapters.get_current_host',
-           return_value='https://onepercentclub.com')
     def test_update_payment(self, get_current_host):
         """
         Play some posts that Vitepay might fire at us.
         """
-        self.init_projects()
-        order = OrderFactory.create()
-        DonationFactory.create(amount=Money(2000, XOF), order=order)
-        order_payment = OrderPaymentFactory.create(payment_method='vitepayOrangemoney', order=order)
-        adapter = VitepayPaymentAdapter(order_payment)
-        self.assertEqual(adapter.payment.amount_100, 200000)
+        # order = OrderFactory.create()
+        # DonationFactory.create(amount=Money(2000, XOF), order=order)
+        # order_payment = OrderPaymentFactory.create(payment_method='vitepayOrangemoney', order=order)
+        order_payment = VitepayOrderPaymentFactory.create(amount=Money(2000, XOF))
+        payment = VitepayPaymentFactory.create(order_id='opc-1', order_payment=order_payment)
+        # authenticity = sha1({order_id};{amount_100};{currency_code};{api_secret})
+        authenticity = 'd2492ecd8d51ae72a43e5c6460e8da7ceae8195a'
         update_view = reverse('vitepay-status-update')
-        authenticity = adapter._create_update_hash()
         data = {
             'success': 1,
-            'order_id': adapter.payment.order_id,
+            'order_id': payment.order_id,
             'authenticity': authenticity
         }
         response = self.client.post(update_view, data)
         self.assertEqual(response.content, '{"status": "1"}')
+
+    def test_invalid_update_hash(self, get_current_host):
+        """
+        Test for invalid hash from Vitepay.
+        """
+        payment = VitepayPaymentFactory.create()
+        update_view = reverse('vitepay-status-update')
+        data = {
+            'success': 1,
+            'order_id': payment.order_id,
+            'authenticity': 'hashyhashy'
+        }
+        response = self.client.post(update_view, data)
+        data = json.loads(response.content)
+        self.assertEqual(data['status'], '0')
+        self.assertEqual(data['message'], 'Authenticity incorrect.')
+
+    def test_invalid_order_id(self, get_current_host):
+        """
+        Test for invalid hash from Vitepay.
+        """
+        update_view = reverse('vitepay-status-update')
+        data = {
+            'success': 1,
+            'order_id': 999,
+            'authenticity': 'hashyhashy'
+        }
+        response = self.client.post(update_view, data)
+        data = json.loads(response.content)
+        self.assertEqual(data['status'], '0')
+        self.assertEqual(data['message'], 'Order not found.')

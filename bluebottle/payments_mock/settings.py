905552c6fcd061800702f97b75bad894185d1204
Ernst Odolphi <ernst@onepercentclub.com>
2016-10-24 16:46:05 +0200

    Group currency settings in payment methods
    BB-8240 #resolve

--- bluebottle/payments_mock/settings.py
+++ bluebottle/payments_mock/settings.py
@@ -7,7 +7,9 @@ MOCK_PAYMENT_METHODS = (
         'profile': 'paypal',
         'name': 'MockPal',
         'supports_recurring': False,
-        'currencies': ['EUR'],
+        'currencies': {
+            'EUR': {'min_amount': 5, 'max_amount': 100}
+        }
     },
     {
         'provider': 'mock',
@@ -16,7 +18,9 @@ MOCK_PAYMENT_METHODS = (
         'name': 'MockDeal',
         'restricted_countries': ('NL',),
         'supports_recurring': False,
-        'currencies': ['USD', 'EUR'],
+        'currencies': {
+            'EUR': {'min_amount': 5, 'max_amount': 100}
+        }
     },
     {
         'provider': 'mock',
@@ -24,7 +28,9 @@ MOCK_PAYMENT_METHODS = (
         'profile': 'creditcard',
         'name': 'MockCard',
         'supports_recurring': False,
-        'currencies': ['USD'],
+        'currencies': {
+            'EUR': {'min_amount': 5, 'max_amount': 100}
+        }
     }
 )
 

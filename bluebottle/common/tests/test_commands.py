18062831cd2b0c7fbd2bb7958037ff3b3dfeba82
rollick <github.kcillor@recursor.net>
2016-11-10 14:29:59 +0100

    fix management commands in django 1.10

---
+++ bluebottle/common/tests/test_commands.py
@@ -0,0 +1,16 @@
+import mock
+
+from django.test import TestCase
+from django.test.utils import override_settings
+from django.core.management import call_command
+from ..management.commands.base import Command
+
+
+@override_settings(TENANT_APPS=('django_nose',),
+                   TENANT_MODEL='client.clients',
+                   DATABASE_ROUTERS=('tenant_schemas.routers.TenantSyncRouter',))
+class ManagementCommandArgsTests(TestCase):
+    def test_base(self):
+        cmd = Command()
+
+        self.assertEqual(cmd.option_list, [])

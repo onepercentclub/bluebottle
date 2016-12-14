c8384b25d04bcb8145eff2f1f05ee231b83b0b0b
Loek van Gent <loek@onepercentclub.com>
2016-12-14 09:01:23 +0100

    PEP8 rewards

--- bluebottle/rewards/permissions.py
+++ bluebottle/rewards/permissions.py
@@ -5,6 +5,7 @@ class NoDonationsOrReadOnly(permissions.BasePermission):
     """
     If a reward has no donations it should be editable/deletable
     """
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True

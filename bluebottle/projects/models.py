f17b7fc60e92f7ea342e8e45b0d7e4da9f6f6934
Ashutosh <ashutosh@onepercentclub.com>
2017-02-17 11:32:38 +0100

    add daily cumulative project status stats

--- bluebottle/projects/models.py
+++ bluebottle/projects/models.py
@@ -292,6 +292,28 @@ class Project(BaseProject, PreviousStatusMixin):
                 # this way we will not trigger signals and hit the save method
                 self.objects.filter(pk=project.pk).update(popularity=popularity)
 
+    @classmethod
+    def update_status_daily_stats(cls, tenant):
+        logger.info('Updating Project Status Stats: {}'.format(tenant.name))
+        timestamp = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+        for status in ProjectPhase.objects.all():
+            # TODO: Should we count only where the status is active?
+            count = Project.objects.filter(status=status).count()
+            logger.info('status: {}, count: {}'.format(status.name, count))
+            tags = {
+                'type': 'project_status_daily',
+                'status': status.name,
+                'status_slug': status.slug,
+                'tenant': tenant.name,
+            }
+            fields = {
+                'total': count,
+            }
+            if getattr(settings, 'CELERY_RESULT_BACKEND', None):
+                queue_analytics_record.delay(timestamp=timestamp, tags=tags, fields=fields)
+            else:
+                queue_analytics_record(timestamp=timestamp, tags=tags, fields=fields)
+
     def save(self, *args, **kwargs):
         # Set valid slug
         if not self.slug:

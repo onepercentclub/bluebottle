32b605d0cae2989c8b05027c3f7e398df4bc2477
Ashutosh <ashutosh@onepercentclub.com>
2017-02-20 14:54:27 +0100

    remove wallpost metric module

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,3 @@
-from admin_tools.dashboard.models import DashboardModule
-from django.utils.translation import ugettext_lazy as _
 from fluent_dashboard.dashboard import FluentIndexDashboard
 
 from bluebottle.bb_tasks.dashboard import RecentTasks
@@ -17,24 +15,3 @@ class CustomIndexDashboard(FluentIndexDashboard):
         self.children.append(StartedCampaigns())
         self.children.append(EndedProjects())
         self.children.append(RecentTasks())
-
-
-class WallpostModule(DashboardModule):
-    """
-    Metrics module for the django admin tools dashboard.
-    Since this is only meant to be here for a short while so lot's of simple hacks.
-    """
-    # FIXME: Replace with a decent metrics solution.
-    title = _('Wallposts')
-    template = 'admin_tools/dashboard/metrics_module.html'
-
-    def __init__(self, **kwargs):
-        mediawallpost_url = '/admin/wallposts/mediawallpost/'
-        wallpost_url = '/admin/wallposts/wallpost/'
-
-        self.children = (
-            {'title': _('Media wallposts'), 'url': mediawallpost_url},
-            {'title': _('All wallposts'), 'url': wallpost_url},
-
-        )
-        super(WallpostModule, self).__init__(**kwargs)

14b1f9bde45b66f8752778469f1daae77b49f4e0
Loek van Gent <loek@onepercentclub.com>
2017-03-31 12:11:19 +0200

    Add comment

--- bluebottle/bb_orders/signals.py
+++ bluebottle/bb_orders/signals.py
@@ -24,6 +24,8 @@ def update_order_amount_post_save(sender, instance, **kwargs):
 def update_order_amount(sender, instance, **kwargs):
     # If we're deleting order and donations do nothing.
     # If we're just deleting a donation then we should update the order total.
+
+    # Import it here to avoid circular imports
     from bluebottle.orders.models import Order
     try:
         instance.order.update_total()

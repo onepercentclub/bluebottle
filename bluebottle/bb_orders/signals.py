14dcfdc1b968747d81f71d07970e428dcc78047b
Loek van Gent <loek@onepercentclub.com>
2017-03-31 11:54:14 +0200

    Catch errors when running monthly donations

--- bluebottle/bb_orders/signals.py
+++ bluebottle/bb_orders/signals.py
@@ -24,8 +24,11 @@ def update_order_amount_post_save(sender, instance, **kwargs):
 def update_order_amount(sender, instance, **kwargs):
     # If we're deleting order and donations do nothing.
     # If we're just deleting a donation then we should update the order total.
-    if getattr(instance, 'order', None):
+    from bluebottle.orders.models import Order
+    try:
         instance.order.update_total()
+    except Order.DoesNotExist:
+        pass
 
 
 @receiver(post_transition, sender=OrderPayment)

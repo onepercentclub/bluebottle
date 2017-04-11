840c66574925d15e1308910c0c07ddcd00693526
Ashutosh <ashutosh@onepercentclub.com>
2017-04-11 11:24:17 +0200

    WIP: refactor, validate tenants, separate gnerating data and creating excel

--- bluebottle/analytics/management/commands/export_engagement_metrics.py
+++ bluebottle/analytics/management/commands/export_engagement_metrics.py
@@ -22,7 +22,6 @@ logger = logging.getLogger(__name__)
 # TODO: Add charts
 # NOTE: Score is a numeric value, Rating is a textual classification based on the score
 
-
 class Command(BaseCommand):
     help = 'Export the engagement metrics'
 
@@ -31,6 +30,28 @@ class Command(BaseCommand):
 
     row_counter_engagement_raw_data = 1
     row_counter_engagement_aggregated_data = 1
+
+    tenants = []
+
+    def __init__(self, **kwargs):
+        for client in Client.objects.all():
+            self.tenants.append(client.client_name)
+
+        super(Command, self).__init__(**kwargs)
+
+    def add_arguments(self, parser):
+
+        # TODO: Add arguments to select export destination
+        parser.add_argument('--start', metavar='YYYY-MM-DD', action='store', dest='start', required=True,
+                            type=self._validate_date, help="Start date (YYYY-MM-DD) for dump. UTC is the default \
+                            time zone")
+
+        parser.add_argument('--end', metavar='YYYY-MM-DD', action='store', dest='end', required=True,
+                            type=self._validate_date,
+                            help="End date (YYYY-MM-DD) for dump. UTC is the default time zone")
+
+        parser.add_argument('--tenants', metavar='tenants', action='store', dest='tenants', required=False, nargs='*',
+                            choices=self.tenants, help="The names of the tenants to export")
 
     @staticmethod
     def _validate_date(date_string):
@@ -42,10 +63,13 @@ class Command(BaseCommand):
             msg = '{} is not a valid date or is not in the required date format YYYY-MM-DD'.format(date_string)
             raise argparse.ArgumentTypeError(msg)
 
-    @staticmethod
-    def has_active_members(start_date, end_date):
+    def handle(self, **options):
 
-        return True if Member.objects.filter(last_login__gte=start_date, last_login__lte=end_date).count() else False
+        self.start_date = dateparse.parse_datetime('{} 00:00:00+00:00'.format(options['start']))
+        self.end_date = dateparse.parse_datetime('{} 23:59:59+00:00'.format(options['end']))
+        tenants = set(options['tenants']) if options['tenants'] else None
+
+        self.generate_engagement_data(tenants)
 
     @staticmethod
     def get_engagement_score(entry):
@@ -82,32 +106,6 @@ class Command(BaseCommand):
             worksheet.write_row(0, 0, headers)
         return worksheet
 
-    def add_arguments(self, parser):
-
-        # TODO: Add arguments to select export destination
-        # TODO: Make tenant selection interactive
-        # TODO: Validate user entered tenant names
-        parser.add_argument('--start', metavar='YYYY-MM-DD', action='store', dest='start', required=True,
-                            type=self._validate_date, help="Start date (YYYY-MM-DD) for dump. UTC is the default \
-                            time zone")
-
-        parser.add_argument('--end', metavar='YYYY-MM-DD', action='store', dest='end', required=True,
-                            type=self._validate_date,
-                            help="End date (YYYY-MM-DD) for dump. UTC is the default time zone")
-
-        parser.add_argument('--tenants', metavar='tenants', action='store', dest='tenants', required=False, nargs='*',
-                            help="The names of the tenants to export")
-
-    """ Dump engagement records for all tenants over a given date range """
-
-    def handle(self, **options):
-
-        self.start_date = dateparse.parse_datetime('{} 00:00:00+00:00'.format(options['start']))
-        self.end_date = dateparse.parse_datetime('{} 23:59:59+00:00'.format(options['end']))
-        tenants = set(options['tenants']) if options['tenants'] else None
-
-        self.generate_engagement_data(tenants)
-
     def generate_engagement_data(self, tenants):
 
         file_name = 'engagement_report_{}_{}_generated_{}.xlsx'.format(self.start_date.strftime("%Y%m%d"),
@@ -121,24 +119,17 @@ class Command(BaseCommand):
                     with LocalTenant(client, clear_tenant=True):
                         logger.info('export tenant:{}'.format(client.client_name))
                         raw_data = self.generate_raw_data()
+                        aggregated_data = self.generate_aggregated_data(raw_data)
                         self.generate_raw_data_worksheet(workbook, client.client_name, raw_data)
-                        self.generate_aggregated_data_worksheet(workbook, client.client_name, raw_data)
+                        self.generate_aggregated_data_worksheet(workbook, client.client_name, aggregated_data)
 
     def generate_raw_data(self):
         raw_data = {}
         members = Member.objects.all()
 
         for member in members:
-            raw_data[member.id] = {
-                'year_last_seen': getattr(member.last_seen, 'year', ''),
-                'comments': 0,
-                'votes': 0,
-                'donations': 0,
-                'fundraisers': 0,
-                'projects': 0,
-                'tasks': 0,
-                'engagement_score': 0
-            }
+            raw_data[member.id] = defaultdict(int)
+            raw_data[member.id]['year_last_seen'] = getattr(member.last_seen, 'year', '')
 
         raw_data = self.generate_comments_raw_data(raw_data)
         raw_data = self.generate_votes_raw_data(raw_data)
@@ -147,6 +138,88 @@ class Command(BaseCommand):
         raw_data = self.generate_projects_raw_data(raw_data)
         raw_data = self.generate_tasks_raw_data(raw_data)
         raw_data = self.generate_raw_scores(raw_data)
+
+        return raw_data
+
+    def generate_raw_scores(self, data):
+        for _, entry in data.iteritems():
+            score = self.get_engagement_score(entry)
+            entry['engagement_score'] = score
+            entry['engagement_rating'] = self.get_engagement_rating(score)
+        return data
+
+    def generate_comments_raw_data(self, raw_data):
+        members = Member.objects \
+            .filter(wallpost_wallpost__created__gte=self.start_date,
+                    wallpost_wallpost__created__lte=self.end_date) \
+            .annotate(comments_total=Count('wallpost_wallpost')) \
+            .values('id', 'comments_total')
+
+        for member in members:
+            raw_data[member['id']]['comments'] = member['comments_total']
+
+        return raw_data
+
+    def generate_votes_raw_data(self, raw_data):
+        members = Member.objects \
+            .filter(vote__created__gte=self.start_date, vote__created__lte=self.end_date) \
+            .annotate(votes_total=Count('vote')) \
+            .values('id', 'votes_total')
+
+        for member in members:
+            raw_data[member['id']]['votes'] = member['votes_total']
+
+        return raw_data
+
+    def generate_donations_raw_data(self, raw_data):
+        members = Member.objects \
+            .filter(order__created__gte=self.start_date, order__created__lte=self.end_date,
+                    order__status="success", id__isnull=False) \
+            .annotate(donations_total=Count('order')) \
+            .values('id', 'donations_total')
+
+        for member in members:
+            raw_data[member['id']]['donations'] = member['donations_total']
+
+        return raw_data
+
+    def generate_fundraisers_raw_data(self, raw_data):
+        members = Member.objects \
+            .filter(fundraiser__created__gte=self.start_date, fundraiser__created__lte=self.end_date) \
+            .annotate(fundraisers_total=Count('fundraiser')) \
+            .values('id', 'fundraisers_total')
+
+        for member in members:
+            raw_data[member['id']]['fundraisers'] = member['fundraisers_total']
+
+        return raw_data
+
+    def generate_projects_raw_data(self, raw_data):
+        members = Member.objects \
+            .filter(owner__created__gte=self.start_date,
+                    owner__created__lte=self.end_date,
+                    owner__status__slug__in=['voting', 'voting-done', 'campaign',
+                                             'to-be-continued', 'done-complete',
+                                             'done-incomplete']) \
+            .annotate(projects_total=Count('owner')) \
+            .values('id', 'projects_total')
+
+        for member in members:
+            raw_data[member['id']]['projects'] = member['projects_total']
+
+        return raw_data
+
+    def generate_tasks_raw_data(self, raw_data):
+        members = Member.objects \
+            .filter(tasks_taskmember_related__created__gte=self.start_date,
+                    tasks_taskmember_related__created__lte=self.end_date,
+                    tasks_taskmember_related__status='realized',
+                    tasks_taskmember_related__time_spent__gt=0) \
+            .annotate(tasks_total=Sum('tasks_taskmember_related__time_spent')) \
+            .values('id', 'tasks_total')
+
+        for member in members:
+            raw_data[member['id']]['tasks'] = member['tasks_total']
 
         return raw_data
 
@@ -175,30 +248,12 @@ class Command(BaseCommand):
             worksheet.write(self.row_counter_engagement_raw_data, 7, entry['tasks'])
             worksheet.write(self.row_counter_engagement_raw_data, 8, entry['year_last_seen'])
             worksheet.write(self.row_counter_engagement_raw_data, 9, entry['engagement_score'])
-            worksheet.write(self.row_counter_engagement_raw_data, 10,
-                            self.get_engagement_rating(entry['engagement_score']))
+            worksheet.write(self.row_counter_engagement_raw_data, 10, entry['engagement_rating'])
 
             self.row_counter_engagement_raw_data += 1
 
-    def generate_aggregated_data_worksheet(self, workbook, organisation, raw_data):
+    def generate_aggregated_data_worksheet(self, workbook, organisation, aggregated_data):
         worksheet_aggregated_data = self.initialize_aggregated_data_worksheet(workbook)
-
-        aggregated_data = self.generate_aggregate_score(raw_data)
-        aggregated_data['total_members'] = Member.objects.all().count()
-
-        projects_realised_count = Project.objects.filter(status__slug='done-complete',
-                                                         created__gte=self.start_date,
-                                                         created__lte=self.end_date).count()
-        projects_done_count = Project.objects.filter(status__slug='done-incomplete',
-                                                     created__gte=self.start_date,
-                                                     created__lte=self.end_date).count()
-        donations_anonymous_count = Order.objects.filter(created__gte=self.start_date,
-                                                         created__lte=self.end_date,
-                                                         status='success',
-                                                         user__isnull=True).count()
-        aggregated_data['projects_realised'] = projects_realised_count
-        aggregated_data['projects_done'] = projects_done_count
-        aggregated_data['donations_anonymous'] = donations_anonymous_count
         self.write_aggregated_data(organisation, worksheet_aggregated_data, aggregated_data)
 
     def initialize_aggregated_data_worksheet(self, workbook):
@@ -221,9 +276,6 @@ class Command(BaseCommand):
 
     def write_aggregated_data(self, organisation, worksheet, data):
 
-        total_engaged = data['engagement_score_engaged'] + data['engagement_score_very_engaged']
-        total_engaged_percentage = (total_engaged * 100) / data['total_members']
-
         worksheet.write(self.row_counter_engagement_aggregated_data, 0, organisation)
         worksheet.write(self.row_counter_engagement_aggregated_data, 1, 1)  # TODO: How do you count these ?
         worksheet.write(self.row_counter_engagement_aggregated_data, 2, data['total_members'])
@@ -231,20 +283,18 @@ class Command(BaseCommand):
         worksheet.write(self.row_counter_engagement_aggregated_data, 4, data['engagement_score_little_engaged'])
         worksheet.write(self.row_counter_engagement_aggregated_data, 5, data['engagement_score_engaged'])
         worksheet.write(self.row_counter_engagement_aggregated_data, 6, data['engagement_score_very_engaged'])
-        worksheet.write(self.row_counter_engagement_aggregated_data, 7, total_engaged)
-        worksheet.write(self.row_counter_engagement_aggregated_data, 8, total_engaged_percentage)
+        worksheet.write(self.row_counter_engagement_aggregated_data, 7, data['total_engaged'])
+        worksheet.write(self.row_counter_engagement_aggregated_data, 8, data['total_engaged_percentage'])
         worksheet.write(self.row_counter_engagement_aggregated_data, 9, data['projects_realised'])
         worksheet.write(self.row_counter_engagement_aggregated_data, 10, data['projects_done'])
         worksheet.write(self.row_counter_engagement_aggregated_data, 11, data['donations_anonymous'])
 
         self.row_counter_engagement_aggregated_data += 1
 
-    def generate_aggregate_score(self, data):
+    def generate_aggregated_data(self, data):
 
         aggregated_data = defaultdict(int)
 
-        # TODO: Do we need to really iterate twice to get engagement aggregate score
-        # TODO: Maybe try a different data structure integrated into raw_data
         for _, entry in data.iteritems():
             engagement_score = self.get_engagement_score(entry)
             engagement_rating = self.get_engagement_rating(engagement_score)
@@ -258,89 +308,22 @@ class Command(BaseCommand):
             elif engagement_rating == 'very engaged':
                 aggregated_data['engagement_score_very_engaged'] += 1
 
+        aggregated_data['total_members'] = Member.objects.all().count()
+
+        aggregated_data['projects_realised'] = Project.objects.filter(status__slug='done-complete',
+                                                                      created__gte=self.start_date,
+                                                                      created__lte=self.end_date).count()
+        aggregated_data['projects_done'] = Project.objects.filter(status__slug='done-incomplete',
+                                                                  created__gte=self.start_date,
+                                                                  created__lte=self.end_date).count()
+        aggregated_data['donations_anonymous'] = Order.objects.filter(created__gte=self.start_date,
+                                                                      created__lte=self.end_date,
+                                                                      status='success',
+                                                                      user__isnull=True).count()
+
+        aggregated_data['total_engaged'] = aggregated_data['engagement_score_engaged'] + \
+            aggregated_data['engagement_score_very_engaged']
+        aggregated_data['total_engaged_percentage'] = (aggregated_data['total_engaged'] * 100) / \
+            aggregated_data['total_members']
+
         return aggregated_data
-
-    def generate_raw_scores(self, data):
-        for _, entry in data.iteritems():
-            entry['engagement_score'] = self.get_engagement_score(entry)
-        return data
-
-    def generate_comments_raw_data(self, raw_data):
-        comments = Member.objects \
-            .filter(wallpost_wallpost__created__gte=self.start_date,
-                    wallpost_wallpost__created__lte=self.end_date) \
-            .annotate(total=Count('wallpost_wallpost')) \
-            .values('id', 'total')
-
-        for comment in comments:
-            # TODO: comment.id doesnt feel right. it should be comment.member_id
-            raw_data[comment['id']]['comments'] = comment['total']
-
-        return raw_data
-
-    def generate_votes_raw_data(self, raw_data):
-        votes = Member.objects \
-            .filter(vote__created__gte=self.start_date, vote__created__lte=self.end_date) \
-            .annotate(total=Count('vote')) \
-            .values('id', 'total')
-
-        for vote in votes:
-            # TODO: votes['id] doesnt feel right. it should be votes.member_id
-            raw_data[vote['id']]['votes'] = vote['total']
-
-        return raw_data
-
-    def generate_donations_raw_data(self, raw_data):
-        donations = Member.objects \
-            .filter(order__created__gte=self.start_date, order__created__lte=self.end_date,
-                    order__status="success", id__isnull=False) \
-            .annotate(total=Count('order')) \
-            .values('id', 'total')
-
-        for donation in donations:
-            # TODO: donation['id] doesnt feel right. it should be donation.member_id
-            raw_data[donation['id']]['donations'] = donation['total']
-
-        return raw_data
-
-    def generate_fundraisers_raw_data(self, raw_data):
-        fundraisers = Member.objects \
-            .filter(fundraiser__created__gte=self.start_date, fundraiser__created__lte=self.end_date) \
-            .annotate(total=Count('fundraiser')) \
-            .values('id', 'total')
-
-        for fundraiser in fundraisers:
-            # TODO: fundraiser['id] doesnt feel right. it should be fundraiser.member_id
-            raw_data[fundraiser['id']]['fundraisers'] = fundraiser['total']
-
-        return raw_data
-
-    def generate_projects_raw_data(self, raw_data):
-        projects = Member.objects \
-            .filter(owner__created__gte=self.start_date,
-                    owner__created__lte=self.end_date,
-                    owner__status__slug__in=['voting', 'voting-done', 'campaign',
-                                             'to-be-continued', 'done-complete',
-                                             'done-incomplete']) \
-            .annotate(total=Count('owner')) \
-            .values('id', 'total')
-
-        for project in projects:
-            raw_data[project['id']]['projects'] = project['total']
-
-        return raw_data
-
-    def generate_tasks_raw_data(self, raw_data):
-        tasks = Member.objects \
-            .filter(tasks_taskmember_related__created__gte=self.start_date,
-                    tasks_taskmember_related__created__lte=self.end_date,
-                    tasks_taskmember_related__status='realized',
-                    tasks_taskmember_related__time_spent__gt=0) \
-            .annotate(total=Sum('tasks_taskmember_related__time_spent')) \
-            .values('id', 'total')
-
-        for task in tasks:
-            # TODO: task['id] doesnt feel right. it should be task.member_id
-            raw_data[task['id']]['tasks'] = task['total']
-
-        return raw_data

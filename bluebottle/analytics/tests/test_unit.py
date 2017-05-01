adff6e180aa2f137a5f89fc7eae82d371db15ed5
Ashutosh <ashutosh@onepercentclub.com>
2017-05-01 20:05:33 +0200

    fix failing test

--- bluebottle/analytics/tests/test_unit.py
+++ bluebottle/analytics/tests/test_unit.py
@@ -75,7 +75,7 @@ class TestFileAnalyticsQueue(SimpleTestCase):
         timestamp = datetime(2016, 12, 31, 23, 59, 59, 123456)
         queue_analytics_record(timestamp=timestamp, tags=self.tags, fields=self.fields)
 
-        log_path = os.path.join(self.log_dir, '{}.log'.format(timestamp.strftime('%Y-%m-%d')))
+        log_path = os.path.join(self.log_dir, '{}.log'.format(timestamp.strftime('%Y-%m')))
         self.assertTrue(os.path.exists(log_path))
 
         # Get last line from log

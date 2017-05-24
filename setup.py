5d81410c836953fa03d588afdb3f61d8ce61ea2a
Ashutosh <ashutosh@onepercentclub.com>
2017-05-25 01:00:15 +0200

    fix sorl-thumbnail dependency

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ dependency_links = [
     'git+https://github.com/onepercentclub/django-taggit-autocomplete-modified.git@8e7fbc2deae2f1fbb31b574bc8819d9ae7c644d6#egg=django-taggit-autocomplete-modified-0.1.1b1',
     'git+https://github.com/onepercentclub/django-bb-salesforce.git@1.2.2#egg=django-bb-salesforce-1.2.2',
     'git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.11#egg=django-tenant-extras-2.0.11',
-    'git+https://github.com/mariocesar/sorl-thumbnail.git@v12.3#egg=sorl-thumbnail-12.3-github',
     'git+https://github.com/onepercentclub/django-token-auth.git@0.3.4#egg=django-token-auth-0.3.4',
     'git+https://github.com/st4lk/django-select-multiple-field.git@1dc7733008150a111cd141ff7c3f42bf4953dc7d#egg=django-select-multiple-field-0.5.0a-draft',
     # Use pre-release version of django-money as long as
@@ -87,7 +86,7 @@ install_requires = [
     'raven==5.16.0',
     'regex==2016.4.25',
     'requests==2.14.2',
-    'sorl-thumbnail==12.3-github',
+    'sorl-thumbnail==12.3',
     'sorl-watermark==1.0.0',
     'South==1.0.2',
     'Sphinx==1.4.1',

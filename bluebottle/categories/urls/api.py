90e4631bb35b2e2f96095599e36356498eeaec30
Ashutosh <ashutosh@onepercentclub.com>
2017-07-13 21:22:49 +0200

    add category content url config

--- bluebottle/categories/urls/api.py
+++ bluebottle/categories/urls/api.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from ..views import CategoryList
+from ..views import CategoryList, CategoryDetail
 
 urlpatterns = [
     url(r'^$', CategoryList.as_view(), name='category-list'),
+    url(r'^(?P<slug>[\w-]+)$', CategoryDetail.as_view(), name='category-detail'),
 ]

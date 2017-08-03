7c67f6fa247443b3ccd787099b557b66241f36cc
Ashutosh <ashutosh@onepercentclub.com>
2017-08-03 17:35:21 +0200

    remove unused metrics from participation stats

--- bluebottle/statistics/participation.py
+++ bluebottle/statistics/participation.py
@@ -1,20 +1,11 @@
 import itertools
 
-from django.db.models import Q, F
-from django.db.models.aggregates import Sum
-from memoize import memoize
-from moneyed.classes import Money
 import pendulum
+from django.db.models import Q, F
+from memoize import memoize
 
-from bluebottle.clients import properties
-from bluebottle.donations.models import Donation
-from bluebottle.fundraisers.models import Fundraiser
-from bluebottle.orders.models import Order
 from bluebottle.projects.models import Project, ProjectPhaseLog
 from bluebottle.tasks.models import Task, TaskMember, TaskStatusLog, TaskMemberStatusLog
-from bluebottle.utils.exchange_rates import convert
-from bluebottle.utils.utils import StatusDefinition
-from bluebottle.votes.models import Vote
 
 
 class Statistics(object):
@@ -41,71 +32,6 @@ class Statistics(object):
             filter_args = {}
 
         return Q(**filter_args)
-
-    @property
-    @memoize(timeout=60 * 60)
-    def people_involved(self):
-        """
-        The (unique) total number of people that donated, fundraised, campaigned, or was a
-        task owner or  member.
-        """
-        donor_ids = Order.objects.filter(
-            self.date_filter('confirmed'),
-            user_id__isnull=False,
-            status__in=(StatusDefinition.PENDING, StatusDefinition.SUCCESS)
-        ).order_by(
-            'user__id'
-        ).distinct('user').values_list('user_id', flat=True)
-
-        fundraiser_owner_ids = Fundraiser.objects.filter(
-            self.date_filter('deadline'),
-        ).order_by(
-            'owner__id'
-        ).distinct('owner').values_list('owner_id', flat=True)
-
-        project_owner_ids = Project.objects.filter(
-            self.date_filter('created'),
-            status__slug__in=(
-                'voting', 'voting-done', 'to-be-continued', 'campaign', 'done-complete', 'done-incomplete'
-            )
-        ).order_by(
-            'owner__id'
-        ).distinct('owner').values_list('owner_id', flat=True)
-
-        task_member_ids = TaskMember.objects.filter(
-            self.date_filter('task__deadline'),
-            status__in=('realized', 'accepted', 'applied')
-        ).order_by('member__id').distinct(
-            'member'
-        ).values_list('member_id', flat=True)
-
-        task_owner_ids = Task.objects.filter(
-            self.date_filter()
-        ).order_by('author__id').distinct(
-            'author'
-        ).values_list('author_id', flat=True)
-
-        people_count = len(
-            set(donor_ids) | set(fundraiser_owner_ids) | set(project_owner_ids) |
-            set(task_member_ids) | set(task_owner_ids)
-        )
-
-        # Add anonymous donations
-        people_count += len(Order.objects.filter(
-            self.date_filter('completed'),
-            user_id=None,
-            status__in=(StatusDefinition.PENDING, StatusDefinition.SUCCESS)
-        ))
-
-        # Add "plus one"
-        people_count += TaskMember.objects.filter(
-            self.date_filter('task__deadline'),
-            status__in=['accepted', 'realized']
-        ).aggregate(
-            externals=Sum('externals')
-        )['externals'] or 0
-
-        return people_count
 
     @property
     @memoize(timeout=60 * 60)
@@ -437,57 +363,6 @@ class Statistics(object):
                 count += log.task_member.time_spent
 
         return count
-
-    @property
-    @memoize(timeout=60 * 60)
-    def donated_total(self):
-        """ Total amount donated to all projects"""
-        donations = Donation.objects.filter(
-            self.date_filter('order__confirmed'),
-            order__status__in=['pending', 'success', 'pledged']
-        )
-        totals = donations.values('amount_currency').annotate(total=Sum('amount'))
-        amounts = [Money(total['total'], total['amount_currency']) for total in totals]
-        if totals:
-            donated = sum([convert(amount, properties.DEFAULT_CURRENCY) for amount in amounts])
-        else:
-            donated = Money(0, properties.DEFAULT_CURRENCY)
-
-        return donated
-
-    @property
-    @memoize(timeout=60 * 60)
-    def amount_matched(self):
-        """ Total amount matched on realized (done and incomplete) projects """
-        totals = Project.objects.values('amount_extra_currency').annotate(total=Sum('amount_extra'))
-
-        amounts = [Money(total['total'], total['amount_extra_currency']) for total in totals]
-        if totals:
-            return sum([convert(amount, properties.DEFAULT_CURRENCY) for amount in amounts])
-        else:
-            return Money(0, properties.DEFAULT_CURRENCY)
-
-    @property
-    @memoize(timeout=60 * 60)
-    def pledged_total(self):
-        """ Total amount of pledged donations """
-        donations = Donation.objects.filter(
-            self.date_filter('order__confirmed'),
-            order__status='pledged'
-        )
-        totals = donations.values('amount_currency').annotate(total=Sum('amount'))
-        amounts = [Money(total['total'], total['amount_currency']) for total in totals]
-        if totals:
-            donated = sum([convert(amount, properties.DEFAULT_CURRENCY) for amount in amounts])
-        else:
-            donated = Money(0, properties.DEFAULT_CURRENCY)
-
-        return donated
-
-    @property
-    @memoize(timeout=60 * 60)
-    def votes_cast(self):
-        return len(Vote.objects.filter(self.date_filter()))
 
     def __repr__(self):
         start = self.start.strftime('%s') if self.start else 'none'

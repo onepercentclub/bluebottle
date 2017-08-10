4e3c4f405663a6246de73fae62a761c17408054e
rollick <mark@rollick.id.au>
2017-08-10 10:32:05 +0200

    pep8 fixes

--- bluebottle/bb_tasks/permissions.py
+++ bluebottle/bb_tasks/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-from bluebottle.tasks.models import Task, TaskMember
+from bluebottle.tasks.models import TaskMember
 from bluebottle.utils.utils import get_class
 from bluebottle.utils.permissions import BasePermission, RelatedResourceOwnerPermission
 
@@ -32,8 +32,8 @@ class MemberOrOwnerOrReadOnlyPermission(BasePermission):
         if method in permissions.SAFE_METHODS:
             return True
 
-	# FIXME: when this permission is used with the update task member
-	#        then the obj is still a Task. Why?
+        # FIXME: when this permission is used with the update task member
+        #        then the obj is still a Task. Why?
         if isinstance(obj, TaskMember) and obj.task.owner == user:
             return True
 

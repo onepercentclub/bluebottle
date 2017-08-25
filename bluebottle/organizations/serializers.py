ff136e0e76cade72fb8903a9d7323814fb7180e8
Ashutosh <ashutosh@onepercentclub.com>
2017-08-25 15:02:06 +0200

    add members and projects to organization payload

--- bluebottle/organizations/serializers.py
+++ bluebottle/organizations/serializers.py
@@ -6,23 +6,37 @@ from bluebottle.utils.serializers import URLField
 
 
 class OrganizationPreviewSerializer(serializers.ModelSerializer):
-    slug = serializers.SlugField(required=False, allow_null=True)
+    slug = serializers.SlugField(allow_null=True, required=False)
     name = serializers.CharField(required=True)
-    website = URLField(required=False, allow_blank=True)
-    logo = ImageSerializer(required=False, allow_null=True)
+    website = URLField(allow_blank=True, required=False)
+    logo = ImageSerializer(allow_null=True, required=False)
+    members = serializers.SlugRelatedField(many=True,
+                                           read_only=True,
+                                           slug_field='full_name',
+                                           source='partner_organization_members')
+    projects = serializers.SlugRelatedField(many=True,
+                                            read_only=True,
+                                            slug_field='title')
 
     class Meta:
         model = Organization
-        fields = ('id', 'name', 'slug', 'website', 'logo')
+        fields = ('id', 'name', 'slug', 'website', 'logo', 'members', 'projects')
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
-    slug = serializers.SlugField(required=False, allow_null=True)
+    slug = serializers.SlugField(allow_null=True, required=False)
     name = serializers.CharField(required=True)
-    website = URLField(required=False, allow_blank=True)
-    email = serializers.EmailField(required=False, allow_blank=True)
+    website = URLField(allow_blank=True, required=False)
+    email = serializers.EmailField(allow_blank=True, required=False)
     contacts = serializers.SerializerMethodField()
-    logo = ImageSerializer(required=False, allow_null=True)
+    logo = ImageSerializer(allow_null=True, required=False)
+    members = serializers.SlugRelatedField(many=True,
+                                           read_only=True,
+                                           slug_field='full_name',
+                                           source='partner_organization_members')
+    projects = serializers.SlugRelatedField(many=True,
+                                            read_only=True,
+                                            slug_field='title')
 
     def get_contacts(self, obj):
         owner = self.context['request'].user
@@ -39,7 +53,7 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'slug', 'address_line1', 'address_line2',
                   'city', 'state', 'country', 'postal_code', 'phone_number',
                   'website', 'email', 'contacts', 'partner_organizations',
-                  'created', 'updated', 'logo')
+                  'created', 'updated', 'logo', 'members', 'projects')
 
 
 class OrganizationContactSerializer(serializers.ModelSerializer):

ccc84ac275f5b19f939fd1872c21717ce2a4c5ab
Loek van Gent <loek@onepercentclub.com>
2017-08-31 11:28:48 +0200

    Fix test

--- bluebottle/tasks/migrations/0034_more_owner_permissions.py
+++ bluebottle/tasks/migrations/0034_more_owner_permissions.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-from django.contrib.auth.models import Permission, Group
 
 from bluebottle.utils.utils import update_group_permissions
 

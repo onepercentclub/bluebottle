c594871eeb1fb73e03c9bd725ce68f3a675e8882
requires.io <support@requires.io>
2017-10-03 01:20:34 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
     'Pillow==4.3.0',
     'South==1.0.2',
     'beautifulsoup4==4.6.0',
-    'bleach==2.1',
+    'bleach==2.1.1',
     'bunch==1.0.1',
     'celery==4.1.0',
     'django-admin-sortable==2.1',

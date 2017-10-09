f950e0e75642b477fd61811367900d20ff8650e1
Loek van Gent <loek@onepercentclub.com>
2017-10-09 14:14:05 +0200

    Fix content blocks

--- bluebottle/cms/content_plugins.py
+++ bluebottle/cms/content_plugins.py
@@ -1,7 +1,6 @@
-from django.utils.translation import ugettext_lazy as _
-
 from fluent_contents.extensions import plugin_pool, ContentPlugin
 
+from bluebottle.cms.admin import QuoteInline, StatInline
 from bluebottle.cms.models import (
     QuotesContent, StatsContent, SurveyContent, ProjectsContent,
     ProjectImagesContent, ShareResultsContent, ProjectsMapContent,
@@ -16,53 +15,43 @@ class CMSContentPlugin(ContentPlugin):
 @plugin_pool.register
 class QuotesBlockPlugin(CMSContentPlugin):
     model = QuotesContent
-    fieldsets = (
-        (None, {'fields': ('quotes',), }),
-    )
-    category = _('Results')
+    inlines = [QuoteInline]
 
 
 @plugin_pool.register
 class StatsBlockPlugin(CMSContentPlugin):
     model = StatsContent
-    category = _('Stats')
+    inlines = [StatInline]
 
 
 @plugin_pool.register
 class SurveyBlockPlugin(CMSContentPlugin):
     model = SurveyContent
-    category = _('Results')
 
 
 @plugin_pool.register
 class ProjectsBlockPlugin(CMSContentPlugin):
     model = ProjectsContent
-    category = _('Projects')
 
 
 @plugin_pool.register
 class ProjectImagesBlockPlugin(CMSContentPlugin):
     model = ProjectImagesContent
-    category = _('Projects')
 
 
 @plugin_pool.register
 class ShareResultsBlockPlugin(CMSContentPlugin):
     model = ShareResultsContent
 
-    category = _('Results')
-
 
 @plugin_pool.register
 class ProjectMapBlockPlugin(CMSContentPlugin):
     model = ProjectsMapContent
-    category = _('Projects')
 
 
 @plugin_pool.register
 class SupporterTotalBlockPlugin(CMSContentPlugin):
     model = SupporterTotalContent
-    category = _('Results')
 
 
 @plugin_pool.register

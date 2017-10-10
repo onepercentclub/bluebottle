2a233c76dbbc0702b2e267e7ab70b6ad687ed61b
Loek van Gent <loek@onepercentclub.com>
2017-10-10 15:45:11 +0200

    New fluent contents with smal fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dependency_links = [
     'git+https://github.com/onepercentclub/django-taggit-autocomplete-modified.git@8e7fbc2deae2f1fbb31b574bc8819d9ae7c644d6#egg=django-taggit-autocomplete-modified-0.1.1b1',
     'git+https://github.com/onepercentclub/django-bb-salesforce.git@1.2.2#egg=django-bb-salesforce-1.2.2',
     'git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.12#egg=django-tenant-extras-2.0.12',
-    'git+https://github.com/onepercentclub/django-fluent-contents.git@f496282aacbb188ada87c0464f2c3fb3e1bce9a8#egg=django-fluent-contents-1.2.1-f49628',
+    'git+https://github.com/onepercentclub/django-fluent-contents.git@741ffae615a4afed01388a202709ed9a5b60e80f#egg=django-fluent-contents-1.2.1-741ffae6',
     'git+https://github.com/mariocesar/sorl-thumbnail.git@v12.3#egg=sorl-thumbnail-12.3-github',
     'git+https://github.com/onepercentclub/django-token-auth.git@0.3.4#egg=django-token-auth-0.3.4',
     'git+https://github.com/st4lk/django-select-multiple-field.git@1dc7733008150a111cd141ff7c3f42bf4953dc7d#egg=django-select-multiple-field-0.5.0a-draft',
@@ -101,7 +101,7 @@ install_requires = [
     'xlsxwriter==0.9.8',
 
     # Github requirements
-    'django-fluent-contents==1.2.1-f49628',
+    'django-fluent-contents==1.2.1-741ffae6',
     'django-tenant-extras==2.0.12',
     'django-bb-salesforce==1.2.2',
     'django-taggit-autocomplete-modified==0.1.1b1',

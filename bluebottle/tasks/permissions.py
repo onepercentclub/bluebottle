1285ad16dc53e4e3d14b77b9f9c82e1f7f0572cd
Ernst Odolphi <ernst@onepercentclub.com>
2017-10-10 17:24:47 +0200

    Make it possible to send custom messages when confirming / rejecting users.
    Adds an API endpoint /api/tasks/members/status/id that makes it possible
    to get and set the status of a taskmember. A message has to be passed in
    that will become part of the email send to the member.
    
    BB-9498 #resolve

--- bluebottle/tasks/permissions.py
+++ bluebottle/tasks/permissions.py
@@ -23,3 +23,9 @@ class TaskMemberPermission(RelatedResourceOwnerPermission):
             self.has_parent_permission(action, user, obj.task) or
             user == obj.member
         )
+
+
+class TaskManagerPermission(RelatedResourceOwnerPermission):
+    def has_parent_permission(self, action, user, parent, model=None):
+        return user == parent.project.task_manager
+

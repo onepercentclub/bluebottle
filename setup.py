6a4e79ca0ec923a4df78d840a9d8b45127326018
requires.io <support@requires.io>
2017-10-13 14:20:58 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ changes = ''
 dependency_links = [
     'git+https://github.com/onepercentclub/django-taggit-autocomplete-modified.git@8e7fbc2deae2f1fbb31b574bc8819d9ae7c644d6#egg=django-taggit-autocomplete-modified-0.1.1b1',
     'git+https://github.com/onepercentclub/django-bb-salesforce.git@1.2.2#egg=django-bb-salesforce-1.2.2',
-    'git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.12#egg=django-tenant-extras-2.0.12',
+    'git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.13#egg=django-tenant-extras-2.0.13',
     'git+https://github.com/mariocesar/sorl-thumbnail.git@v12.3#egg=sorl-thumbnail-12.3-github',
     'git+https://github.com/onepercentclub/django-token-auth.git@0.3.4#egg=django-token-auth-0.3.4',
     'git+https://github.com/st4lk/django-select-multiple-field.git@1dc7733008150a111cd141ff7c3f42bf4953dc7d#egg=django-select-multiple-field-0.5.0a-draft',
@@ -101,7 +101,7 @@ install_requires = [
 
     # Github requirements
     'django-fluent-contents==1.2.1',
-    'django-tenant-extras==2.0.12',
+    'django-tenant-extras==2.0.13',
     'django-bb-salesforce==1.2.2',
     'django-taggit-autocomplete-modified==0.1.0',
     'django-token-auth==1.3.2b4',

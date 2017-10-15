f8387ffcf333ee163e0b9e468c426fd466e96ae5
requires.io <support@requires.io>
2017-10-15 14:53:35 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ install_requires = [
     'transifex-client==0.12.4',
     'unicodecsv==0.14.1',
     'wheel==0.30.0',
-    'xlsxwriter==1.0.1',
+    'xlsxwriter==1.0.2',
 
     # Github requirements
     'django-fluent-contents==1.2.1',

68595b7aafa29acaaafc5cf755f15cd5bcf243fa
rollick <mark@rollick.id.au>
2017-10-24 17:22:10 +0200

    import site link permissions

--- bluebottle/clients/management/commands/create_site_links.py
+++ bluebottle/clients/management/commands/create_site_links.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand, CommandError
 from bluebottle.clients.models import Client
 from bluebottle.clients import properties
 from bluebottle.clients.utils import LocalTenant
-from bluebottle.cms.models import SiteLinks, LinkGroup, Link  # , LinkPermission
+from bluebottle.cms.models import SiteLinks, LinkGroup, Link, LinkPermission
 from bluebottle.utils.models import Language
 
 
@@ -40,6 +40,13 @@ class Command(BaseCommand):
         )
 
     def _create_links(self, force):
+        def _log(info, created):
+            if created:
+                label = 'created'
+            else:
+                label = 'exists'
+            print('{} [{}]'.format(info, label))
+
         site_links = getattr(properties, 'SITE_LINKS', None)
         if site_links:
             # get languages
@@ -51,17 +58,30 @@ class Command(BaseCommand):
                 for group_name in lang_links.keys():
                     if group_name in ['main', 'about', 'info', 'discover', 'social']:
                         group_links = lang_links[group_name]
-                        lg, create = LinkGroup.objects.get_or_create(name=group_name,
-                                                                     site_links=sl,
-                                                                     title=group_links.get('title', ''))
+                        lg, created = LinkGroup.objects.get_or_create(name=group_name,
+                                                                      site_links=sl,
+                                                                      title=group_links.get('title', ''))
+                        _log('Link Group called {}'.format(group_name), created)
+
                         for link in group_links['links']:
-                            print(link)
                             l, created = Link.objects.get_or_create(link_group=lg,
                                                                     highlight=link.get('highlighted', False),
                                                                     title=link.get('title', 'N/A'),
                                                                     component=link.get('component', None),
                                                                     component_id=link.get('component_id', None),
                                                                     external_link=link.get('external_link', None))
+
+                            _log('  Link called {} created for {}'.format(link.get('title', 'N/A'), group_name),
+                                 created)
+
+                            perms = link.get('permissions', [])
+                            for perm in perms:
+                                p, created = LinkPermission.objects.get_or_create(permission=perm['permission'],
+                                                                                  present=perm.get('present', True))
+
+                                l.link_permissions.add(p)
+                                _log('    Permission called {} ({}) for {}'.format(perm['permission'], perm['present'],
+                                                                                   link.get('title', 'N/A')), created)
 
     def handle(self, *args, **options):
         if options['all'] and options['tenant']:

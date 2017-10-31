6d05bd7301b842bd24ca3bee744dc255a17e063d
Loek van Gent <loek@onepercentclub.com>
2017-10-31 12:24:25 +0100

    Fix migration and test analytics platform settings

--- bluebottle/analytics/migrations/0001_initial.py
+++ bluebottle/analytics/migrations/0001_initial.py
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='analyticsadapter',
-            name='metrics_settings',
+            name='analytics_settings',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='adapters', to='analytics.AnalyticsPlatformSettings'),
         ),
     ]

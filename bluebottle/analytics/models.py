1ad6d28209d5629ffa77989632f91b270ae45de5
Loek van Gent <loek@onepercentclub.com>
2017-10-31 08:57:22 +0100

    Add analytics settings to api. Make things generic.

---
+++ bluebottle/analytics/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from bluebottle.utils.models import BasePlatformSettings
+
+
+class AnalyticsAdapter(models.Model):
+    type = models.CharField(max_length=100, default='GoogleAnalytics')
+    code = models.CharField(max_length=100, null=True, blank=True)
+    metrics_settings = models.ForeignKey('analytics.AnalyticsPlatformSettings', related_name='adapters')
+
+
+class AnalyticsPlatformSettings(BasePlatformSettings):
+
+    class Meta:
+        verbose_name_plural = _('analytics platform settings')
+        verbose_name = _('analytics platform settings')

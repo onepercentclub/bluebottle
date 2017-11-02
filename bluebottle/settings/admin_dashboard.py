f9a81d11d600ba47e8330e23a00422217e4ba7d2
Loek van Gent <loek@onepercentclub.com>
2017-11-02 12:30:22 +0100

    Add analytics to settings menu in admin

--- bluebottle/settings/admin_dashboard.py
+++ bluebottle/settings/admin_dashboard.py
@@ -103,6 +103,7 @@ FLUENT_DASHBOARD_APP_GROUPS = (
         'models': (
             'bluebottle.cms.models.SitePlatformSettings',
             'bluebottle.projects.models.ProjectPlatformSettings',
+            'bluebottle.analytics.models.AnalyticsPlatformSettings',
         ),
         'module': 'fluent_dashboard.modules.AppIconList',
         'collapsible': False,

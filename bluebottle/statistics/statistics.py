8eca5166795efba270a0dc7c64810053a5425a05
Ernst Odolphi <ernst@onepercentclub.com>
2017-11-08 12:07:11 +0100

    Make participants query much more efficient

--- bluebottle/statistics/statistics.py
+++ bluebottle/statistics/statistics.py
@@ -149,17 +149,16 @@ class Statistics(object):
         This will get the last status log entry for all project phase logs
         """
 
-        phase_logs = ProjectPhaseLog.objects\
-            .filter(self.date_filter('start'))\
-            .distinct('project__id')\
-            .order_by('-project__id', '-start')
-
-        # TODO: Refactor to use django filters for sub-queries
-        count = 0
-        for log in phase_logs:
-            if log.status.slug in ['done-complete', 'done-incomplete', 'voting-done']:
-                count += 1
-        return count
+        phase_logs = ProjectPhaseLog.objects.filter(
+            self.date_filter('start')
+        ).filter(
+            status__slug__in=['done-complete', 'done-incomplete', 'voting-done']
+        ).distinct(
+            'project__id'
+        ).order_by(
+            '-project__id', '-start'
+        )
+        return len(phase_logs)
 
     @property
     @memoize(timeout=60 * 60)
@@ -294,7 +293,6 @@ class Statistics(object):
     @memoize(timeout=300)
     def members(self):
         """ Total amount of members."""
-        import ipdb; ipdb.set_trace()
         members = Member.objects.filter(
             self.date_filter('created'),
             is_active=True

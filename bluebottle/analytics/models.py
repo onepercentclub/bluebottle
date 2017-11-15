fa4209b83eaf9464e4fac80b36b7345494e8dd2f
Loek van Gent <loek@onepercentclub.com>
2017-11-15 15:11:13 +0100

    Use PgView and move queries to ORM

--- bluebottle/analytics/models.py
+++ bluebottle/analytics/models.py
@@ -1,95 +1,146 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django_pgviews import view as pg
 
 
-def get_report_model(db_table):
-    class ReportMetaClass(models.base.ModelBase):
-        def __new__(cls, name, bases, attrs):
-            model = super(ReportMetaClass, cls).__new__(cls, name, bases, attrs)
-            model._meta.db_table = db_table
-            model._meta.managed = False
-            return model
+class BaseRawReport(models.Model):
+    tenant = models.CharField(_('tenant'), max_length=255, primary_key=True)
+    type = models.CharField(_('type'), max_length=255)
+    type_id = models.PositiveIntegerField(_('type_id'))
+    parent_id = models.PositiveIntegerField(_('parent_id'))
+    timestamp = models.DateTimeField(_('timestamp'))
+    status = models.CharField(_('status'), max_length=20)
+    event_timestamp = models.DateTimeField(_('event_timestamp'))
+    event_status = models.CharField(_('event_status'), max_length=20)
+    user_id = models.PositiveIntegerField(_('user_id'))
+    year = models.PositiveSmallIntegerField(_('year'))
+    quarter = models.PositiveSmallIntegerField(_('quarter'))
+    month = models.PositiveSmallIntegerField(_('month'))
+    week = models.PositiveSmallIntegerField(_('week'))
+    location = models.CharField(_('location'), max_length=255)
+    location_group = models.CharField(_('location_group'), max_length=255)
+    value = models.IntegerField(_('value'))
 
-    class ReportClass(models.Model):
-        __metaclass__ = ReportMetaClass
-
-        #  Column  |           Type         |
-        # ---------+------------------------+
-        # year     | double precision       |
-        # quarter  | double precision       |
-        # month    | double precision       |
-        # location | character varying(255) |
-        # type     | character varying      |
-        # value    | bigint                 |
-        year = models.PositiveSmallIntegerField(_('year'))
-        quarter = models.PositiveSmallIntegerField(_('quarter'))
-        month = models.PositiveSmallIntegerField(_('month'), primary_key=True)
-        location = models.CharField(_('location'), max_length=255)
-        type = models.CharField(_('type'), max_length=20)
-        value = models.IntegerField(_('value'))
-
-    return ReportClass
+    class Meta:
+        abstract = True
 
 
-def get_raw_report_model(db_table):
-    class RawReportMetaClass(models.base.ModelBase):
-        def __new__(cls, name, bases, attrs):
-            model = super(RawReportMetaClass, cls).__new__(cls, name, bases, attrs)
-            model._meta.db_table = db_table
-            model._meta.managed = False
-            return model
-
-    class RawReportClass(models.Model):
-        __metaclass__ = RawReportMetaClass
-
-        #     Column      |           Type           |
-        # ----------------+--------------------------+
-        # tenant          | name                     |
-        # type            | character varying        |
-        # type_id         | integer                  |
-        # parent_id       | integer                  |
-        # timestamp       | timestamp with time zone |
-        # status          | character varying(20)    |
-        # event_timestamp | timestamp with time zone |
-        # event_status    | character varying(20)    |
-        # user_id         | integer                  |
-        # year            | double precision         |
-        # quarter         | double precision         |
-        # month           | double precision         |
-        # week            | double precision         |
-        # location        | character varying(255)   |
-        # location_group  | character varying(255)   |
-        # value           | integer                  |
-        tenant = models.CharField(_('tenant'), max_length=255, primary_key=True)
-        type = models.CharField(_('type'), max_length=255)
-        type_id = models.PositiveIntegerField(_('type_id'))
-        parent_id = models.PositiveIntegerField(_('parent_id'))
-        timestamp = models.DateTimeField(_('timestamp'))
-        status = models.CharField(_('status'), max_length=20)
-        event_timestamp = models.DateTimeField(_('event_timestamp'))
-        event_status = models.CharField(_('event_status'), max_length=20)
-        user_id = models.PositiveIntegerField(_('user_id'))
-        year = models.PositiveSmallIntegerField(_('year'))
-        quarter = models.PositiveSmallIntegerField(_('quarter'))
-        month = models.PositiveSmallIntegerField(_('month'))
-        week = models.PositiveSmallIntegerField(_('week'))
-        location = models.CharField(_('location'), max_length=255)
-        location_group = models.CharField(_('location_group'), max_length=255)
-        value = models.IntegerField(_('value'))
-
-    return RawReportClass
+class ProjectDoneManager(models.Manager):
+    def get_queryset(self):
+        return super(ProjectDoneManager, self).get_queryset(). \
+            filter(status='done-complete', event_status='done-complete')
 
 
-def export_report(table_name):
-    ReportModel = get_report_model(table_name)
-    results = ReportModel.objects.all()
-    for row in results:
-        print(int(row.year), int(row.quarter), int(row.month), row.location, row.type, int(row.value))
+class ProjectRawReport(BaseRawReport, pg.View):
+    projection = ['analytics.BaseRawReport.*', ]
+    done_objects = ProjectDoneManager()
+
+    sql = """
+        SELECT "current_schema"() AS tenant,
+            'project'::character varying AS type,
+            p.id AS type_id,
+            NULL::integer AS parent_id,
+            p.campaign_ended AS "timestamp",
+            pp.slug::character varying(20) AS status,
+            pl.start AS event_timestamp,
+            plp.slug::character varying(20) AS event_status,
+            p.owner_id AS user_id,
+            date_part('year'::text, pl.start) AS year,
+            date_part('quarter'::text, pl.start) AS quarter,
+            date_part('month'::text, pl.start) AS month,
+            date_part('week'::text, pl.start) AS week,
+            l.name AS location,
+            lg.name AS location_group,
+            1 AS value
+            FROM dll.projects_project p
+            LEFT JOIN (
+                SELECT max(start) AS start, project_id, status_id
+                FROM dll.projects_projectphaselog
+                GROUP BY project_id, status_id
+            ) pl ON pl.project_id = p.id
+            LEFT JOIN dll.bb_projects_projectphase plp ON plp.id = pl.status_id
+            LEFT JOIN dll.bb_projects_projectphase pp ON pp.id = p.status_id
+            LEFT JOIN dll.geo_location l ON p.location_id = l.id
+            LEFT JOIN dll.geo_locationgroup lg ON l.group_id = lg.id;
+           """
+
+    class Meta:
+        app_label = 'analytics'
+        db_table = 'v_projects'
+        managed = False
 
 
-def export_raw_report(table_name):
-    RawReportModel = get_raw_report_model(table_name)
-    results = RawReportModel.objects.all()
-    for row in results:
-        print(row.type, int(row.type_id), int(row.parent_id or 0),
-              row.timestamp, row.status, row.location, row.type, int(row.value))
+class TaskRawReport(BaseRawReport, pg.View):
+    projection = ['analytics.BaseRawReport.*', ]
+    sql = """
+        SELECT current_schema() as tenant,
+            'task'::varchar as type,
+            t.id as type_id,
+            t.project_id as parent_id,
+            t.deadline as timestamp,
+            t.status as status,
+            tl.start as event_timestamp,
+            tl.status as event_status,
+            author_id as user_id,
+            EXTRACT(YEAR FROM t.deadline) as year,
+            EXTRACT(QUARTER FROM t.deadline) as quarter,
+            EXTRACT(MONTH FROM t.deadline) as month,
+            EXTRACT(WEEK FROM t.deadline) as week,
+            l.name as location,
+            lg.name as location_group,
+            1 as value
+        FROM tasks_task as t
+        LEFT JOIN tasks_taskstatuslog as tl
+        ON tl.task_id = t.id
+        LEFT JOIN projects_project as p
+        ON p.id = t.project_id
+        LEFT JOIN geo_location as l
+        ON p.location_id = l.id
+        LEFT JOIN geo_locationgroup as lg
+        ON l.group_id = lg.id
+    """
+
+    class Meta:
+        app_label = 'analytics'
+        db_table = 'v_tasks'
+        managed = False
+
+
+class TaskMemberRawReport(BaseRawReport, pg.View):
+    projection = ['analytics.BaseRawReport.*', ]
+    sql = """
+        SELECT current_schema() as tenant,
+               'taskmember_hours'::varchar as type,
+            tm.id as type_id,
+            t.type_id as parent_id,
+            t.timestamp as timestamp,
+            tm.status as status,
+            tml.start as event_timestamp,
+            tml.status as event_status,
+            member_id as user_id,
+            EXTRACT(YEAR FROM t.timestamp) as year,
+            EXTRACT(QUARTER FROM t.timestamp) as quarter,
+            EXTRACT(MONTH FROM t.timestamp) as month,
+            EXTRACT(WEEK FROM t.timestamp) as week,
+            l.name as location,
+            lg.name as location_group,
+            tm.time_spent::integer as value
+        FROM tasks_taskmember as tm
+        LEFT JOIN tasks_taskmemberstatuslog as tml
+        ON tml.task_member_id = tm.id
+        LEFT JOIN (
+            SELECT DISTINCT ON (1) v_tasks.type_id as task_type_id, *
+            FROM v_tasks) as t
+        ON t.task_type_id = tm.task_id
+        LEFT JOIN projects_project as p
+        ON p.id = t.parent_id
+        LEFT JOIN geo_location as l
+        ON p.location_id = l.id
+        LEFT JOIN geo_locationgroup as lg
+        ON l.group_id = lg.id)
+    """
+
+    class Meta:
+        app_label = 'analytics'
+        db_table = 'v_taskmembers'
+        managed = False
--- bluebottle/analytics/reports.py
+++ bluebottle/analytics/reports.py
@@ -1,10 +1,11 @@
-import pendulum
 import xlsxwriter
 import calendar
 import datetime
 import StringIO
 
-from bluebottle.analytics.models import get_report_model
+from django.db.models.aggregates import Count, Sum
+
+from bluebottle.analytics.models import ProjectRawReport, TaskRawReport, TaskMemberRawReport
 
 
 class MetricsReport(object):
@@ -16,115 +17,258 @@ class MetricsReport(object):
         'taskmember_hours': 3
     }
 
+    formats = {}
+
     def __init__(self):
         pass
 
-    def get_file_name(self):
-        now = pendulum.now()
-        file_name = 'metrics_report_generated_{}_{}.xlsx'.format(
-            now.to_date_string(),
-            now.int_timestamp
-        )
-        return file_name
-        # return os.path.join(tempfile.gettempdir(), file_name)
+    def get_project_data(self, period='year', year=None):
+        if period == 'year':
+            return ProjectRawReport.done_objects.values('year').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'quarter':
+            return ProjectRawReport.done_objects.values('year', 'quarter').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'month':
+            return ProjectRawReport.done_objects.values('year', 'quarter', 'month').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'week':
+            return ProjectRawReport.done_objects.values('year', 'quarter', 'month', 'week').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
 
-    def get_year_data(self, year):
-        ReportModel = get_report_model('v_year_totals_report')
-        return ReportModel.objects.filter(year=year).all()
+    def get_task_data(self, period='year', year=None):
+        if period == 'year':
+            return TaskRawReport.objects.values('year').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'quarter':
+            return TaskRawReport.objects.values('year', 'quarter').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'month':
+            return TaskRawReport.objects.values('year', 'quarter', 'month').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'week':
+            return TaskRawReport.objects.values('year', 'quarter', 'month', 'week').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
 
-    def get_quarter_data(self, year):
-        ReportModel = get_report_model('v_quarter_totals_report')
-        return ReportModel.objects.filter(year=year).order_by('quarter', 'type')
+    def get_taskmember_data(self, period='year', year=None):
+        if period == 'year':
+            return TaskMemberRawReport.objects.values('year').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'quarter':
+            return TaskMemberRawReport.objects.values('year', 'quarter').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'month':
+            return TaskMemberRawReport.objects.values('year', 'quarter', 'month').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
+        if period == 'week':
+            return TaskMemberRawReport.objects.values('year', 'quarter', 'month', 'week').\
+                filter(year=year).\
+                annotate(value=Count('type_id', distinct=True))
 
-    def get_month_data(self, year):
-        ReportModel = get_report_model('v_month_totals_report')
-        return ReportModel.objects.filter(year=year).order_by('month', 'type')
+    def get_taskmember_hours_data(self, period='year', year=None):
+        if period == 'year':
+            return TaskMemberRawReport.objects.values('year').\
+                filter(year=year).\
+                annotate(value=Sum('value'))
+        if period == 'quarter':
+            return TaskMemberRawReport.objects.values('year', 'quarter').\
+                filter(year=year).\
+                annotate(value=Sum('value'))
+        if period == 'month':
+            return TaskMemberRawReport.objects.values('year', 'quarter', 'month').\
+                filter(year=year).\
+                annotate(value=Sum('value'))
+        if period == 'week':
+            return TaskMemberRawReport.objects.values('year', 'quarter', 'month', 'week').\
+                filter(year=year).\
+                annotate(value=Sum('value'))
 
-    def to_output(self):
-        output = StringIO.StringIO()
-        workbook = xlsxwriter.Workbook(output, {
-            'in_memory': True,
-            'default_date_format': 'dd/mm/yy',
-            'remove_timezone': True
-        })
-        format_dark = workbook.add_format({
-            'bold': True,
-            'font_color': 'white',
-            'bg_color': '#555555',
-            'align': 'center'
-        })
-        format_grey = workbook.add_format({'bg_color': '#DDDDDD'})
-        border_bottom = workbook.add_format({'bottom': 1})
+    def add_year_totals(self, year):
+        border_bottom = self.formats['border']
+        format_dark = self.formats['dark']
+        format_grey = self.formats['light']
 
-        year = 2017
-        totals_worksheet = workbook.add_worksheet('Totals {}'.format(year))
-        # data = self.get_totals_data(year)
-        totals_worksheet.merge_range(0, 0, 0, 7, 'Period', format_dark)
-        totals_worksheet.write(1, 0, 'Duration', format_grey)
-        totals_worksheet.write(1, 1, 'Year', format_grey)
-        totals_worksheet.write(1, 2, 'Quarter', format_grey)
-        totals_worksheet.write(1, 3, 'Month', format_grey)
-        totals_worksheet.write(1, 4, 'Week', format_grey)
-        totals_worksheet.write(1, 5, 'Start date', format_grey)
-        totals_worksheet.write(1, 6, 'End date', format_grey)
+        ws = self.workbook.add_worksheet('Totals {}'.format(year))
+        ws.merge_range(0, 0, 0, 7, 'Period', format_dark)
+        ws.write(1, 0, 'Duration', format_grey)
+        ws.write(1, 1, 'Year', format_grey)
+        ws.write(1, 2, 'Quarter', format_grey)
+        ws.write(1, 3, 'Month', format_grey)
+        ws.write(1, 4, 'Week', format_grey)
+        ws.write(1, 5, 'Start date', format_grey)
+        ws.write(1, 6, 'End date', format_grey)
 
-        totals_worksheet.merge_range(0, 7, 0, 10, 'Totals', format_dark)
-        totals_worksheet.write(1, 7, 'Projects successful', format_grey)
-        totals_worksheet.write(1, 8, 'Activities successful', format_grey)
-        totals_worksheet.write(1, 9, 'Members volunteered', format_grey)
-        totals_worksheet.write(1, 10, 'Hours volunteered', format_grey)
+        ws.merge_range(0, 7, 0, 10, 'Totals', format_dark)
+        ws.write(1, 7, 'Projects successful', format_grey)
+        ws.write(1, 8, 'Activities successful', format_grey)
+        ws.write(1, 9, 'Members volunteered', format_grey)
+        ws.write(1, 10, 'Hours volunteered', format_grey)
 
         # Rows for year
         start = datetime.date(year, 1, 1)
         end = datetime.date(year, 12, 31)
-        totals_worksheet.write(2, 0, 'Year', border_bottom)
-        totals_worksheet.write(2, 1, year, border_bottom)
-        totals_worksheet.write(2, 2, '', border_bottom)
-        totals_worksheet.write(2, 3, '', border_bottom)
-        totals_worksheet.write(2, 4, '', border_bottom)
-        totals_worksheet.write(2, 5, start, border_bottom)
-        totals_worksheet.write(2, 6, end, border_bottom)
+        ws.write(2, 0, 'Year', border_bottom)
+        ws.write(2, 1, year, border_bottom)
+        ws.write(2, 2, '', border_bottom)
+        ws.write(2, 3, '', border_bottom)
+        ws.write(2, 4, '', border_bottom)
+        ws.write_datetime(2, 5, date=start)
+        ws.write_datetime(2, 6, date=end)
 
-        year_data = self.get_year_data(year)
-        for val in year_data:
-            i = self.row_lookup[val.type] + 7
-            totals_worksheet.write(2, int(i), val.value, border_bottom)
+        # Values for year
+        year_data = self.get_project_data('year', year)
+        if len(year_data):
+            ws.write(2, 7, year_data[0]['value'])
+        else:
+            pass
+        year_data = self.get_task_data('year', year)
+        if len(year_data):
+            ws.write(2, 8, year_data[0]['value'])
+        else:
+            pass
+        year_data = self.get_taskmember_data('year', year)
+        if len(year_data):
+            ws.write(2, 9, year_data[0]['value'])
+        else:
+            pass
+        year_data = self.get_taskmember_hours_data('year', year)
+        if len(year_data):
+            ws.write(2, 10, year_data[0]['value'])
+        else:
+            pass
 
         # Rows for quarter
         for q in range(1, 5):
             start = datetime.date(year, q * 3 - 2, 1)
             end = datetime.date(year, q * 3, calendar.monthrange(year, q * 3)[1])
-            totals_worksheet.write(2 + q, 0, 'Quarter')
-            totals_worksheet.write(2 + q, 1, year)
-            totals_worksheet.write(2 + q, 2, q)
-            totals_worksheet.write(2 + q, 3, '')
-            totals_worksheet.write(2 + q, 4, '')
-            totals_worksheet.write(2 + q, 5, start)
-            totals_worksheet.write(2 + q, 6, end)
+            cell_format = None
+            if q == 4:
+                cell_format = border_bottom
+            ws.write(2 + q, 0, 'Quarter', cell_format)
+            ws.write(2 + q, 1, year, cell_format)
+            ws.write(2 + q, 2, q, cell_format)
+            ws.write(2 + q, 3, '', cell_format)
+            ws.write(2 + q, 4, '', cell_format)
+            ws.write_datetime(2 + q, 5, start)
+            ws.write_datetime(2 + q, 6, end)
 
-        quarter_data = self.get_quarter_data(year)
+        quarter_data = self.get_project_data('quarter', year)
         for val in quarter_data:
-            i = self.row_lookup[val.type] + 7
-            j = val.quarter + 2
-            totals_worksheet.write(int(j), int(i), val.value)
+            i = 7
+            j = val['quarter'] + 2
+            cell_format = None
+            if val['quarter'] == 4:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
+
+        quarter_data = self.get_task_data('quarter', year)
+        for val in quarter_data:
+            i = 8
+            j = val['quarter'] + 2
+            cell_format = None
+            if val['quarter'] == 4:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
+
+        quarter_data = self.get_taskmember_data('quarter', year)
+        for val in quarter_data:
+            i = 9
+            j = val['quarter'] + 2
+            cell_format = None
+            if val['quarter'] == 4:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
+
+        quarter_data = self.get_taskmember_hours_data('quarter', year)
+        for val in quarter_data:
+            i = 10
+            j = val['quarter'] + 2
+            cell_format = None
+            if val['quarter'] == 4:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
 
         # Rows for months
         for m in range(1, 13):
             start = datetime.date(year, m, 1)
             end = datetime.date(year, m, calendar.monthrange(year, m)[1])
-            totals_worksheet.write(6 + m, 0, 'Month')
-            totals_worksheet.write(6 + m, 1, year)
-            totals_worksheet.write(6 + m, 2, '')
-            totals_worksheet.write(6 + m, 3, start.strftime('%B'))
-            totals_worksheet.write(6 + m, 4, '')
-            totals_worksheet.write(6 + m, 5, start)
-            totals_worksheet.write(6 + m, 6, end)
+            cell_format = None
+            if m == 12:
+                cell_format = border_bottom
+            ws.write(6 + m, 0, 'Month', cell_format)
+            ws.write(6 + m, 1, year, cell_format)
+            ws.write(6 + m, 2, '', cell_format)
+            ws.write(6 + m, 3, start.strftime('%B'), cell_format)
+            ws.write(6 + m, 4, '', cell_format)
+            ws.write(6 + m, 5, start)
+            ws.write_datetime(6 + m, 6, end)
 
-        month_data = self.get_month_data(year)
+        month_data = self.get_project_data('month', year)
         for val in month_data:
-            i = self.row_lookup[val.type] + 7
-            j = val.month + 6
-            totals_worksheet.write(int(j), int(i), val.value)
+            i = 7
+            j = val['month'] + 6
+            cell_format = None
+            if val['month'] == 12:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
 
-        workbook.close()
+        month_data = self.get_task_data('month', year)
+        for val in month_data:
+            i = 8
+            j = val['month'] + 6
+            cell_format = None
+            if val['month'] == 12:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
+
+        month_data = self.get_taskmember_data('month', year)
+        for val in month_data:
+            i = 9
+            j = val['month'] + 6
+            cell_format = None
+            if val['month'] == 12:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
+
+        month_data = self.get_taskmember_hours_data('month', year)
+        for val in month_data:
+            i = 10
+            j = val['month'] + 6
+            cell_format = None
+            if val['month'] == 12:
+                cell_format = border_bottom
+            ws.write(int(j), int(i), val['value'], cell_format)
+
+    def define_styles(self):
+        self.formats['dark'] = self.workbook.add_format({
+            'bold': True,
+            'font_color': 'white',
+            'bg_color': '#555555',
+            'align': 'center'
+        })
+        self.formats['light'] = self.workbook.add_format({'bg_color': '#DDDDDD'})
+        self.formats['border'] = self.workbook.add_format({'bottom': 1})
+
+    def to_output(self):
+        output = StringIO.StringIO()
+        self.workbook = xlsxwriter.Workbook(output, {
+            'in_memory': True,
+            'default_date_format': 'dd/mm/yy',
+            'remove_timezone': True
+        })
+        self.define_styles()
+        for year in [2017, 2016, 2015]:
+            self.add_year_totals(year)
+        self.workbook.close()
         return output

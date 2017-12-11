bdbc862df0b4f0b1c31e1baf0a796756b1ddc78d
Loek van Gent <loek@onepercentclub.com>
2017-12-11 14:24:33 +0100

    Clean up

--- bluebottle/mails/models.py
+++ bluebottle/mails/models.py
@@ -1,4 +1,4 @@
-from django.db import models, connection
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from bluebottle.utils.models import BasePlatformSettings
@@ -6,16 +6,6 @@ from bluebottle.utils.models import BasePlatformSettings
 
 class MailPlatformSettings(BasePlatformSettings):
     email_logo = models.ImageField(null=True, blank=True, upload_to='site_content/')
-
-    @property
-    def email_logo_url(self):
-        if not self.email_logo:
-            return ''
-        if 'localhost' in connection.tenant.domain_url:
-            logo_url = 'http://' + connection.tenant.domain_url + ':4200' + self.email_logo.url
-        else:
-            logo_url = 'https://' + connection.tenant.domain_url + self.email_logo.url
-        return logo_url
 
     class Meta:
         verbose_name_plural = _('mail platform settings')

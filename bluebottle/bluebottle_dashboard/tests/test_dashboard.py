3b7e4c778891ed77d4ddbd662fd1c68ee2638da8
Loek van Gent <loek@onepercentclub.com>
2018-02-10 13:01:25 +0100

    More tests

--- bluebottle/bluebottle_dashboard/tests/test_dashboard.py
+++ bluebottle/bluebottle_dashboard/tests/test_dashboard.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+from bluebottle.bluebottle_dashboard.dashboard import CustomAppIndexDashboard
+from bluebottle.projects.dashboard import AppIndexDashboard as ProjectAppIndexDashboard
+from django.test.client import RequestFactory
+from jet.dashboard.dashboard import DefaultAppIndexDashboard
 from tenant_schemas.urlresolvers import reverse
 
 from bluebottle.test.utils import BluebottleAdminTestCase
@@ -22,3 +26,24 @@ class MainDashboardTest(BluebottleAdminTestCase):
         self.assertContains(response, 'Recently joined users')
         self.assertContains(response, 'Tasks nearing deadline')
         self.assertContains(response, 'Export metrics')
+
+
+class CustomAppDashboardTest(BluebottleAdminTestCase):
+    """
+    Test custom app dashboard loading
+    """
+
+    def setUp(self):
+        super(CustomAppDashboardTest, self).setUp()
+        self.client.force_login(self.superuser)
+        self.admin_url = reverse('admin:index')
+        self.request = RequestFactory().get(self.admin_url)
+        self.request.user = self.superuser
+
+    def test_existing_app_dashboard(self):
+        dash = CustomAppIndexDashboard({'request': self.request}, app_label='projects')
+        self.assertTrue(isinstance(dash, ProjectAppIndexDashboard))
+
+    def test_non_existing_app_dashboard(self):
+        dash = CustomAppIndexDashboard({'request': self.request}, app_label='orders')
+        self.assertTrue(isinstance(dash, DefaultAppIndexDashboard))

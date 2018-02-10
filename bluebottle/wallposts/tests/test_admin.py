e853d41a9fcc623074775713b45bc9ce694bcfc4
Loek van Gent <loek@onepercentclub.com>
2018-02-10 22:26:06 +0100

    Set admin url explicitely

--- bluebottle/wallposts/tests/test_admin.py
+++ bluebottle/wallposts/tests/test_admin.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from django.urls.base import reverse
-
 from bluebottle.test.factory_models.wallposts import MediaWallpostFactory, MediaWallpostPhotoFactory
 from bluebottle.test.utils import BluebottleAdminTestCase
 
@@ -11,8 +9,10 @@ class TestWallpostAdmin(BluebottleAdminTestCase):
         super(TestWallpostAdmin, self).setUp()
         wallpost = MediaWallpostFactory.create()
         MediaWallpostPhotoFactory.create_batch(10, mediawallpost=wallpost)
+        wallpost.save()
         self.client.force_login(self.superuser)
-        self.media_wallpost_url = reverse('admin:wallposts_mediawallpost_changelist')
+        # Don't user reverse here, because polymorphic sometimes makes a mistake.
+        self.media_wallpost_url = '/en/admin/wallposts/mediawallpost/'
 
     def test_mediawallpost_admin(self):
         response = self.client.get(self.media_wallpost_url)

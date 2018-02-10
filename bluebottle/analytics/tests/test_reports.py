8f69e75c0cecfa4fd989fcf27efde34f2e210fcd
Loek van Gent <loek@onepercentclub.com>
2018-02-10 18:04:20 +0100

    Generate some projects for future test

--- bluebottle/analytics/tests/test_reports.py
+++ bluebottle/analytics/tests/test_reports.py
@@ -1,5 +1,7 @@
 from bluebottle.analytics.reports import MetricsReport
+from bluebottle.bb_projects.models import ProjectPhase
 from bluebottle.test.factory_models.projects import ProjectFactory
+from bluebottle.test.factory_models.tasks import TaskFactory
 from bluebottle.test.utils import BluebottleTestCase
 from django.utils.timezone import now
 from openpyxl.reader.excel import load_workbook
@@ -8,7 +10,13 @@ from openpyxl.reader.excel import load_workbook
 class TestReportExport(BluebottleTestCase):
 
     def test_report_export(self):
-        ProjectFactory.create_batch(12)
+        campaign = ProjectPhase.objects.get(slug='done-complete')
+        done = ProjectPhase.objects.get(slug='done-complete')
+        projects = ProjectFactory.create_batch(10, status=campaign)
+        TaskFactory.create_batch(12, project=projects[0])
+        for project in projects:
+            project.status = done
+            project.save()
         report = MetricsReport()
         result = report.to_output()
         wb = load_workbook(result)

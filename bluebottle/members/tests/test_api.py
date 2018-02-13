e860bc06ddbe11e0a7cf381fcd334d673ba9a469
Ernst Odolphi <ernst@onepercentclub.com>
2018-02-13 17:04:08 +0100

    Add link with more information about consent

--- bluebottle/members/tests/test_api.py
+++ bluebottle/members/tests/test_api.py
@@ -11,11 +11,20 @@ class ProjectPlatformSettingsTestCase(BluebottleTestCase):
     def test_member_platform_settings(self):
         MemberPlatformSettings.objects.create(
             require_consent=True,
+            consent_link='http://example.com'
         )
 
         response = self.client.get(reverse('settings'))
         self.assertEqual(response.data['platform']['members']['require_consent'], True)
+        self.assertEqual(
+            response.data['platform']['members']['consent_link'],
+            'http://example.com'
+        )
 
     def test_member_platform_settings_default(self):
         response = self.client.get(reverse('settings'))
         self.assertEqual(response.data['platform']['members']['require_consent'], False)
+        self.assertEqual(
+            response.data['platform']['members']['consent_link'],
+            '/pages/terms-and-conditions'
+        )

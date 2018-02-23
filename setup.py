27939fd98fa5688e5df8e31c37073020a3cf3458
requires.io <support@requires.io>
2018-02-23 01:15:10 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ install_requires = [
     'micawber==0.3.5',
     'mixpanel==4.3.2',
     'openpyxl==2.5.0',
-    'pendulum==1.4.1',
+    'pendulum==1.4.2',
     'psycopg2==2.7.4',
     'pygeoip==0.3.2',
     'pyjwt==1.5.3',

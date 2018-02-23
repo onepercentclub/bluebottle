6af91df5ca64a4c352fd3265ee73dbc85dd3175a
Loek van Gent <loek@onepercentclub.com>
2018-02-23 12:00:33 +0100

    More code clean up  and add some tests

---
+++ bluebottle/slides/tests/test_admin.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from bluebottle.test.factory_models.slides import SlideFactory
+from tenant_schemas.urlresolvers import reverse
+
+from bluebottle.test.utils import BluebottleAdminTestCase
+
+
+class NewsAdminTest(BluebottleAdminTestCase):
+    def setUp(self):
+        super(NewsAdminTest, self).setUp()
+        self.news = SlideFactory()
+        self.news_url = reverse('admin:slides_slide_changelist')
+        self.client.force_login(self.superuser)
+
+    def test_form(self):
+        response = self.client.get(self.news_url)
+        self.assertIn('Add slide', response.content)

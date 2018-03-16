6cb0b85b9d4b0cd387f0640c999937c1f3cf99e8
Ernst Odolphi <ernst@onepercentclub.com>
2018-03-16 15:25:40 +0100

    Remove filter that was only there for debugging

--- bluebottle/projects/management/commands/reverse_geocode_projects.py
+++ bluebottle/projects/management/commands/reverse_geocode_projects.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     help = 'Set project location based on geo location'
 
     def handle(self, *args, **options):
-        for client in Client.objects.all().filter(client_name='onepercent'):
+        for client in Client.objects.all():
             connection.set_tenant(client)
             with LocalTenant(client, clear_tenant=True):
                 for project in Project.objects.filter(
@@ -19,6 +19,7 @@ class Command(BaseCommand):
                     longitude__isnull=False,
                     projectlocation__isnull=True
                 ):
+                    print project.title, project.created
                     ProjectLocation.objects.create(
                         project=project,
                         latitude=project.latitude,

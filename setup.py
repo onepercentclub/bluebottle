21cfae45bf4a41527311c9daaedc3469c674e1f3
requires.io <support@requires.io>
2018-03-21 17:25:45 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ install_requires = [
     'sorl-thumbnail==12.4.1',
     'sorl-watermark==1.0.0',
     'South==1.0.2',
-    'Sphinx==1.7.1',
+    'Sphinx==1.7.2',
     'suds-jurko==0.6',
     'SurveyGizmo==1.2.3',
     'transifex-client==0.13.1',

5ef50d1bb3285561624a52db4c5f7ab9a6e2b2e3
Loek van Gent <loek@onepercentclub.com>
2018-03-26 08:22:22 +0200

    Add document field to content pages.

--- bluebottle/pages/content_plugins.py
+++ bluebottle/pages/content_plugins.py
@@ -1,4 +1,4 @@
-from bluebottle.pages.models import ImageTextItem
+from bluebottle.pages.models import ImageTextItem, DocumentItem
 
 from fluent_contents.extensions import plugin_pool, ContentPlugin
 
@@ -7,3 +7,9 @@ from fluent_contents.extensions import plugin_pool, ContentPlugin
 class ImageTextPlugin(ContentPlugin):
     model = ImageTextItem
     render_template = "pages/plugins/imagetext/default.html"
+
+
+@plugin_pool.register
+class DocumentPlugin(ContentPlugin):
+    model = DocumentItem
+    render_template = "pages/plugins/document/default.html"

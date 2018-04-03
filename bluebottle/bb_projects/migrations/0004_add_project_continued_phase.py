6bb8d497ef78a9ec6c6f775fe86c33347aad9f9b
Ernst Odolphi <ernst@onepercentclub.com>
2018-04-03 14:36:33 +0200

    Fix unused migration and update geo fixture

--- bluebottle/bb_projects/migrations/0004_add_project_continued_phase.py
+++ bluebottle/bb_projects/migrations/0004_add_project_continued_phase.py
@@ -5,19 +5,6 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
-def add_project_continued(apps, schema_editor):
-    ProjectPhase = apps.get_model('bb_projects', 'ProjectPhase')
-    ProjectPhase.objects.get_or_create(
-        id=7,
-        viewable=True,
-        slug="to-be-continued",
-        name="Project - To be continued",
-        sequence=7,
-        editable=False,
-        active=True,
-        description=""
-    )
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -25,5 +12,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(add_project_continued)
     ]

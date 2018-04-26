37f92bdabb62a886eef3e5f7b62584e6a252aed8
Loek van Gent <loek@onepercentclub.com>
2018-04-26 08:44:22 +0200

    Keep setting tenant on db connection

--- bluebottle/donations/management/commands/export_donations.py
+++ bluebottle/donations/management/commands/export_donations.py
@@ -1,10 +1,10 @@
 import json
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from django.db import connection
 
 from bluebottle.orders.models import Order
 from bluebottle.clients.models import Client
-
 from bluebottle.clients.utils import LocalTenant
 
 
@@ -19,6 +19,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         results = []
         for client in Client.objects.all():
+            connection.set_tenant(client)
             with LocalTenant(client, clear_tenant=True):
                 ContentType.objects.clear_cache()
                 orders = Order.objects.filter(

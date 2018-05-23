6664d075b4037ae40a91267afaca5731aa73ed3c
Ernst Odolphi <ernst@onepercentclub.com>
2018-05-23 16:43:38 +0200

    Fix url fields when no value is set

--- bluebottle/utils/widgets.py
+++ bluebottle/utils/widgets.py
@@ -33,7 +33,7 @@ class CheckboxSelectMultipleWidget(CheckboxSelectMultiple):
 
 class SecureAdminURLFieldWidget(AdminURLFieldWidget):
     def render(self, name, value, attrs=None):
-        if urlparse(value).scheme not in ('http', 'https', ):
+        if value and urlparse(value).scheme not in ('http', 'https', ):
             return super(AdminURLFieldWidget, self).render(name, value, attrs)
         else:
             return super(SecureAdminURLFieldWidget, self).render(name, value, attrs)

719f4f799a257751efee51986aa661499d7dc729
Ernst Odolphi <ernst@onepercentclub.com>
2018-07-11 11:27:47 +0200

    Add a link to the terms and services page in terms objects
    BB-12758 #resolve

--- bluebottle/terms/serializers.py
+++ bluebottle/terms/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
+
+from bluebottle.pages.models import Page
 from bluebottle.terms.models import Terms, TermsAgreement
 
 
 class TermsSerializer(serializers.ModelSerializer):
+    page = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Page.objects.all()
+    )
+
     class Meta:
         model = Terms
-        fields = ('id', 'date', 'version', 'contents')
+        fields = ('id', 'date', 'version', 'page', 'contents')
 
 
 class TermsAgreementSerializer(serializers.ModelSerializer):
-    # FIXME: returning a primary key should have the format xxx_id for the field name
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     terms = serializers.PrimaryKeyRelatedField(read_only=True)
 

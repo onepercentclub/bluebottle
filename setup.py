f4ecd5cd43ca296f265846828580c80d0b2c28a6
Loek van Gent <loek@onepercentclub.com>
2018-09-12 10:02:01 +0200

    Up version for django-token-auth

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependency_links = [
     'git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.14#egg=django-tenant-extras-2.0.14',
     'git+https://github.com/onepercentclub/django-fluent-contents.git@741ffae615a4afed01388a202709ed9a5b60e80f#egg=django-fluent-contents-1.2.1-741ffae6',
     'git+https://github.com/mariocesar/sorl-thumbnail.git@v12.3#egg=sorl-thumbnail-12.3-github',
-    'git+https://github.com/onepercentclub/django-token-auth.git@54936ee2894ac58863c65a6e491bc2129e6d68cb#egg=django-token-auth-0.3.7',
+    'git+https://github.com/onepercentclub/django-token-auth.git@4e1e42b38ae60522a4dd6902db4c0f80558ba4e3#egg=django-token-auth-0.3.8',
     'git+https://github.com/st4lk/django-select-multiple-field.git@1dc7733008150a111cd141ff7c3f42bf4953dc7d#egg=django-select-multiple-field-0.5.0a-draft',
     'hg+https://bitbucket.org/jdiascarvalho/django-filetransfers@89c8381764da217d72f1fa396ce3929f0762b8f9#egg=django-filetransfers-0.1.1',
     'git+https://github.com/skada/django-money-rates@aeb2edf240471fac64f9cdf71e34f91d632f1b86#egg=django-money-rates-0.3.1-github',
@@ -116,7 +116,7 @@ install_requires = [
     'django-fluent-contents==1.2.1-741ffae6',
     'django-bb-salesforce==1.2.2',
     'django-taggit-autocomplete-modified==0.1.1b1',
-    'django-token-auth==0.3.7',
+    'django-token-auth==0.3.8',
     'django-money-rates==0.3.1-github'
 ]
 

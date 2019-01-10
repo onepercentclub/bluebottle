2260bebb8027e6e13314bd985fa5194b54938d7c
Loek van Gent <loek@onepercentclub.com>
2019-01-10 11:39:56 +0100

    Add test for plain payout account permissions

--- bluebottle/payouts/tests/test_admin.py
+++ bluebottle/payouts/tests/test_admin.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import Permission, Group
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden
 from django.test.utils import override_settings
 
+from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
+from bluebottle.test.factory_models.payouts import PlainPayoutAccountFactory
 from bluebottle.test.utils import BluebottleAdminTestCase
 
 from ..admin import ProjectPayoutAdmin
@@ -26,3 +30,32 @@ class PayoutTestAdmin(BluebottleAdminTestCase):
         response = self.app.get(payout_url, user=self.superuser)
         self.failUnless('5%' in response.body)
         self.failUnless('7.25%' in response.body)
+
+
+class PayoutAccountAdminTestCase(BluebottleAdminTestCase):
+
+    def setUp(self):
+        self.user = BlueBottleUserFactory.create(is_staff=True)
+        account = PlainPayoutAccountFactory.create()
+        self.payout_url = reverse('admin:payouts_payoutaccount_change', args=(account.id,))
+
+    def test_permissions_denied(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.payout_url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_permissions_granted_user(self):
+        # Check user has permission when added specific permission
+        self.user.user_permissions.add(
+            Permission.objects.get(codename='change_plainpayoutaccount')
+        )
+        self.client.force_login(self.user)
+        response = self.client.get(self.payout_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_permissions_granted_staff(self):
+        # Check that user has permission if added to Staff group
+        self.user.groups.add(Group.objects.get(name='Staff'))
+        self.client.force_login(self.user)
+        response = self.client.get(self.payout_url)
+        self.assertEqual(response.status_code, 200)
--- bluebottle/test/factory_models/accounts.py
+++ bluebottle/test/factory_models/accounts.py
@@ -3,12 +3,12 @@ import factory
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 
-BB_USER_MODEL = get_user_model()
+from bluebottle.members.models import Member
 
 
 class BlueBottleUserFactory(factory.DjangoModelFactory):
     class Meta(object):
-        model = BB_USER_MODEL
+        model = Member
 
     username = factory.Sequence(lambda n: u'user_{0}'.format(n))
     email = factory.Sequence(lambda o: u'user_{0}@onepercentclub.com'.format(o))
@@ -16,6 +16,7 @@ class BlueBottleUserFactory(factory.DjangoModelFactory):
     first_name = factory.Sequence(lambda f: u'user_{0}'.format(f))
     last_name = factory.Sequence(lambda l: u'user_{0}'.format(l))
     is_active = True
+    is_staff = False
     is_superuser = False
 
 

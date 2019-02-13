f3a78e89f2ec8949dfd03e0c243ddbdf09b4c608
Ernst Odolphi <ernst@onepercentclub.com>
2019-02-13 13:12:54 +0100

    Fix merge

--- setup.py
+++ setup.py
@@ -109,21 +109,12 @@ install_requires = [
     'xlsxwriter==0.9.8',
 
     # Github requirements
-<<<<<<< HEAD
-    'django-tenant-extras==2.0.14',
-    'django-fluent-contents==1.2.1-741ffae6',
-    'django-bb-salesforce==1.2.2',
-    'django-taggit-autocomplete-modified==0.1.1b1',
-    'django-money-rates==0.3.1-github',
-    'Flutterwave==1.1.0'
-=======
     'django-tenant-extras @ git+https://github.com/onepercentclub/django-tenant-extras.git@2.0.14#egg=django-tenant-extras-2.0.14',
     'django-fluent-contents @ git+https://github.com/onepercentclub/django-fluent-contents.git@741ffae615a4afed01388a202709ed9a5b60e80f#egg=django-fluent-contents-1.2.1-741ffae6',
     'django-bb-salesforce @ git+https://github.com/onepercentclub/django-bb-salesforce.git@1.2.2#egg=django-bb-salesforce-1.2.2',
     'django-taggit-autocomplete-modified @ git+https://github.com/onepercentclub/django-taggit-autocomplete-modified.git@8e7fbc2deae2f1fbb31b574bc8819d9ae7c644d6#egg=django-taggit-autocomplete-modified-0.1.1b1',
     'django-money-rates @ git+https://github.com/skada/django-money-rates@aeb2edf240471fac64f9cdf71e34f91d632f1b86#egg=django-money-rates-0.3.1-github',
     'Flutterwave @ git+https://github.com/onepercentclub/flutterwave-python.git@9b6adba8e7eff4204d36ea3a627ebccebe285cf4#egg=Flutterwave-1.1.0'
->>>>>>> master
 ]
 
 tests_requires = [

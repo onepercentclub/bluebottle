668adab766178c8381d402155bd2464d663733b0
Loek van Gent <loek@goodup.com>
2019-03-04 09:04:25 +0100

    More donation export tests

--- bluebottle/donations/tests/test_exports.py
+++ bluebottle/donations/tests/test_exports.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from django.core.management import call_command
@@ -15,17 +16,30 @@ class TestDonationExport(BluebottleTestCase, SessionTestMixin):
         super(TestDonationExport, self).setUp()
         self.project = ProjectFactory.create(amount_asked=5000)
         self.user = BlueBottleUserFactory.create()
-        self.orders = OrderFactory.create_batch(7)
-        for order in self.orders:
+        created = datetime.datetime(2019, 1, 13, 23, 25, 0)
+        orders = OrderFactory.create_batch(7, created=created)
+        for order in orders:
             DonationFactory.create(project=self.project, order=order)
             order_payment = OrderPaymentFactory(order=order)
             payment = PaymentFactory(order_payment=order_payment)
             payment.status = 'settled'
             payment.save()
+        created = datetime.datetime(2019, 1, 1, 3, 25, 0)
+        orders = OrderFactory.create_batch(3, created=created)
+        for order in orders:
+            DonationFactory.create(project=self.project, order=order)
+            order_payment = OrderPaymentFactory(order=order)
+            payment = PaymentFactory(order_payment=order_payment)
+            payment.status = 'unknown'
+            payment.save()
 
     def test_export(self):
-        call_command('export_donations', '--file', 'temp.json')
+
+        call_command('export_donations',
+                     '--start', '2019-01-01',
+                     '--end', '2019-01-31',
+                     '--file', 'temp.json')
         with open('temp.json') as json_data:
             data = json.load(json_data)
-            self.assertEqual(len(data), 7)
+            self.assertEqual(len(data), 10)
             self.assertEqual(data[0]['donations'][0]['project_id'], self.project.id)

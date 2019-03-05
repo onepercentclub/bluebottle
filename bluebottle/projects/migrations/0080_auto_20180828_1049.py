8ebdc5045179c967c7d12b410f6620f7a1d43657
Ernst Odolphi <ernst@onepercentclub.com>
2019-03-05 09:50:37 +0100

    Do not remove popularity field from db for now.
    This will break the old code running against the migrated db, resulting
    in downtime during the release.
    
    We should remove this field when the changes are deployed.

--- bluebottle/projects/migrations/0080_auto_20180828_1049.py
+++ bluebottle/projects/migrations/0080_auto_20180828_1049.py
@@ -14,10 +14,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='project',
-            name='popularity',
-        ),
         migrations.AlterField(
             model_name='project',
             name='location',

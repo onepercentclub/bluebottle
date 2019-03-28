0fa526443fe4bc1328294481812a585c3310a739
Ernst Odolphi <ernst@onepercentclub.com>
2019-03-28 13:52:04 +0100

    Do not add a slash to the url. Ember does not like slashes after the id, and we (incorrectly) use 'homepage' as the id.

--- bluebottle/cms/urls/api.py
+++ bluebottle/cms/urls/api.py
@@ -6,7 +6,7 @@ from bluebottle.cms.views import (
 
 urlpatterns = [
     url(r'^results/(?P<pk>\d+)$', ResultPageDetail.as_view(), name='result-page-detail'),
-    url(r'^homepage/$', HomePageDetail.as_view(), {'pk': 1}, name='home-page-detail'),
+    url(r'^homepage$', HomePageDetail.as_view(), {'pk': 1}, name='home-page-detail'),
     url(r'^news-item/(?P<slug>[\w-]+)$', NewsItemDetail.as_view(), name='news-item-detail'),
     url(r'^page/(?P<slug>[\w-]+)$', PageDetail.as_view(), name='page-detail'),
 ]

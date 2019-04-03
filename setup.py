f7e2aee5efa7847c7ed72c0698418f118e3779f9
Enrique Salazar <enrique.salazar@gmail.com>
2019-04-03 15:56:58 +0200

    add dependcy

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ install_requires = [
     'django-uuidfield==0.5.0',
     'django-wysiwyg==0.8.0',
     'djangorestframework-jwt==1.11.0',
+    'djangorestframework-jsonapi==2.7.0',
     'djangorestframework==3.8.2',
     'dkimpy==0.6.1',
     'elasticsearch==6.3.0',

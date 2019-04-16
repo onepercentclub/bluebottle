1a2b7da80893dfdb678db88efe35bcf4c4867837
Loek van Gent <loek@goodup.com>
2019-04-16 11:30:51 +0200

    Be more strict in listening to signals

--- bluebottle/notifications/signals.py
+++ bluebottle/notifications/signals.py
@@ -3,8 +3,8 @@ from django_fsm import post_transition
 
 
 @receiver(post_transition)
-def transition_messages(sender, instance, name, source, target, **kwargs):
-
-    if getattr(instance, 'send_messages', True):
-        for message in getattr(instance, name).messages:
+def transition_messages(sender, instance, **kwargs):
+    # Only try to send messages if we have a 'name' and 'send_messages' is not False.
+    if hasattr(kwargs, 'name') and getattr(instance, 'send_messages', True):
+        for message in getattr(instance, kwargs['name']).messages:
             message(instance).compose_and_send()

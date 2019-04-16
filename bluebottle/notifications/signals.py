bf792ee99c93e30f308dc815aef3e26f083ccd63
Loek van Gent <loek@goodup.com>
2019-04-16 16:09:30 +0200

    More fixing of tests

--- bluebottle/notifications/signals.py
+++ bluebottle/notifications/signals.py
@@ -7,5 +7,6 @@ def transition_messages(sender, instance, **kwargs):
 
     # Only try to send messages if we have a 'name' and 'send_messages' is not False.
     if 'name' in kwargs and getattr(instance, 'send_messages', True):
-        for message in getattr(instance, kwargs['name']).messages:
+        transition = getattr(instance, kwargs['name'])
+        for message in getattr(transition, 'messages', []):
             message(instance).compose_and_send()

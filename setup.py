459005df6bd080a2b124a18a9b4c9f0ca4930b32
Ernst Odolphi <ernst@onepercentclub.com>
2019-05-01 17:27:54 +0200

    Add scent.py file for easy test watching

--- setup.py
+++ setup.py
@@ -136,7 +136,8 @@ tests_requires = [
     'pylint-django==0.7.2',
     'tblib==1.3.2',
     'tdaemon==0.1.1',
-    'WebTest==2.0.27'
+    'WebTest==2.0.27',
+    'sniffer==0.4.0'
 ]
 
 dev_requires = [

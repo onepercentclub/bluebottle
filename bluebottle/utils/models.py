9592551350ef8e6d2f76bb98038c3028f39eee91
Loek van Gent <loek@goodup.com>
2019-05-03 16:29:58 +0200

    Re-apply monkey patches

--- bluebottle/utils/models.py
+++ bluebottle/utils/models.py
@@ -10,6 +10,12 @@ from parler.models import TranslatableModel
 
 from bluebottle.utils.managers import SortableTranslatableManager, PublishedManager
 
+import bluebottle.utils.monkey_patch_migration  # noqa
+import bluebottle.utils.monkey_patch_corsheaders  # noqa
+import bluebottle.utils.monkey_patch_parler  # noqa
+import bluebottle.utils.monkey_patch_money_readonly_fields  # noqa
+import bluebottle.utils.monkey_patch_django_elasticsearch_dsl  # noqa
+
 
 class Language(models.Model):
     """

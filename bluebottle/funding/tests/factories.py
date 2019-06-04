3d3a133c6bc6421f2a500ef7e780cefd8f771f83
Ernst Odolphi <ernst@onepercentclub.com>
2019-06-04 16:50:58 +0200

    Start of funding api.
    Fix API for donations and funding activities
    
    Add API for stripe and pledge paymments.
    Start of tests.
    
    TODO: Permission checks, refunding, admin, webhooks, other payments
    methods, test for funding activity and donations API and transitions,
    update amount in intent if donation amount changes.

--- bluebottle/funding/tests/factories.py
+++ bluebottle/funding/tests/factories.py
@@ -1,12 +1,12 @@
 import factory.fuzzy
 
-from bluebottle.funding.models import Funding
+from bluebottle.funding.models import Funding, Donation
 from bluebottle.initiatives.tests.factories import InitiativeFactory
 from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
+from bluebottle.test.factory_models.payouts import StripePayoutAccountFactory
 
 
 class FundingFactory(factory.DjangoModelFactory):
-
     class Meta(object):
         model = Funding
 
@@ -15,3 +15,13 @@ class FundingFactory(factory.DjangoModelFactory):
 
     owner = factory.SubFactory(BlueBottleUserFactory)
     initiative = factory.SubFactory(InitiativeFactory)
+    account = factory.SubFactory(StripePayoutAccountFactory)
+    target = 100
+
+
+class DonationFactory(factory.DjangoModelFactory):
+    class Meta(object):
+        model = Donation
+
+    activity = factory.SubFactory(FundingFactory)
+    user = factory.SubFactory(BlueBottleUserFactory)

cee22585c7f00a72693159145bf406d03d9cf600
Enrique Salazar <enrique.salazar@gmail.com>
2019-09-05 14:03:47 +0200

    adds initiative.location and initiative.place to activities serializer

--- bluebottle/activities/serializers.py
+++ bluebottle/activities/serializers.py
@@ -20,7 +20,9 @@ class ActivitySerializer(PolymorphicModelSerializer):
         'owner': 'bluebottle.initiatives.serializers.MemberSerializer',
         'initiative': 'bluebottle.initiatives.serializers.InitiativeSerializer',
         'initiative.image': 'bluebottle.initiatives.serializers.InitiativeImageSerializer',
-        'location': 'bluebottle.geo.serializers.GeolocationSerializer',
+        'location': 'bluebottle.geo.serializers.GeolocationSerializer',    
+        'initiative.location': 'bluebottle.geo.serializers.LocationSerializer',
+        'initiative.place': 'bluebottle.geo.serializers.GeolocationSerializer',    
     }
 
     class Meta:
@@ -31,7 +33,9 @@ class ActivitySerializer(PolymorphicModelSerializer):
             'owner',
             'initiative',
             'location',
-            'initiative.image'
+            'initiative.image',
+            'initiative.place',
+            'initiative.location',
         ]
 
 

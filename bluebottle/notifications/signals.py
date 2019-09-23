673f9c49ad939e4e041bd532b21be944c658c8f6
Loek van Gent <loek@goodup.com>
2019-09-23 19:59:59 +0200

    Fix test

--- bluebottle/notifications/signals.py
+++ bluebottle/notifications/signals.py
@@ -4,8 +4,8 @@ from bluebottle.fsm import post_transition
 
 
 @receiver(post_transition)
-def transition_messages(sender, instance, transition, signal=None, send_messages=True, **options):
+def transition_messages(sender, instance, transition, send_messages=True, message=None, **kwargs):
     # Only try to send messages if 'send_messages' is not False.
     if send_messages:
         for msg in transition.options.get('messages', []):
-            msg(instance, **options).compose_and_send()
+            msg(instance, custom_message=message).compose_and_send()

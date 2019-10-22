b5fc47a73944645a5c6c48c1b5bd5b8b91736ef0
Loek van Gent <loek@goodup.com>
2019-10-22 13:50:28 +0200

    Improve pledge bank account api

--- bluebottle/funding_pledge/serializers.py
+++ bluebottle/funding_pledge/serializers.py
@@ -1,5 +1,8 @@
+from rest_framework_json_api.relations import ResourceRelatedField
+
 from bluebottle.funding.base_serializers import PaymentSerializer, BaseBankAccountSerializer
 from bluebottle.funding_pledge.models import PledgePayment, PledgeBankAccount
+from bluebottle.geo.models import Country
 
 
 class PledgePaymentSerializer(PaymentSerializer):
@@ -11,6 +14,8 @@ class PledgePaymentSerializer(PaymentSerializer):
 
 
 class PledgeBankAccountSerializer(BaseBankAccountSerializer):
+    account_holder_country = ResourceRelatedField(queryset=Country.objects)
+    account_bank_country = ResourceRelatedField(queryset=Country.objects)
 
     class Meta(BaseBankAccountSerializer.Meta):
         model = PledgeBankAccount
@@ -20,13 +25,16 @@ class PledgeBankAccountSerializer(BaseBankAccountSerializer):
             'account_holder_address',
             'account_holder_postal_code',
             'account_holder_city',
-            'account_holder_country',
             'account_number',
             'account_details',
-            'account_bank_country',
+
+            'account_holder_country',
+            'account_bank_country'
         )
 
     included_serializers = {
+        'account_holder_country': 'bluebottle.geo.serializers.InitiativeCountrySerializer',
+        'account_bank_country': 'bluebottle.geo.serializers.InitiativeCountrySerializer',
         'connect_account': 'bluebottle.funding.serializers.PlainPayoutAccountSerializer',
     }
 

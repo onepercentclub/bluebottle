4b6e2db3702b8014d92e589cb340855abe8d052f
Loek van Gent <loek@goodup.com>
2019-11-15 11:48:04 +0100

    Don;t change old url to new view

--- bluebottle/payments_vitepay/urls/core.py
+++ bluebottle/payments_vitepay/urls/core.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 
-from bluebottle.funding_vitepay.views import VitepayWebhookView
-
+from ..views import PaymentStatusListener
 
 urlpatterns = [
     url(r'^status_update/$',
-        VitepayWebhookView.as_view(),
+        PaymentStatusListener.as_view(),
         name='vitepay-status-update')
 ]

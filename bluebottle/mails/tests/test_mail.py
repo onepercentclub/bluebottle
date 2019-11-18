db87ab2fb0fd687c2f96b0cfb40145ccc87686f2
Loek van Gent <loek@goodup.com>
2019-11-18 14:26:08 +0100

    Fix email logo test

--- bluebottle/mails/tests/test_mail.py
+++ bluebottle/mails/tests/test_mail.py
@@ -3,11 +3,11 @@ from django.core.files.base import File
 from django.test.utils import override_settings
 
 from bluebottle.mails.models import MailPlatformSettings
-from bluebottle.test.factory_models.tasks import TaskMemberFactory
+from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
 from bluebottle.test.utils import BluebottleTestCase
 
 
-@override_settings(SEND_WELCOME_MAIL=False)
+@override_settings(SEND_WELCOME_MAIL=True)
 class TestMailLogo(BluebottleTestCase):
     """
     Test that email logo from backend ends up in emails
@@ -16,7 +16,7 @@ class TestMailLogo(BluebottleTestCase):
     def test_mail_works_without_logo(self):
         # Upload an email logo
 
-        TaskMemberFactory.create(status='applied')
+        BlueBottleUserFactory.create()
         email = mail.outbox[0]
         self.assertFalse('<img alt="logo"' in email.alternatives[0][0])
 
@@ -26,7 +26,6 @@ class TestMailLogo(BluebottleTestCase):
         mail_settings = MailPlatformSettings.load()
         mail_settings.email_logo.save("email-logo.gif", File(image_file), save=True)
         mail_settings.save()
-
-        TaskMemberFactory.create(status='applied')
+        BlueBottleUserFactory.create()
         email = mail.outbox[0]
         self.assertTrue('<img alt="logo"' in email.alternatives[0][0])

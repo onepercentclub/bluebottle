0dd6ecf45a1306c330eafc1554507ff009186538
Loek van Gent <loek@goodup.com>
2019-12-03 09:57:13 +0100

    Add bulk transition action to activities and contributions

--- bluebottle/funding_stripe/tests/test_transitions.py
+++ bluebottle/funding_stripe/tests/test_transitions.py
@@ -9,19 +9,28 @@ from moneyed import Money
 from bluebottle.funding_stripe.tests.factories import (
     StripePaymentFactory, StripePayoutAccountFactory, ExternalAccountFactory,
 )
-from bluebottle.funding.tests.factories import FundingFactory, DonationFactory
+from bluebottle.funding.tests.factories import FundingFactory, DonationFactory, BudgetLineFactory
+from bluebottle.initiatives.tests.factories import InitiativeFactory
 from bluebottle.test.utils import BluebottleTestCase
 
 
 class StripePaymentTransitionsTestCase(BluebottleTestCase):
     def setUp(self):
-        account = StripePayoutAccountFactory.create()
+        account = StripePayoutAccountFactory.create(status='verified')
         bank_account = ExternalAccountFactory.create(connect_account=account)
+        self.initiative = InitiativeFactory.create(
+
+        )
+        self.initiative.transitions.submit()
+        self.initiative.transitions.approve()
         self.funding = FundingFactory(
             deadline=now() + timedelta(days=10),
             target=Money(4000, 'EUR'),
-            bank_account=bank_account
+            bank_account=bank_account,
+            initiative=self.initiative
         )
+        BudgetLineFactory.create(activity=self.funding)
+        self.funding.review_transitions.submit()
         self.funding.transitions.reviewed()
         self.funding.save()
 

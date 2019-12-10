f232ca852613765578ebe8259ab1f9e1c3d00e4f
Loek van Gent <loek@goodup.com>
2019-12-10 19:25:53 +0100

    another fix

--- bluebottle/assignments/transitions.py
+++ bluebottle/assignments/transitions.py
@@ -193,7 +193,7 @@ class ApplicantTransitions(ContributionTransitions):
     def succeed(self):
         follow(self.instance.user, self.instance.activity)
         if not self.instance.time_spent:
-            self.instance.time_spent = self.instance.activity.assignment.duration
+            self.instance.time_spent = self.instance.activity.duration
 
     @transition(
         field='status',

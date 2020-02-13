bf5bae9c3bea70997544a7fd4728163d91c627dd
Loek van Gent <loek@goodup.com>
2020-02-13 15:38:10 +0100

    Override makemessages and don't do fuzzy translations

---
+++ bluebottle/common/management/commands/makemessages.py
@@ -0,0 +1,5 @@
+from django.core.management.commands.makemessages import Command as MakeMessagesCommand
+
+
+class Command(MakeMessagesCommand):
+    msgmerge_options = ['-q', '--previous', '--no-fuzzy-matching']

f5b4562be2c4a048d6bf6f1931676874004f12d8
Ernst Odolphi <ernst@onepercentclub.com>
2020-03-30 15:42:00 +0200

    Authenticate user profile detail api call

--- bluebottle/bb_accounts/permissions.py
+++ bluebottle/bb_accounts/permissions.py
@@ -1,3 +1,4 @@
+from bluebottle.members.models import MemberPlatformSettings
 from bluebottle.utils.permissions import BasePermission
 
 
@@ -7,3 +8,13 @@ class CurrentUserPermission(BasePermission):
 
     def has_permission(self, request, view):
         return True
+
+
+class IsAuthenticatedOrOpenPermission(BasePermission):
+    def has_object_action_permission(self, action, user, obj):
+        settings = MemberPlatformSettings.objects.get()
+        return (user and user.is_authenticated) or not settings.closed
+
+    def has_permission(self, request, view):
+        settings = MemberPlatformSettings.objects.get()
+        return request.user.is_authenticated or not settings.closed

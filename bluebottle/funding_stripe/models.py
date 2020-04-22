32366ff806a459c3999bea762454312e592f29db
Ernst Odolphi <ernst@onepercentclub.com>
2020-04-22 14:40:00 +0200

    Mark as incomplete when having past_due fields

--- bluebottle/funding_stripe/models.py
+++ bluebottle/funding_stripe/models.py
@@ -393,8 +393,8 @@ class StripePayoutAccount(PayoutAccount):
                 if self.status != PayoutAccountTransitions.values.rejected:
                     self.transitions.reject()
             elif getattr(self.account.requirements, 'disabled_reason', None):
-                if self.status != PayoutAccountTransitions.values.rejected:
-                    self.transitions.reject()
+                if self.status != PayoutAccountTransitions.values.incomplete:
+                    self.transitions.set_incomplete()
             elif len(self.missing_fields) == 0 and len(self.pending_fields) == 0:
                 if self.status != PayoutAccountTransitions.values.verified:
                     self.transitions.verify()

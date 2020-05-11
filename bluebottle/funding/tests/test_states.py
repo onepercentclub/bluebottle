b0b75ca112836de43ab3e77fc18f3739bcaf723f
Loek van Gent <loek@goodup.com>
2020-05-11 12:54:11 +0200

    Fix funding state tests

--- bluebottle/funding/tests/test_states.py
+++ bluebottle/funding/tests/test_states.py
@@ -1,6 +1,7 @@
 from bluebottle.activities.states import OrganizerStateMachine
 from bluebottle.funding.states import FundingStateMachine
-from bluebottle.funding.tests.factories import FundingFactory
+from bluebottle.funding.tests.factories import FundingFactory, BudgetLineFactory, BankAccountFactory, \
+    PlainPayoutAccountFactory
 from bluebottle.initiatives.tests.factories import InitiativeFactory
 from bluebottle.test.utils import BluebottleTestCase
 
@@ -9,16 +10,27 @@ class FundingStateMachineTests(BluebottleTestCase):
     def setUp(self):
         self.initiative = InitiativeFactory.create()
         self.initiative.states.approve(save=True)
+        self.funding = FundingFactory.create(initiative=self.initiative)
+        BudgetLineFactory.create(activity=self.funding)
+        payout_account = PlainPayoutAccountFactory.create()
+        bank_account = BankAccountFactory.create(connect_account=payout_account)
+        self.funding.bank_account = bank_account
+        self.funding.save()
 
-    def test_create(self):
-        funding = FundingFactory.create(initiative=self.initiative)
-        self.assertEqual(funding.status, FundingStateMachine.open.value)
-        organizer = funding.contributions.get()
+    def test_approve(self):
+        self.funding.states.approve(save=True)
+        self.assertEqual(self.funding.status, FundingStateMachine.open.value)
+        organizer = self.funding.contributions.get()
         self.assertEqual(organizer.status, OrganizerStateMachine.succeeded.value)
 
     def test_reject(self):
-        funding = FundingFactory.create(initiative=self.initiative)
-        funding.states.reject(save=True)
-        self.assertEqual(funding.status, FundingStateMachine.rejected.value)
-        organizer = funding.contributions.get()
+        self.funding.states.reject(save=True)
+        self.assertEqual(self.funding.status, FundingStateMachine.rejected.value)
+        organizer = self.funding.contributions.get()
+        self.assertEqual(organizer.status, OrganizerStateMachine.failed.value)
+
+    def test_delete(self):
+        self.funding.states.delete(save=True)
+        self.assertEqual(self.funding.status, FundingStateMachine.deleted.value)
+        organizer = self.funding.contributions.get()
         self.assertEqual(organizer.status, OrganizerStateMachine.failed.value)

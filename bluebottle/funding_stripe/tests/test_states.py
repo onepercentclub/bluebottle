aa2f15ca53d33093179cf2f3313e3f3a8a739446
Loek van Gent <loek@goodup.com>
2020-05-25 10:37:43 +0200

    Fix Stipe payment tests

--- bluebottle/funding_stripe/tests/test_states.py
+++ bluebottle/funding_stripe/tests/test_states.py
@@ -26,57 +26,55 @@ class BaseStripePaymentStateMachineTests(BluebottleTestCase):
 class StripeSourcePaymentStateMachineTests(BaseStripePaymentStateMachineTests):
 
     @patch('stripe.Source.modify')
-    def test_request_refund(self, mock_modify):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.succeed(save=True)
-        self.assertEqual(payment.status, 'succeeded')
+    def setUp(self, mock_modify):
+        super(StripeSourcePaymentStateMachineTests, self).setUp()
+        self.donation = DonationFactory.create(activity=self.funding)
+        self.payment = StripeSourcePaymentFactory.create(
+            charge_token='some_token',
+            donation=self.donation
+        )
+
+    def test_request_refund(self):
+        self.payment.states.succeed(save=True)
+        self.assertEqual(self.payment.status, 'succeeded')
         with patch('bluebottle.funding_stripe.models.StripeSourcePayment.refund') as refund:
-            payment.states.request_refund(save=True)
+            self.payment.states.request_refund(save=True)
             refund.assert_called_once()
-            self.assertEqual(payment.status, 'refund_requested')
+            self.assertEqual(self.payment.status, 'refund_requested')
 
     def test_authorize(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.authorize(save=True)
-        self.assertEqual(payment.status, 'pending')
+        self.payment.states.charge(save=True)
+        self.payment.states.authorize(save=True)
+        self.assertEqual(self.payment.status, 'pending')
 
     def test_authorize_donation_succeed(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.authorize(save=True)
-        self.assertEqual(donation.status, 'succeeded')
+        self.payment.states.charge(save=True)
+        self.payment.states.authorize(save=True)
+        self.assertEqual(self.donation.status, 'succeeded')
 
     def test_succeed(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.succeed(save=True)
-        self.assertEqual(payment.status, 'succeeded')
+        self.payment.states.charge(save=True)
+        self.payment.states.succeed(save=True)
+        self.assertEqual(self.payment.status, 'succeeded')
 
     def test_succeed_donation_succeed(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.succeed(save=True)
-        self.assertEqual(donation.status, 'succeeded')
+        self.payment.states.charge(save=True)
+        self.payment.states.succeed(save=True)
+        self.assertEqual(self.donation.status, 'succeeded')
 
     def test_charge(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.charge(save=True)
-        self.assertEqual(payment.status, 'charged')
+        self.payment.states.charge(save=True)
+        self.assertEqual(self.payment.status, 'charged')
 
     def test_cancel(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.cancel(save=True)
-        self.assertEqual(payment.status, 'canceled')
+        self.payment.states.cancel(save=True)
+        self.assertEqual(self.payment.status, 'canceled')
 
     def test_dispute(self):
-        donation = DonationFactory.create(activity=self.funding)
-        payment = StripeSourcePaymentFactory.create(donation=donation)
-        payment.states.dispute(save=True)
-        self.assertEqual(payment.status, 'disputed')
+        self.payment.states.charge(save=True)
+        self.payment.states.succeed(save=True)
+        self.payment.states.dispute(save=True)
+        self.assertEqual(self.payment.status, 'disputed')
 
 
 class StripePaymentStateMachineTests(BaseStripePaymentStateMachineTests):

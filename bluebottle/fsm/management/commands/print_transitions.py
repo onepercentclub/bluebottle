5badfd06bcee72ff89c65fb14fbc430de40db078
Loek van Gent <loek@goodup.com>
2020-06-04 15:42:57 +0200

    Tweak print command

--- bluebottle/fsm/management/commands/print_transitions.py
+++ bluebottle/fsm/management/commands/print_transitions.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import FieldDoesNotExist
 from django.core.management.base import BaseCommand
 from django.utils.module_loading import import_string
 
+from bluebottle.funding.models import Donation, Funding
 from bluebottle.members.models import Member
 
 
@@ -36,6 +38,13 @@ class Command(BaseCommand):
             help='Email of the models user'
         )
 
+    def _has_field(self, model, field):
+        try:
+            model._meta.get_field(field)
+            return True
+        except FieldDoesNotExist:
+            return False
+
     def handle(self, *args, **options):
         model = import_string(options['model'])
 
@@ -46,59 +55,64 @@ class Command(BaseCommand):
         else:
             model_args = {}
 
-        if options.get('owner'):
+        if options.get('owner') and self._has_field(model, 'owner'):
             model_args['owner'] = Member(email=options['owner'])
 
-        if options.get('user'):
+        if options.get('user') and self._has_field(model, 'user'):
             model_args['user'] = Member(email=options['user'])
 
         instance = model(
             **model_args
         )
+
+        if isinstance(instance, Donation):
+            instance.activity = Funding(title='Dummy Fundraising Campaign')
+
         machine = instance.states
 
-        print '# {}\n'.format(model._meta.model_name.capitalize())
+        print u'# {}\n'.format(model._meta.model_name.capitalize())
 
-        print '## States\n'
+        print u'## States\n'
 
-        print '|State Name|Description|'
-        print '|---|---|'
+        print u'|State Name|Description|'
+        print u'|---|---|'
 
         for state in machine.states.values():
-            print '|{}|{}|'.format(state.name.capitalize(), state.description)
+            print u'|{}|{}|'.format(state.name.capitalize(), state.description)
 
-        print '\n'
+        print u'\n'
 
-        print '## Transitions\n'
-        print '|Name|Description|From|To|Manual|Conditions|Side Effects|'
-        print '|---|---|---|---|---|--------|--------|'
+        print u'## Transitions\n'
+        print u'|Name|Description|From|To|Manual|Conditions|Side Effects|'
+        print u'|---|---|---|---|---|--------|--------|'
 
         for transition in machine.transitions.values():
-            print '| {} | {} | {} | {} | {} | {} | {} |'.format(
+            print u'| {} | {} | {} | {} | {} | {} | {} |'.format(
                 transition.name,
                 transition.description,
                 ', '.join(state.name.capitalize() for state in transition.sources),
                 transition.target.name.capitalize(),
                 'Automatic' if transition.automatic else 'Manual',
-                ', '.join(
+                '<br>'.join(
                     get_doc(condition)
                     for condition
                     in transition.conditions
                 ),
-                ', '.join(
-                    unicode(effect(instance)) for effect in transition.effects
+                '<br>'.join(
+                    unicode(effect(instance))
+                    for effect
+                    in transition.effects
                 ),
             )
-
-        print '\n'
+        print u'\n'
 
         if model.triggers:
-            print '# Triggers\n'
-            print '|When|Effects|'
-            print '|---|---|'
+            print u'# Triggers\n'
+            print u'|When|Effects|'
+            print u'|---|---|'
 
             for trigger in model.triggers:
-                print '|{}|{}|'.format(
+                print u'|{}|{}|'.format(
                     unicode(trigger),
                     ', '.join(unicode(effect(instance)) for effect in trigger.effects)
                 )

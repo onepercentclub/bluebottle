46b7b1ec74d3cc80ef1318e30fc7d01ec28950c5
Ernst Odolphi <ernst@onepercentclub.com>
2020-06-30 09:34:20 +0200

    Add tests

--- bluebottle/utils/tests/test_api.py
+++ bluebottle/utils/tests/test_api.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from bluebottle.test.utils import BluebottleTestCase
 from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
 from bluebottle.test.factory_models.projects import ProjectFactory
+from bluebottle.utils.models import TranslationPlatformSettings
 from django.core.urlresolvers import reverse
 
 
@@ -94,3 +95,32 @@ class TestLanguageApi(BluebottleTestCase):
         """ simple language list """
         response = self.client.get(self.language_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TranslationSettingsTestCase(BluebottleTestCase):
+
+    def setUp(self):
+        super(TranslationSettingsTestCase, self).setUp()
+        self.settings_url = reverse('settings')
+
+    def test_no_translations(self):
+        response = self.client.get(self.settings_url)
+        self.assertEqual(
+            response.data['platform']['translations'],
+            {}
+        )
+
+    def test_with_translation(self):
+        TranslationPlatformSettings.objects.create(
+            office='Site',
+            whats_the_location_of_your_office='What is your office site'
+        )
+        response = self.client.get(self.settings_url)
+        self.assertEqual(
+            response.data['platform']['translations']['Office'],
+            'Site'
+        )
+        self.assertEqual(
+            response.data['platform']['translations'][u'What\u2019s the location of your office?'],
+            'What is your office site'
+        )

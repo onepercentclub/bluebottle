6ef19a24f660e9d76b74f9729ad4273ee4b14d7e
Loek van Gent <loek@goodup.com>
2020-07-07 16:17:14 +0200

    Limit private files upload types

--- bluebottle/settings/base.py
+++ bluebottle/settings/base.py
@@ -666,6 +666,12 @@ DJANGO_WYSIWYG_FLAVOR = "tinymce_advanced"
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 IMAGE_ALLOWED_MIME_TYPES = ('image/png', 'image/jpeg', 'image/gif', 'image/svg+xml')
+PRIVATE_FILE_ALLOWED_MIME_TYPES = (
+    'image/png', 'image/jpeg', 'image/gif', 'image/tiff',
+    'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'application/pdf', 'application/vnd.oasis.opendocument.text',
+    'text/rtf'
+)
 
 EXPORTDB_EXPORT_CONF = {
     'models': OrderedDict([

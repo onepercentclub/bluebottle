4e0d90fc157760606ae8503762f10bdef30bff8c
Ernst Odolphi <ernst@onepercentclub.com>
2020-07-07 11:54:59 +0200

    Remove trailing slashes

--- bluebottle/impact/urls/api.py
+++ bluebottle/impact/urls/api.py
@@ -7,10 +7,10 @@ from bluebottle.impact.views import (
 )
 
 urlpatterns = [
-    url(r'^types/$', ImpactTypeList.as_view(), name='impact-type-list'),
-    url(r'^goals/$', ImpactGoalList.as_view(), name='impact-goal-list'),
+    url(r'^types$', ImpactTypeList.as_view(), name='impact-type-list'),
+    url(r'^goals$', ImpactGoalList.as_view(), name='impact-goal-list'),
     url(
-        r'^goals/(?P<pk>\d+)/$',
+        r'^goals/(?P<pk>\d+)$',
         ImpactGoalDetail.as_view(),
         name='impact-goal-details'
     )

431896acbeb2c1f92195619583d0b18998992f16
Ernst Odolphi <ernst@onepercentclub.com>
2020-07-07 16:18:24 +0200

    Make sure we save the user when setting location

--- bluebottle/token_auth/auth/base.py
+++ bluebottle/token_auth/auth/base.py
@@ -49,6 +49,7 @@ class BaseTokenAuthentication(object):
         if 'location.slug' in data:
             try:
                 user.location = Location.objects.get(slug=data['location.slug'])
+                user.save()
             except Location.DoesNotExist:
                 pass
 

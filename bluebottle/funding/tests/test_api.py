3513db3d9380eddab63217a5f3649a5aa222ea3d
Loek van Gent <loek@goodup.com>
2020-07-14 10:41:56 +0200

    Feature/telesom zaad (#4163)
    * First models and stuff for Telesom/Zaad
    
    * Add Telesom logic
    
    * Fix migration
    
    * Fix old style transitions
    
    * REmove new states
    
    * Fix Telesom
    
    * No update on Telesom Payments
    
    * Fix tests
    
    * Add TElesom tests

--- bluebottle/funding/tests/test_api.py
+++ bluebottle/funding/tests/test_api.py
@@ -1344,24 +1344,28 @@ class PayoutAccountTestCase(BluebottleTestCase):
                 {
                     u'code': u'bancontact',
                     u'name': u'Bancontact',
+                    u'provider': u'stripe',
                     u'currencies': [u'EUR'],
                     u'countries': [u'BE']
                 },
                 {
                     u'code': u'credit-card',
                     u'name': u'Credit card',
+                    u'provider': u'stripe',
                     u'currencies': [u'EUR', u'USD'],
                     u'countries': []
                 },
                 {
                     u'code': u'direct-debit',
                     u'name': u'Direct debit',
+                    u'provider': u'stripe',
                     u'currencies': [u'EUR'],
                     u'countries': []
                 },
                 {
                     u'code': u'ideal',
                     u'name': u'iDEAL',
+                    u'provider': u'stripe',
                     u'currencies': [u'EUR'],
                     u'countries': [u'NL']
                 }
@@ -1393,6 +1397,7 @@ class PayoutAccountTestCase(BluebottleTestCase):
                     u'code': u'credit-card',
                     u'name': u'Credit card',
                     u'currencies': [u'EUR', u'USD', u'GBP', u'AUD'],
+                    u'provider': u'stripe',
                     u'countries': []
                 }
             ]

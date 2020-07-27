5b9a7316b7d8a31cc5f6fa298b3c7ae07e8f3f00
Loek van Gent <loek@goodup.com>
2020-07-27 08:42:15 +0200

    Remove obsolete

--- bluebottle/activities/admin.py
+++ bluebottle/activities/admin.py
@@ -140,7 +140,6 @@ class ActivityChildAdmin(PolymorphicChildModelAdmin, FSMAdmin):
     detail_fields = (
         'description',
         'highlight'
-        'segments',
     )
 
     def get_fieldsets(self, request, obj=None):

228576e809afe2294ef63af63bb9b18bd76ad180
Loek van Gent <loek@goodup.com>
2020-08-03 09:59:37 +0200

    Cache used countries

--- bluebottle/geo/views.py
+++ bluebottle/geo/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
-
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework_json_api.views import AutoPrefetchMixin
 
@@ -12,6 +13,10 @@ from .serializers import CountrySerializer
 class CountryList(TranslatedApiViewMixin, ListAPIView):
     serializer_class = CountrySerializer
     queryset = Country.objects
+
+    @method_decorator(cache_page(3600))
+    def get(self, request, *args, **kwargs):
+        return super(CountryList, self).get(request, *args, **kwargs)
 
     def get_queryset(self):
         qs = super(CountryList, self).get_queryset().filter(
@@ -37,9 +42,9 @@ class CountryDetail(RetrieveAPIView):
 
 
 class UsedCountryList(CountryList):
+
     def get_queryset(self):
         qs = super(UsedCountryList, self).get_queryset()
-
         return qs.filter(
             Q(geolocation__initiative__status='approved') |
             Q(geolocation__event__review_status='approved') |

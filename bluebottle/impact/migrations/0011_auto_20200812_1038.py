a9f2f374207eb9c3ced10f74e54d4e1711da189d
Ernst Odolphi <ernst@onepercentclub.com>
2020-08-12 10:40:47 +0200

    Add impact permissions

---
+++ bluebottle/impact/migrations/0011_auto_20200812_1038.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+
+from django.db import migrations, connection
+
+from bluebottle.utils.utils import update_group_permissions
+
+from bluebottle.clients import properties
+from bluebottle.clients.models import Client
+from bluebottle.clients.utils import LocalTenant
+
+
+def add_group_permissions(apps, schema_editor):
+    tenant = Client.objects.get(schema_name=connection.tenant.schema_name)
+    with LocalTenant(tenant):
+        group_perms = {
+            'Staff': {
+                'perms': (
+                    'add_impacttype', 'change_impacttype', 'delete_impacttype',
+                    'add_impactgoal', 'change_impactgoal', 'delete_impactgoal',
+                )
+            },
+        }
+
+        update_group_permissions('impact', group_perms, apps)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('impact', '0010_impacttypetranslation_unit'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_group_permissions,
+            migrations.RunPython.noop
+        )
+    ]

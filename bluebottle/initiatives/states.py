55b9ba6f45988f107be494f6c62f688672901e89
Ernst Odolphi <ernst@onepercentclub.com>
2020-08-28 10:34:31 +0200

    Some small content fixes for tooltips

--- bluebottle/initiatives/states.py
+++ bluebottle/initiatives/states.py
@@ -149,7 +149,7 @@ class ReviewStateMachine(ModelStateMachine):
         cancelled,
         name=_('Cancel'),
         description=_("Cancel if the initiative will not be executed. "
-                      "The initiator will not be able to edit the initiative and"
+                      "The initiator will not be able to edit the initiative and "
                       "it won't show up on the search page in the front end. "
                       "The initiative will still be available in the back office and appear in your reporting."),
         automatic=False,
@@ -180,7 +180,7 @@ class ReviewStateMachine(ModelStateMachine):
         ],
         needs_work,
         name=_('Restore'),
-        description=_("The status of the initiative is set to 'Draft'. "
+        description=_("The status of the initiative is set to 'needs work'. "
                       "The initiator can edit and submit the initiative again."),
         automatic=False,
         permission=is_staff,

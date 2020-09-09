12c6a976e6f372170cad948b074b684c04b8c296
Loek van Gent <loek@goodup.com>
2020-09-09 15:29:55 +0200

    Fix setting

--- bluebottle/settings/base.py
+++ bluebottle/settings/base.py
@@ -671,7 +671,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 IMAGE_ALLOWED_MIME_TYPES = (
     'image/png', 'image/jpeg', 'image/gif', 'image/svg+xml'
 )
-VIDEO_ALLOWED_MIME_TYPES = (
+VIDEO_FILE_ALLOWED_MIME_TYPES = (
     'video/ogg', 'video/mp4', 'video/webm', 'video/3gpp',
     'video/x-msvideo', 'video/quicktime'
 )

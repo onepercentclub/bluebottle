a1d091df794db54e8b7d041893ac297907e540f6
Ernst Odolphi <ernst@goodup.com>
2020-09-18 11:46:54 +0200

    Add celery task for updating rates

--- bluebottle/funding/tasks.py
+++ bluebottle/funding/tasks.py
@@ -2,6 +2,7 @@ import logging
 
 from celery.schedules import crontab
 from celery.task import periodic_task
+from djmoney.contrib.exchange.backends import OpenExchangeRatesBackend
 
 from bluebottle.clients.models import Client
 from bluebottle.clients.utils import LocalTenant
@@ -20,3 +21,12 @@ def funding_tasks():
         with LocalTenant(tenant, clear_tenant=True):
             for task in Funding.get_periodic_tasks():
                 task.execute()
+
+
+@periodic_task(
+    run_every=(crontab(minute='*')),
+    name="update_rates",
+    ignore_result=True
+)
+def update_rates():
+    OpenExchangeRatesBackend().update_rates()

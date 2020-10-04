7e629f610d3bce71569578750b1931f90718f14a
John Vandenberg <jayvdb@gmail.com>
2020-10-04 21:23:06 +0700

    setup.py: Add django-nested-inline
    django-nested-inline is currently installed as an
    indirect dependency, but it is also directly imported
    so it should be directly listed.

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ install_requires = [
     'django-memoize==2.1.0',
     'django-modeltranslation==0.12.1',
     'django-money==0.15.1',
+    'django-nested-inline==0.4.2',
     'django-parler==1.9.2',
     'django-permissions-widget==1.5.1',
     'django_polymorphic==1.2',

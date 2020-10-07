13882191334ed7e6fbd0905cbf664813a507a5ce
Ernst Odolphi <ernst@goodup.com>
2020-10-07 13:26:16 +0200

    Do not convert is currencies are the same

--- bluebottle/utils/exchange_rates.py
+++ bluebottle/utils/exchange_rates.py
@@ -4,7 +4,13 @@ from djmoney.contrib.exchange.models import convert_money
 
 def convert(money, currency):
     """ Convert money object `money` to `currency`."""
-    if money.currency != settings.BASE_CURRENCY and currency != settings.BASE_CURRENCY:
+    if hasattr(currency, 'code'):
+        currency = currency.code
+
+    if money.currency.code == currency:
+        return money
+
+    if money.currency.code != settings.BASE_CURRENCY and currency != settings.BASE_CURRENCY:
         money = convert_money(money, settings.BASE_CURRENCY)
 
     return convert_money(money, currency)

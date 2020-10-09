618e992d848e1b95e5b4028154bdb3a8303fc83f
Loek van Gent <loek@goodup.com>
2020-10-09 09:20:55 +0200

    REmove more old code

--- bluebottle/utils/context_processors.py
+++ bluebottle/utils/context_processors.py
@@ -1,33 +1,4 @@
-import re
-
 from django.db import connection
-from django.conf import settings
-
-
-def sentry_dsn(request):
-    """
-    Make the Sentry / Raven DSN available in the templates *without* the secret key.
-    """
-    try:
-        raven_config = settings.RAVEN_CONFIG['dsn']
-    except AttributeError:
-        return {}
-    except KeyError:
-        return {}
-
-    match = re.search(
-        r"https:\/\/([a-z|0-9]+):([a-z|0-9]+)\@app.getsentry.com\/(\d+)",
-        raven_config, re.M | re.I)
-
-    if not match:
-        return {}
-    else:
-        public_key = match.group(1)
-        project_id = match.group(3)
-
-        return {
-            'RAVEN_DSN': "https://{0}@app.getsentry.com/{1}".format(public_key,
-                                                                    project_id)}
 
 
 def tenant(request):

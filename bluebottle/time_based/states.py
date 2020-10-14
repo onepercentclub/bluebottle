be469f3dd958d38db01fa99b2cd97c2041fa8aaf
Ernst Odolphi <ernst@goodup.com>
2020-10-14 16:19:14 +0200

    Add some triggers and transitions

--- bluebottle/time_based/states.py
+++ bluebottle/time_based/states.py
@@ -1,18 +1,71 @@
+from django.utils.translation import ugettext_lazy as _
+
 from bluebottle.activities.states import ActivityStateMachine
 from bluebottle.time_based.models import OnADateActivity, WithADeadlineActivity, OngoingActivity
-from bluebottle.fsm.state import register
+from bluebottle.fsm.state import register, State, Transition
+
+
+class TimeBasedStateMachine(ActivityStateMachine):
+    full = State(_('full'), 'full', _('The event is full, users can no longer apply.'))
+
+    lock = Transition(
+        [
+            ActivityStateMachine.open,
+            ActivityStateMachine.succeeded
+        ],
+        full,
+        name=_("Lock"),
+        description=_(
+            "People can no longer join the event. Triggered when the attendee limit is reached."
+        )
+    )
+
+    reopen = Transition(
+        full,
+        ActivityStateMachine.open,
+        name=_("Reopen"),
+        description=_(
+            "People can join the event again. Triggered when the number of attendees become "
+            "less than the attendee limit."
+        )
+    )
 
 
 @register(OnADateActivity)
-class OnADateStateMachine(ActivityStateMachine):
-    pass
+class OnADateStateMachine(TimeBasedStateMachine):
+    running = State(
+        _('running'),
+        'running',
+        _('The event is taking place and people can\'t join any more.')
+    )
+
+    start = Transition(
+        [
+            ActivityStateMachine.open,
+            TimeBasedStateMachine.full
+        ],
+        running,
+        name=_("Start"),
+        description=_("Start the event.")
+    )
+
+    reschedule = Transition(
+        [
+            running,
+            ActivityStateMachine.cancelled,
+            ActivityStateMachine.succeeded
+        ],
+        ActivityStateMachine.open,
+        name=_("Reschedule"),
+        description=_("People can join the event again, because the date has changed."),
+    )
 
 
 @register(WithADeadlineActivity)
-class WithADeadlineStateMachine(ActivityStateMachine):
+class WithADeadlineStateMachine(TimeBasedStateMachine):
     pass
 
 
 @register(OngoingActivity)
-class OngoingStateMachine(ActivityStateMachine):
+class OngoingStateMachine(TimeBasedStateMachine):
     pass
--- bluebottle/time_based/triggers.py
+++ bluebottle/time_based/triggers.py
@@ -1,18 +1,114 @@
-from bluebottle.fsm.triggers import register
-from bluebottle.time_based.models import OnADateActivity, WithADeadlineActivity, OngoingActivity
+from datetime import timedelta
+from django.utils.timezone import now
+
+from bluebottle.fsm.triggers import register, ModelChangedTrigger
+from bluebottle.fsm.effects import TransitionEffect
+from bluebottle.notifications.effects import NotificationEffect
+
 from bluebottle.activities.triggers import ActivityTriggers
+
+from bluebottle.time_based.models import OnADateActivity, WithADeadlineActivity, OngoingActivity
+from bluebottle.time_based.messages import DateChanged
+from bluebottle.time_based.states import TimeBasedStateMachine, OnADateStateMachine
+
+
+def is_full(effect):
+    "the activity is full"
+    return effect.instance.capacity == len(effect.instance.participants)
+
+
+def is_not_full(effect):
+    "the activity is not full"
+    return effect.instance.capacity > len(effect.instance.participants)
+
+
+def is_finished(effect):
+    return (
+        effect.instance.start and
+        effect.instance.duration and
+        effect.start + timedelta(hours=effect.instance.duration) > now()
+    )
+
+
+def is_not_finished(effect):
+    return (
+        effect.instance.start and
+        effect.instance.duration and
+        effect.start + timedelta(hours=effect.instance.duration) < now()
+    )
+
+
+def is_running(effect):
+    return (
+        effect.instance.start and
+        effect.instance.duration and
+        effect.start > now() and
+        effect.start + timedelta(hours=effect.instance.duration) < now()
+    )
+
+
+class TimeBasedTriggers(ActivityTriggers):
+    triggers = ActivityTriggers.triggers + [
+        ModelChangedTrigger(
+            'capacity',
+            effects=[
+                TransitionEffect(TimeBasedStateMachine.reopen, conditions=[
+                    is_not_full
+                ]),
+                TransitionEffect(TimeBasedStateMachine.lock, conditions=[
+                    is_full
+                ]),
+            ]
+        ),
+    ]
 
 
 @register(OnADateActivity)
-class OnADateTriggers(ActivityTriggers):
-    pass
+class OnADateTriggers(TimeBasedTriggers):
+    triggers = TimeBasedTriggers.triggers + [
+        ModelChangedTrigger(
+            'start',
+            effects=[
+                NotificationEffect(
+                    DateChanged,
+                    conditions=[
+                        is_not_finished
+                    ]
+                ),
+                TransitionEffect(
+                    OnADateStateMachine.succeed,
+                    conditions=[
+                        is_finished
+                    ]
+                ),
+                TransitionEffect(
+                    OnADateStateMachine.start,
+                    conditions=[
+                        is_running,
+                    ]
+                ),
+                TransitionEffect(
+                    OnADateStateMachine.expire,
+                    conditions=[
+                        is_finished
+                    ]
+                ),
+                TransitionEffect(
+                    OnADateStateMachine.reschedule,
+                    conditions=[
+                        is_not_finished
+                    ]
+                ),
+            ]
+        )
+    ]
 
 
 @register(WithADeadlineActivity)
-class WithADeadlineTriggers(ActivityTriggers):
+class WithADeadlineTriggers(TimeBasedTriggers):
     pass
 
 
 @register(OngoingActivity)
-class OngoingTriggers(ActivityTriggers):
+class OngoingTriggers(TimeBasedTriggers):
     pass

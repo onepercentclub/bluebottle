238d9bdb09b8a9e2be86aa5e638b9db47ed95427
Rolfertjan <thijsen.rolf@gmail.com>
2020-10-26 11:38:42 +0100

    update copy (#4341)
    Co-authored-by: Rolf <rolf@goodup.com>

--- bluebottle/members/tests/test_api.py
+++ bluebottle/members/tests/test_api.py
@@ -108,7 +108,7 @@ class SignUpTokenTestCase(BluebottleTestCase):
 
         member = Member.objects.get(email=email)
         self.assertTrue('{}:'.format(member.pk) in mail.outbox[0].body)
-        self.assertEqual('Your activation link for Test', mail.outbox[0].subject)
+        self.assertEqual('Activate your account for Test', mail.outbox[0].subject)
         self.assertFalse(member.is_active)
 
     def test_create_twice(self):

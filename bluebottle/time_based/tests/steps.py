9e96a6bd1a4b2d079922892811d8b78eed5ec202
Loek van Gent <loek@goodup.com>
2021-01-22 13:07:55 +0100

    More scneario based tests

---
+++ bluebottle/time_based/tests/steps.py
@@ -0,0 +1,57 @@
+import json
+
+from django.urls import reverse
+from rest_framework.status import HTTP_201_CREATED
+
+
+def api_user_joins_activity(test, activity, supporter, request_user=None):
+    if not request_user:
+        request_user = supporter
+    test.data = {
+        'data': {
+            'type': 'contributors/time-based/date-participants',
+            'attributes': {
+                'motiviation': 'I am great',
+            },
+            'relationships': {
+                'activity': {
+                    'data': {
+                        'type': 'activities/time-based/dates',
+                        'id': activity.pk
+                    }
+                }
+            }
+        }
+    }
+    url = reverse('date-participant-list')
+    response = test.client.post(url, json.dumps(test.data), user=request_user)
+    test.assertEqual(response.status_code, HTTP_201_CREATED)
+
+
+def api_participant_transition(test, activity, supporter, transition, request_user=None):
+    if not request_user:
+        request_user = supporter
+    participant = activity.contributors.filter(user=supporter).get()
+    test.data = {
+        'data': {
+            'type': 'contributors/time-based/date-participant-transitions',
+            'attributes': {
+                'transition': transition
+            },
+            'relationships': {
+                'resource': {
+                    'data': {
+                        'type': 'contributors/time-based/date-participants',
+                        'id': participant.pk
+                    }
+                }
+            }
+        }
+    }
+    url = reverse('date-participant-transition-list')
+    response = test.client.post(url, json.dumps(test.data), user=request_user)
+    test.assertEqual(response.status_code, HTTP_201_CREATED)
+
+
+def assert_participant_status(test, activity, supporter, status):
+    return test.assertEqual(activity.contributors.filter(user=supporter).first().status, status)
---
+++ bluebottle/time_based/tests/test_scenarios.py
@@ -0,0 +1,33 @@
+from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
+from bluebottle.test.utils import BluebottleTestCase, JSONAPITestClient
+from bluebottle.time_based.tests.factories import DateActivityFactory
+from bluebottle.time_based.tests.steps import api_user_joins_activity, assert_participant_status, \
+    api_participant_transition
+
+
+class DateParticipantScenarioTestCase(BluebottleTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.owner = BlueBottleUserFactory.create()
+        self.supporter = BlueBottleUserFactory.create()
+        self.activity = DateActivityFactory.create(status='open', owner=self.owner)
+        self.client = JSONAPITestClient()
+
+    def test_scenario_user_joins_activity(self):
+        api_user_joins_activity(self, activity=self.activity, supporter=self.supporter)
+        assert_participant_status(self, activity=self.activity, supporter=self.supporter, status='accepted')
+
+    def test_scenario_user_joins_review_activity(self):
+        self.activity.review = True
+        self.activity.save()
+        api_user_joins_activity(self, activity=self.activity, supporter=self.supporter)
+        assert_participant_status(self, activity=self.activity, supporter=self.supporter, status='new')
+        api_participant_transition(self, activity=self.activity, supporter=self.supporter,
+                                   transition='accept', request_user=self.owner)
+        assert_participant_status(self, activity=self.activity, supporter=self.supporter, status='accepted')
+
+    def test_scenario_user_withdraws_from_activity(self):
+        api_user_joins_activity(self, activity=self.activity, supporter=self.supporter)
+        api_participant_transition(self, activity=self.activity, supporter=self.supporter, transition='withdraw')
+        assert_participant_status(self, activity=self.activity, supporter=self.supporter, status='withdrawn')

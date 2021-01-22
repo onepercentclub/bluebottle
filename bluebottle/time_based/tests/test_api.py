184e19266dbc8e8d0413a9b0df64e7f9840b071f
Ernst Odolphi <ernst@goodup.com>
2021-01-22 09:40:08 +0100

    Fix tests

--- bluebottle/time_based/tests/test_api.py
+++ bluebottle/time_based/tests/test_api.py
@@ -1398,10 +1398,10 @@ class SlotParticipantTransitionAPIViewTestCase(BluebottleTestCase):
 
         data = json.loads(response.content)
         self.assertEqual(
-            data['included'][0]['type'],
+            data['included'][2]['type'],
             'contributors/time-based/slot-participants'
         )
-        self.assertEqual(data['included'][0]['attributes']['status'], 'withdrawn')
+        self.assertEqual(data['included'][2]['meta']['status'], 'withdrawn')
 
     def test_reapply_by_user(self):
         self.test_withdraw_by_user()
@@ -1417,7 +1417,7 @@ class SlotParticipantTransitionAPIViewTestCase(BluebottleTestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         data = json.loads(response.content)
-        self.assertEqual(data['included'][0]['attributes']['status'], 'registered')
+        self.assertEqual(data['included'][2]['meta']['status'], 'registered')
 
     def test_withdraw_by_owner(self):
         self.data['data']['attributes']['transition'] = 'withdraw'
@@ -1455,7 +1455,7 @@ class SlotParticipantTransitionAPIViewTestCase(BluebottleTestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         data = json.loads(response.content)
-        self.assertEqual(data['included'][0]['attributes']['status'], 'removed')
+        self.assertEqual(data['included'][2]['meta']['status'], 'removed')
 
     def test_accept_by_owner(self):
         self.test_remove_by_owner()
@@ -1471,7 +1471,7 @@ class SlotParticipantTransitionAPIViewTestCase(BluebottleTestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         data = json.loads(response.content)
-        self.assertEqual(data['included'][0]['attributes']['status'], 'registered')
+        self.assertEqual(data['included'][2]['meta']['status'], 'registered')
 
     def test_remove_by_user(self):
         self.data['data']['attributes']['transition'] = 'remove'

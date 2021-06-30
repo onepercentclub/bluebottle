e632a97114c15bb83cd766868e9fbc98f863c496
Loek van Gent <loek@goodup.com>
2021-06-30 11:07:54 +0200

    Improve geocoder

--- scripts/geocode_locations.py
+++ scripts/geocode_locations.py
@@ -22,12 +22,11 @@ def geocode(address):
     return None
 
 
-def set_location(loc):
+def find_position(loc):
     if loc.street in ['thuis!', 'Thuis!']:
         address = "Amsterdam, Nederland"
     else:
         address = "{}, {}, Nederland".format(loc.street, loc.locality)
-
     try:
         position = Point(lookup[address])
     except KeyError:
@@ -35,35 +34,55 @@ def set_location(loc):
         if position:
             lookup[address] = position.coords
     if position:
-        loc.position = position
-        loc.save()
+        return position
     else:
         print("Could not find a location for {}".format(address))
         print("geolocation {}".format(loc.id))
+    return
 
 
 def run(*args):
     if not os.environ.get('GOOGLE_API_KEY'):
-        print("export GOOGLE_API_KEY xxx")
+        print("export GOOGLE_API_KEY=\"xxx\"")
         print("Look up secret at https://console.cloud.google.com/apis/credentials")
-    tne = Client.objects.get(schema_name='nlcares')
+    tenant = Client.objects.get(schema_name='nlcares')
 
-    with LocalTenant(tne):
+    with LocalTenant(tenant):
+        locations = []
         total = Geolocation.objects.filter(position__isnull=True).count()
         count = 0
         print("Geocoding activity locations")
         for loc in Geolocation.objects.filter(position__isnull=True).all():
             count += 1
             print("{} / {}".format(count, total))
-            set_location(loc)
+            loc.position = find_position(loc)
+            if count % 100 == 0:
+                print('Write geocoding file an records')
+                with open('geocoding.json', 'w') as outfile:
+                    json.dump(lookup, outfile, indent=4)
+                Geolocation.objects.bulk_update(locations, ['position'])
+                locations = []
+            locations.append(loc)
 
+        print('Write geocoding file')
+        with open('geocoding.json', 'w') as outfile:
+            json.dump(lookup, outfile, indent=4)
+
+        places = []
         print("Geocoding user locations")
         total = Place.objects.filter(position__isnull=True).count()
         count = 0
         for loc in Place.objects.filter(position__isnull=True).all():
             count += 1
+            if count % 100 == 0:
+                print('Write geocoding file and records')
+                with open('geocoding.json', 'w') as outfile:
+                    json.dump(lookup, outfile, indent=4)
+                Place.objects.bulk_update(places, ['position'])
+                places = []
             print("{} / {}".format(count, total))
-            set_location(loc)
+            loc.position = find_position(loc)
+            places.append(loc)
 
         print('Write geocoding file')
         with open('geocoding.json', 'w') as outfile:

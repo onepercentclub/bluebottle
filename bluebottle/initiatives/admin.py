473c61ed8bf73a14e55cb91db11b722099347f92
Loek van Gent <loek@goodup.com>
2021-08-17 09:37:58 +0200

    Fix issue with adding initiative through admin

--- bluebottle/initiatives/admin.py
+++ bluebottle/initiatives/admin.py
@@ -193,7 +193,7 @@ class InitiativeAdmin(PolymorphicInlineSupportMixin, NotificationAdminMixin, Sta
         if Location.objects.count():
             if obj and obj.is_open:
                 detail_fields.append('is_global')
-            if not obj.is_global:
+            if obj and not obj.is_global:
                 detail_fields.append('location')
         else:
             detail_fields.append('place')

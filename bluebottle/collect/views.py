10d9ba09b1c3d0572fd7d284d59dbf103246d949
Loek van Gent <loek@goodup.com>
2021-12-02 16:18:24 +0100

    Paginate related contribors for deed and collect too

--- bluebottle/collect/views.py
+++ bluebottle/collect/views.py
@@ -67,7 +67,6 @@ class CollectActivityRelatedCollectContributorList(JsonApiViewMixin, ListAPIView
     permission_classes = (
         OneOf(ResourcePermission, ResourceOwnerPermission),
     )
-    pagination_class = None
 
     queryset = CollectContributor.objects.prefetch_related('user')
     serializer_class = CollectContributorSerializer
--- bluebottle/deeds/views.py
+++ bluebottle/deeds/views.py
@@ -65,7 +65,6 @@ class DeedRelatedParticipantList(JsonApiViewMixin, ListAPIView):
     permission_classes = (
         OneOf(ResourcePermission, ResourceOwnerPermission),
     )
-    pagination_class = None
 
     queryset = DeedParticipant.objects.prefetch_related('user')
     serializer_class = DeedParticipantSerializer

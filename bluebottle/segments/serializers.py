efa1b3e052b64119c75684de78668c102c1196fb
Ernst Odolphi <ernst@goodup.com>
2021-12-06 10:36:45 +0100

    Make it possible to filter initiatives on segments

--- bluebottle/segments/serializers.py
+++ bluebottle/segments/serializers.py
@@ -2,6 +2,8 @@ from builtins import object
 from rest_framework import serializers
 
 from bluebottle.bluebottle_drf2.serializers import SorlImageField
+from bluebottle.activities.models import Activity
+from bluebottle.initiatives.models import Initiative
 from bluebottle.segments.models import Segment, SegmentType
 from bluebottle.utils.fields import SafeField
 
@@ -31,16 +33,27 @@ class SegmentSerializer(serializers.ModelSerializer):
 
     story = SafeField(required=False, allow_blank=True, allow_null=True)
 
+    initiatives_count = serializers.SerializerMethodField()
+    activities_count = serializers.SerializerMethodField()
+
     included_serializers = {
         'type': 'bluebottle.segments.serializers.SegmentTypeSerializer',
     }
+
+    def get_initiatives_count(self, obj):
+        return len(Initiative.objects.filter(status='approved', activities__segments=obj))
+
+    def get_activities_count(self, obj):
+        return len(Activity.objects.filter(segments=obj))
 
     class Meta(object):
         model = Segment
         fields = (
             'id', 'name', 'type', 'slug', 'tag_line', 'background_color',
             'text_color', 'logo', 'cover_image', 'story',
+            'initiatives_count', 'activities_count',
         )
+        meta_fields = ['initiatives_count', 'activities_count']
 
     class JSONAPIMeta(object):
         included_resources = ['type', ]

a23fb6d289fa3d4aed6036844127b0745344e081
Ernst Odolphi <ernst@goodup.com>
2022-02-15 09:59:13 +0100

    First check if we have a date activity

--- bluebottle/time_based/triggers.py
+++ bluebottle/time_based/triggers.py
@@ -58,8 +58,14 @@ def is_full(effect):
     """
     the activity is full
     """
+    if (
+        isinstance(effect.instance, DateActivity) and
+        effect.instance.slots.count() > 1 and
+        effect.instance.slot_selection == 'free'
+    ):
+        return False
+
     return (
-        (effect.instance.slots.count() == 1 or effect.instance.slot_selection == 'all') and
         effect.instance.capacity and
         effect.instance.capacity <= len(effect.instance.accepted_participants)
     )
@@ -873,8 +879,15 @@ def activity_will_be_full(effect):
     the activity is full
     """
     activity = effect.instance.activity
+
+    if (
+        isinstance(activity, DateActivity) and
+        activity.slots.count() > 1 and
+        activity.slot_selection == 'free'
+    ):
+        return False
+
     return (
-        (activity.slots.count() == 1 or activity.slot_selection == 'all') and
         activity.capacity and
         activity.capacity == len(activity.accepted_participants) + 1
     )

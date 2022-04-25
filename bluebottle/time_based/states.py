af0b4d2510c08dcca64e505e9b5431c97548c76f
Ernst Odolphi <ernst@goodup.com>
2022-04-25 15:59:48 +0200

    Do not allow a finish transition for period participants that are part of a team.

--- bluebottle/time_based/states.py
+++ bluebottle/time_based/states.py
@@ -420,6 +420,9 @@ class DateParticipantStateMachine(ParticipantStateMachine):
 
 @register(PeriodParticipant)
 class PeriodParticipantStateMachine(ParticipantStateMachine):
+    def is_not_team(self):
+        return not self.instance.team
+
     stopped = State(
         _('stopped'),
         'stopped',
@@ -433,7 +436,7 @@ class PeriodParticipantStateMachine(ParticipantStateMachine):
         permission=ParticipantStateMachine.can_accept_participant,
         description=_("Participant stopped contributing."),
         automatic=False,
-        conditions=[ParticipantStateMachine.activity_is_open]
+        conditions=[ParticipantStateMachine.activity_is_open, is_not_team]
     )
 
     start = Transition(

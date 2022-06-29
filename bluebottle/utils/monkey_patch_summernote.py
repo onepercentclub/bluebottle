0331e438de6f43b2377b030f78493a499b9f69f6
Ernst Odolphi <ernst@goodup.com>
2022-06-29 10:51:14 +0200

    Fix summernote

--- bluebottle/utils/monkey_patch_summernote.py
+++ bluebottle/utils/monkey_patch_summernote.py
@@ -2,11 +2,11 @@ from django_summernote.utils import SUMMERNOTE_THEME_FILES
 
 SUMMERNOTE_THEME_FILES['bs3'] = {
     'base_css': (
-        'bootstrap.min.css',
+        '/static/assets/summernote/bootstrap.min.css',
     ),
     'base_js': (
         '/static/assets/admin/js/vendor/jquery/jquery.min.js',
-        'summernote/bootstrap.min.js'
+        '/static/assets/summernote/bootstrap.min.js'
     ),
     'default_css': (
         'summernote/summernote.min.css',

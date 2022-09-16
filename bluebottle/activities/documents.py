395325d64b16a70b4b174d622f0e9731f10551c9
Loek van Gent <loek@goodup.com>
2022-09-16 09:24:03 +0200

    Don.t trip on missing country

--- bluebottle/activities/documents.py
+++ bluebottle/activities/documents.py
@@ -208,12 +208,18 @@ class ActivityDocument(Document):
                 'id': instance.office_location.pk,
                 'name': instance.office_location.name,
                 'locality': instance.office_location.city,
-                'country_code': instance.office_location.country.alpha2_code,
-                'country': instance.office_location.country.name,
+                'country_code': (
+                    instance.office_location.country.alpha2_code if
+                    instance.office_location.country else None
+                ),
+                'country': (
+                    instance.office_location.country.name if
+                    instance.office_location.country else None
+                ),
                 'type': 'office'
             })
-
         elif instance.initiative.location:
+
             locations.append({
                 'id': instance.initiative.location.pk,
                 'name': instance.initiative.location.name,

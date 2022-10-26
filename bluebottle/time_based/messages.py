ef389c826872a28d0a9e27e76aebe1bc457205dc
Ernst Odolphi <ernst@goodup.com>
2022-10-26 15:48:51 +0200

    Revert "Revert "Only sent time slot changed messages to participants of that slot""
    This reverts commit 9796e93f198343b6a317db22d2e3836a35b39333.

--- bluebottle/time_based/messages.py
+++ bluebottle/time_based/messages.py
@@ -267,7 +267,12 @@ class ChangedMultipleDateNotification(TimeBasedInfoMixin, TransitionMessage):
     def get_recipients(self):
         """participants that signed up"""
         return [
-            participant.user for participant in self.obj.activity.accepted_participants
+            slot_participant.participant.user for slot_participant
+            in self.obj.slot_participants.all()
+            if (
+                slot_participant.status == 'registered' and
+                slot_participant.participant.status == 'accepted'
+            )
         ]
 
 

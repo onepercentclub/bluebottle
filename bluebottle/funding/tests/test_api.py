e7f7b1030fedb8ce925cd395b2e3296e1c00b259
Loek van Gent <loek@goodup.com>
2022-11-01 14:43:19 +0100

    Add more tests

--- bluebottle/funding/tests/test_api.py
+++ bluebottle/funding/tests/test_api.py
@@ -13,7 +13,7 @@ from openpyxl import load_workbook
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
-from bluebottle.funding.models import Donor, FundingPlatformSettings
+from bluebottle.funding.models import Donor, FundingPlatformSettings, Funding
 from bluebottle.funding.tests.factories import (
     FundingFactory, RewardFactory, DonorFactory,
     BudgetLineFactory
@@ -676,7 +676,9 @@ class FundingTestCase(BluebottleTestCase):
         super(FundingTestCase, self).setUp()
         self.client = JSONAPITestClient()
         self.user = BlueBottleUserFactory()
-        self.initiative = InitiativeFactory.create(owner=self.user)
+        self.initiative = InitiativeFactory.create(owner=self.user, status='approved')
+
+        self.bank_account = PledgeBankAccountFactory.create(status='verified')
 
         self.create_url = reverse('funding-list')
 
@@ -685,22 +687,26 @@ class FundingTestCase(BluebottleTestCase):
                 'type': 'activities/fundings',
                 'attributes': {
                     'title': 'test',
+                    'description': 'Yeah',
+                    'target': {'currency': 'EUR', 'amount': 3500},
+                    'deadline': str(now() + timedelta(days=30))
                 },
                 'relationships': {
                     'initiative': {
                         'data': {
                             'type': 'initiatives',
                             'id': self.initiative.pk,
-                        }
-                    }
+                        },
+                    },
                 }
             }
         }
 
     def test_create(self):
         response = self.client.post(self.create_url, json.dumps(self.data), user=self.user)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
         self.assertTrue(
             data['data']['meta']['permissions']['PATCH']
         )
@@ -735,6 +741,40 @@ class FundingTestCase(BluebottleTestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_without_errors(self):
+        response = self.client.post(self.create_url, json.dumps(self.data), user=self.user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = response.json()
+        update_url = reverse('funding-detail', args=(data['data']['id'],))
+        response = self.client.put(update_url, data, user=self.user)
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        funding = Funding.objects.last()
+        funding.bank_account = self.bank_account
+        BudgetLineFactory.create_batch(2, activity=funding)
+        funding.save()
+
+        response = self.client.get(update_url, data, user=self.user)
+        data = response.json()
+
+        print(data['data']['meta']['errors'])
+        print(data['data']['meta']['required'])
+
+        self.assertEqual(
+            len(data['data']['meta']['errors']),
+            0
+        )
+        self.assertEqual(
+            len(data['data']['meta']['required']),
+            0
+        )
+        funding.states.submit(save=True)
+        data['data']['attributes'] = {
+            'deadline': now() + timedelta(days=80),
+        }
 
 
 class DonationTestCase(BluebottleTestCase):

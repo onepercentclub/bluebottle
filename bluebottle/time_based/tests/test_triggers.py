f3d22de707b7591358b96ded5cf77f91a3ddbab0
Loek van Gent <loek@goodup.com>
2022-12-15 09:07:27 +0100

    Fix test

--- bluebottle/time_based/tests/test_triggers.py
+++ bluebottle/time_based/tests/test_triggers.py
@@ -1025,10 +1025,11 @@ class ParticipantTriggerTestCase(object):
     def test_initiate_team_invite(self):
         self.activity.team_activity = Activity.TeamActivityChoices.teams
         self.activity.save()
+        captain = BlueBottleUserFactory.create()
 
         team_captain = self.participant_factory.create(
             activity=self.activity,
-            user=BlueBottleUserFactory.create()
+            user=captain
         )
 
         mail.outbox = []
@@ -1046,7 +1047,7 @@ class ParticipantTriggerTestCase(object):
             in [message.subject for message in mail.outbox]
         )
         self.assertTrue(
-            f'You have joined Team user_6 user_6 for "{self.activity.title}"'
+            f'You have joined Team {captain.first_name} {captain.last_name} for "{self.activity.title}"'
             in [message.subject for message in mail.outbox]
         )
 

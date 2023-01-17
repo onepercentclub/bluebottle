9b9c0ff0398bce3e899133e7a4e4bac0e19a3959
Loek van Gent <loek@goodup.com>
2023-01-17 11:29:24 +0100

    Custom message for pwd length error

---
+++ bluebottle/auth/password_validation.py
@@ -0,0 +1,19 @@
+from gettext import ngettext
+
+from django.contrib.auth.password_validation import MinimumLengthValidator
+from django.core.exceptions import ValidationError
+
+
+class CustomMinimumLengthValidator(MinimumLengthValidator):
+
+    def validate(self, password, user=None):
+        if len(password) < self.min_length:
+            raise ValidationError(
+                ngettext(
+                    "Password should be at least %(min_length)d character.",
+                    "Password should be at least %(min_length)d characters.",
+                    self.min_length
+                ),
+                code='password_too_short',
+                params={'min_length': self.min_length},
+            )

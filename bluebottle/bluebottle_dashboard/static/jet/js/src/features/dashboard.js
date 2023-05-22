7d2b38a2d1f24ce123d19986760d32871155897f
Ernst Odolphi <ernst@goodup.com>
2023-05-22 13:05:31 +0200

    Upgrade js files. Upgrade guplfile and other tooling

--- bluebottle/bluebottle_dashboard/static/jet/js/src/features/dashboard.js
+++ bluebottle/bluebottle_dashboard/static/jet/js/src/features/dashboard.js
@@ -3,6 +3,9 @@ require("./../utils/jquery-slidefade");
 var $ = require("jquery");
 var t = require("../utils/translate");
 
+require("jquery-ui/ui/version");
+require("jquery-ui/ui/data");
+require("jquery-ui/ui/scroll-parent");
 require("jquery-ui/ui/widget");
 require("jquery-ui/ui/widgets/mouse");
 require("jquery-ui/ui/widgets/draggable");

babf328d236d60d1f23006bbce44af6acd7b13d7
Ernst Odolphi <ernst@goodup.com>
2023-06-14 14:58:31 +0200

    Remove email as a second factor when authenticating

--- bluebottle/settings/base.py
+++ bluebottle/settings/base.py
@@ -310,10 +310,8 @@ TENANT_APPS = (
     'django_otp',
     'django_otp.plugins.otp_static',
     'django_otp.plugins.otp_totp',
-    'django_otp.plugins.otp_email',  # <- if you want email capability.
     'two_factor',
     'two_factor.plugins.phonenumber',  # <- if you want phone number capability.
-    'two_factor.plugins.email',  # <- if you want email capability.
 
 
     'django.contrib.contenttypes',

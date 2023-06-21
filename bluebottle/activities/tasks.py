b1549cec1e05fad868c2011e9059ff937587bd15
Ernst Odolphi <ernst@goodup.com>
2023-06-21 13:06:57 +0200

    Run recommendations every hour

--- bluebottle/activities/tasks.py
+++ bluebottle/activities/tasks.py
@@ -128,7 +128,8 @@ def get_matching_activities(user):
 
 
 @periodic_task(
-    run_every=(crontab(0, 0, day_of_month='2')),
+    # run_every=(crontab(0, 0, day_of_month='2')),
+    run_every=(crontab(minute=0)),
     name="recommend",
     ignore_result=True
 )

1b7e7ce8b17b7fff7b54fb4a124d9e4b7ba91fe9
Loek van Gent <loek@goodup.com>
2023-06-27 10:49:46 +0200

    Add comment

--- bluebottle/utils/monkey_patch_jet.py
+++ bluebottle/utils/monkey_patch_jet.py
@@ -48,5 +48,5 @@ ModelAdmin.formfield_for_manytomany = formfield_for_manytomany
 
 jet.utils.get_menu_items = get_menu_items
 
-
+# Change this to bust the cached JS/CSS builds
 jet.__version__ = jet.__version__ + 'goodup-1'

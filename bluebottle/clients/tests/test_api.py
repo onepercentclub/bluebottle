fcd0b8b17dab4e38b9b476f3fd5669459df90640
Ernst Odolphi <ernst@goodup.com>
2023-06-29 09:29:26 +0200

    remove setting from serializer and test

--- bluebottle/clients/tests/test_api.py
+++ bluebottle/clients/tests/test_api.py
@@ -234,14 +234,12 @@ class TestPlatformSettingsApi(BluebottleTestCase):
     def test_notification_platform_settings(self):
         # Create notification platform settings and confirm they end up correctly in settings api
         NotificationPlatformSettings.objects.create(
-            match_options=True,
             share_options=['twitter', 'facebook_at_work'],
             default_yammer_group_id='1234',
             facebook_at_work_url='https://my.facebook.com'
         )
 
         response = self.client.get(self.settings_url)
-        self.assertEqual(response.data['platform']['notifications']['match_options'], True)
         self.assertEqual(response.data['platform']['notifications']['share_options'], ['twitter', 'facebook_at_work'])
         self.assertEqual(response.data['platform']['notifications']['facebook_at_work_url'], 'https://my.facebook.com')
         self.assertEqual(response.data['platform']['notifications']['default_yammer_group_id'], '1234')

5936ead46ee983c520d8b9529e1335c47c0e455f
Ernst Odolphi <ernst@goodup.com>
2023-07-25 10:49:21 +0200

    Add inline updates to deeds admin. Fix update admin

--- bluebottle/updates/admin.py
+++ bluebottle/updates/admin.py
@@ -1,8 +1,25 @@
-
 from django.contrib import admin
+
+from django_admin_inline_paginator.admin import TabularInlinePaginated
 from bluebottle.updates.models import Update
 
 
 @admin.register(Update)
 class UpdateAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ['created', 'notify']
+
+    raw_id_fields = ['author', 'parent']
+    fields = ['created', 'author', 'parent', 'notify', 'image', 'message']
+
+
+class UpdateInline(TabularInlinePaginated):
+    model = Update
+    per_page = 8
+    extra = 0
+    readonly_fields = ['created', 'author', 'parent', 'notify', 'has_image', 'message']
+    fields = ['created', 'author', 'parent', 'notify', 'has_image', 'message']
+
+    show_change_link = True
+
+    def has_image(self, obj):
+        return obj.image is not None

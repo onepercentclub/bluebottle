e23cd06c94f482b99db85dc4d2723b6547204591
Ernst Odolphi <ernst@goodup.com>
2023-09-29 13:00:23 +0200

    Allow start and end date to be empty

--- bluebottle/deeds/serializers.py
+++ bluebottle/deeds/serializers.py
@@ -24,14 +24,15 @@ class StartDateValidator():
     def __call__(self, value, serializer):
         parent = serializer.parent
         try:
+
             end = dateutil.parser.parse(parent.initial_data['end'])
-        except KeyError:
+        except (KeyError, TypeError):
             try:
                 end = parent.instance.end
             except AttributeError:
                 return
 
-        if value and value > end.date():
+        if value and end and value > end.date():
             raise ValidationError('The activity should start before it ends')
 
 

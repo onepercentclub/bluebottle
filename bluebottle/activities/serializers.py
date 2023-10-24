3efe2c7bb6be345714c55f54a8cb6d1648f96df0
Ernst Odolphi <ernst@goodup.com>
2023-10-24 15:25:43 +0200

    Fix upcoming slots filter in serializer

--- bluebottle/activities/serializers.py
+++ bluebottle/activities/serializers.py
@@ -107,7 +107,7 @@ class ActivityPreviewSerializer(ModelSerializer):
 
     def get_start(self, obj):
         if hasattr(obj, 'slots') and obj.slots:
-            upcoming = self.context['request'].GET.get('filter[upcoming]') == '1'
+            upcoming = obj.status in ('open', 'full')
             slots = self.get_filtered_slots(obj, only_upcoming=upcoming)
             if slots:
                 return slots[0].start
@@ -117,7 +117,7 @@ class ActivityPreviewSerializer(ModelSerializer):
 
     def get_end(self, obj):
         if hasattr(obj, 'slots') and obj.slots:
-            upcoming = self.context['request'].GET.get('filter[upcoming]') == '1'
+            upcoming = obj.status in ('open', 'full')
 
             tz = get_current_timezone()
             try:

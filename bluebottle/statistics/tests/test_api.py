ba041f9395c17c17a541a0bd8a51de1ac2623891
Loek van Gent <loek@goodup.com>
2023-11-03 15:34:47 +0100

    Fix test

--- bluebottle/statistics/tests/test_api.py
+++ bluebottle/statistics/tests/test_api.py
@@ -82,7 +82,7 @@ class StatisticListListAPITestCase(BluebottleTestCase):
             if resource['id'] == str(self.impact.pk):
                 self.assertEqual(resource['type'], 'statistics')
                 self.assertEqual(resource['attributes']['value'], self.impact_goal.realized)
-                self.assertEqual(resource['attributes']['name'], self.impact_type.name)
+                self.assertEqual(resource['attributes']['name'], self.impact_type.text_passed)
                 self.assertEqual(resource['attributes']['icon'], self.impact_type.icon)
 
             if resource['id'] == str(self.database.pk):

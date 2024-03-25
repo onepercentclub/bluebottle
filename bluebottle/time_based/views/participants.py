8e7271fdbb0af67de140726e42c3ebb53d965970
Ernst Odolphi <ernst@goodup.com>
2024-03-25 14:54:05 +0100

    Do not include registrations in participants. This might leak information that should not be available to users that can see participants.

--- bluebottle/time_based/views/participants.py
+++ bluebottle/time_based/views/participants.py
@@ -103,6 +103,7 @@ class RelatedParticipantListView(
         OneOf(ResourcePermission, ResourceOwnerPermission),
     )
 
+
     def get_queryset(self):
         queryset = super().get_queryset()
 

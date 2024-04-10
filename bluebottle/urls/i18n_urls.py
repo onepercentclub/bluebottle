1c7dea9045e054d956869cc6c4e52a55d135d846
Loek van Gent <loek@goodup.com>
2024-04-10 11:15:47 +0200

    Update i18n_urls.py

--- bluebottle/urls/i18n_urls.py
+++ bluebottle/urls/i18n_urls.py
@@ -9,7 +9,7 @@ from bluebottle.auth.views import admin_password_reset, admin_logout
 from bluebottle.bluebottle_dashboard.views import locked_out
 from bluebottle.looker.dashboard_views import LookerEmbedView  # noqa This has to be imported early so that custom urls will work
 
-from two_factor.urls import urlpatterns as tf_urls  # noqa This has to be imported early so that custom urls will work
+from two_factor.urls import urlpatterns as tf_urls
 
 
 admin.autodiscover()

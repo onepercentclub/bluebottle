91673a13c24ead5b9fd2fe546db51d52f642f45e
Loek van Gent <loek@goodup.com>
2024-05-15 14:40:45 +0200

    Ad option to fix statuses

--- scripts/incorrect_contributions.py
+++ scripts/incorrect_contributions.py
@@ -1,5 +1,5 @@
 
-from django.db.models import Count, Q
+from django.db.models import Q
 
 from bluebottle.clients.models import Client
 from bluebottle.clients.utils import LocalTenant
@@ -7,18 +7,19 @@ from bluebottle.time_based.models import TimeContribution
 
 
 def run(*args):
+    fix = 'fix' in args
+    total_errors = False
     for client in Client.objects.all():
         with (LocalTenant(client)):
-            failed_contributions = TimeContribution.objects.filter(
+            succeeded_contributions = TimeContribution.objects.filter(
                 status='succeeded',
                 slot_participant_id__isnull=False
             ).exclude(
                 Q(slot_participant__status__in=('registered', )) &
                 Q(contributor__status__in=('accepted', 'new', )) &
                 Q(contributor__activity__status__in=('open', 'succeeded', 'full'))
-
             )
-            succeeded_contributions = TimeContribution.objects.filter(
+            failed_contributions = TimeContribution.objects.filter(
                 status='failed',
                 slot_participant_id__isnull=False,
                 contributor__status__in=('accepted', 'new',),
@@ -26,7 +27,19 @@ def run(*args):
                 contributor__activity__status__in=('open', 'succeeded', 'full',)
             )
 
-            if failed_contributions.count() or succeeded_contributions.count():
-                print(f'{client.name}:\nfailed but should be succeeded: {succeeded_contributions.count()}\nsucceeded but should be failed: {failed_contributions.count()}\n\n')
+            errors = failed_contributions.count() or succeeded_contributions.count()
+            if errors:
+                total_errors = True
+                print("### Tenant {}:".format(client.name))
+                print(f'failed but should be succeeded: {failed_contributions.count()}')
+                print(f'succeeded but should be failed: {succeeded_contributions.count()}')
+                print('\n')
+                if fix:
+                    succeeded_contributions.update(status='failed')
+                    failed_contributions.update(status='succeeded')
 
+    if not fix and total_errors:
+        print("☝️ Add '--script-args=fix' to the command to actually fix the activities.")
 
+    if not total_errors:
+        print("No errors found! 🎉🎉🎉")

e51bf696572882f30476f65bad2cef794afb9032
Loek van Gent <loek@goodup.com>
2024-05-21 13:11:59 +0200

    only activie team members

--- bluebottle/time_based/effects/slots.py
+++ bluebottle/time_based/effects/slots.py
@@ -8,7 +8,7 @@ class CreateTeamSlotParticipantsEffect(Effect):
     template = 'admin/create_team_slot_participants.html'
 
     def post_save(self, **kwargs):
-        for team_member in self.instance.team.team_members.all():
+        for team_member in self.instance.team.team_members.filter(status__in=['new', 'accepted']).all():
             slot = self.instance
             slot.participants.get_or_create(
                 user=team_member.user,

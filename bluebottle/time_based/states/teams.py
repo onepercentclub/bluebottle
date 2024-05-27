dc780f519c17340f94df754c2379fc5816a075ec
Loek van Gent <loek@goodup.com>
2024-05-27 14:58:29 +0200

    More admin for managing teams

--- bluebottle/time_based/states/teams.py
+++ bluebottle/time_based/states/teams.py
@@ -24,6 +24,12 @@ class TeamStateMachine(ModelStateMachine):
         _("This team has been scheduled.")
     )
 
+    cancelled = State(
+        _('cancelled'),
+        'cancelled',
+        _("This team has been cancelled.")
+    )
+
     initiate = Transition(
         EmptyState(),
         new,
@@ -39,6 +45,25 @@ class TeamStateMachine(ModelStateMachine):
         name=_('Accept'),
         description=_(
             'The team has been accepted.'
+        ),
+    )
+    cancel = Transition(
+        [new, accepted, scheduled],
+        cancelled,
+        name=_('Cancel'),
+        automatic=False,
+        description=_(
+            'The team has been cancelled.'
+        ),
+    )
+
+    restore = Transition(
+        cancelled,
+        new,
+        name=_('Restore'),
+        automatic=False,
+        description=_(
+            'The team has been restored.'
         ),
     )
 

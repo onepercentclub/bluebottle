25a4be35d0237528344bbecb5dcc41e6de756e6b
Loek van Gent <loek@goodup.com>
2024-06-19 08:30:59 +0200

    More

--- bluebottle/funding_stripe/utils.py
+++ bluebottle/funding_stripe/utils.py
@@ -8,11 +8,12 @@ def get_stripe_settings():
     provider = StripePaymentProvider.objects.first()
     if not provider:
         raise ImproperlyConfigured('Stripe not enabled for this tenant')
+
+    api_key = provider.stripe_secret or settings.STRIPE['api_key'],
+    stripe.api_key = api_key
+    stripe.api_version = '2019-09-09'
+    stripe.webhook_secret_sources = api_key
+    stripe.webhook_secret_intents = api_key
+    stripe.webhook_secret_connect = api_key
+
     return provider.public_settings
-
-
-stripe.api_key = settings.STRIPE['api_key']
-stripe.api_version = '2019-09-09'
-stripe.webhook_secret_sources = settings.STRIPE['webhook_secret_sources']
-stripe.webhook_secret_intents = settings.STRIPE['webhook_secret_intents']
-stripe.webhook_secret_connect = settings.STRIPE['webhook_secret_connect']

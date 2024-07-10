8636bfe589052ecb43935b0baebd8b3862139e57
Loek van Gent <loek@goodup.com>
2024-07-10 15:42:03 +0200

    Add test for reopen

--- bluebottle/time_based/tests/test_registration_triggers.py
+++ bluebottle/time_based/tests/test_registration_triggers.py
@@ -126,6 +126,12 @@ class RegistrationTriggerTestCase:
             ),
         )
 
+    def test_open_after_remove(self):
+        self.test_fill_accept()
+        self.registration.participants.first().states.remove(save=True)
+        self.activity.refresh_from_db()
+        self.assertEqual(self.activity.status, "open")
+
 
 class DeadlineRegistrationTriggerTestCase(
     RegistrationTriggerTestCase, BluebottleTestCase

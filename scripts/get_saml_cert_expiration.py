de49fe852c74d1fe2a643604792882d782295be4
Ernst Odolphi <ernst@goodup.com>
2024-07-18 09:51:09 +0200

    Fix flake8

--- scripts/get_saml_cert_expiration.py
+++ scripts/get_saml_cert_expiration.py
@@ -29,7 +29,7 @@ def run(*args):
 
                 cert = x509.load_pem_x509_certificate(bytes(cert_string), default_backend())
                 print('Their cert', client.name, cert.not_valid_after)
-            except (AttributeError, KeyError) as e:
+            except (AttributeError, KeyError):
                 pass
             except Exception as e:
                 print(e)

64f12557b1e35e074c5cc81518119cc32e1ef04c
Loek van Gent <loek@goodup.com>
2024-09-17 13:14:54 +0200

    Fix

--- bluebottle/funding_stripe/triggers.py
+++ bluebottle/funding_stripe/triggers.py
@@ -13,6 +13,7 @@ from bluebottle.funding.messages import (
 from bluebottle.funding.models import Funding
 from bluebottle.funding.states import DonorStateMachine, PayoutAccountStateMachine
 from bluebottle.funding.triggers import BasePaymentTriggers
+from bluebottle.funding_stripe.effects import PutActivitiesOnHoldEffect
 from bluebottle.funding_stripe.models import (
     StripeSourcePayment,
     StripePayoutAccount,

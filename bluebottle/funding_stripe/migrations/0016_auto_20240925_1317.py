ab28b694acaeb2fb4c2e930a274329fdb860e480
Ernst Odolphi <ernst@goodup.com>
2024-10-03 10:42:30 +0200

    Make tos acceptance true during migration

--- bluebottle/funding_stripe/migrations/0016_auto_20240925_1317.py
+++ bluebottle/funding_stripe/migrations/0016_auto_20240925_1317.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="stripepayoutaccount",
             name="tos_accepted",
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=True),
         ),
         migrations.AlterField(
             model_name="stripepayoutaccount",

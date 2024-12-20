046f686c958c2affbb74c1fcb1ccf88287cc734d
Loek van Gent <loek@goodup.com>
2024-12-20 13:11:10 +0100

    Tweak

--- bluebottle/activities/admin.py
+++ bluebottle/activities/admin.py
@@ -102,6 +102,9 @@ class BaseContributorInline(TabularInlinePaginated):
     per_page = 10
     ordering = ['-created']
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user')
+
     template = 'admin/participant_list.html'
 
     can_delete = True

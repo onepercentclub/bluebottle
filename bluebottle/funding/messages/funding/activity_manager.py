4395b1e2917ed8dd62fe9ce2f17c3b16e8998fdb
Loek van Gent <loek@goodup.com>
2025-07-14 16:13:25 +0200

    Fix payout account emails

--- bluebottle/funding/messages/funding/activity_manager.py
+++ bluebottle/funding/messages/funding/activity_manager.py
@@ -84,31 +84,48 @@ class FundingCancelledMessage(FundingActivityManagerMessage):
     template = 'messages/funding/activity_manager/funding_cancelled'
 
 
-class FundingPayoutAccountRejected(FundingActivityManagerMessage):
+class BaseFundingPayoutAccountMessage(FundingActivityManagerMessage):
+
+    @property
+    def activity(self):
+        return self.obj.funding.last()
+
+    @property
+    def action_link(self):
+        return self.activity.get_absolute_url()
+
+    def get_recipients(self):
+        """the activity organizer"""
+        if self.activity:
+            return [self.activity.owner]
+        return []
+
+
+class FundingPayoutAccountRejected(BaseFundingPayoutAccountMessage):
     subject = _(u'Action required for your crowdfunding campaign')
     template = 'messages/funding/activity_manager/payout_account_rejected'
 
 
-class FundingPayoutAccountMarkedIncomplete(FundingActivityManagerMessage):
+class FundingPayoutAccountMarkedIncomplete(BaseFundingPayoutAccountMessage):
     subject = _("Action required for your crowdfunding campaign")
     template = "messages/funding/activity_manager/payout_account_marked_incomplete"
 
 
-class FundingPayoutAccountVerified(FundingActivityManagerMessage):
+class FundingPayoutAccountVerified(BaseFundingPayoutAccountMessage):
     subject = _(u'Your identity has been verified')
     template = 'messages/funding/activity_manager/payout_account_verified'
 
 
-class FundingPublicPayoutAccountRejected(FundingPayoutAccountRejected):
+class FundingPublicPayoutAccountRejected(BaseFundingPayoutAccountMessage):
     subject = _(u'Action required for your identity verification')
     template = 'messages/funding/activity_manager/public_payout_account_rejected'
 
 
-class FundingPublicPayoutAccountMarkedIncomplete(FundingPayoutAccountMarkedIncomplete):
+class FundingPublicPayoutAccountMarkedIncomplete(BaseFundingPayoutAccountMessage):
     subject = _("Action required for identity verification")
     template = "messages/funding/activity_manager/public_payout_account_marked_incomplete"
 
 
-class FundingPublicPayoutAccountVerified(TransitionMessage):
+class FundingPublicPayoutAccountVerified(BaseFundingPayoutAccountMessage):
     subject = _(u'Your identity has been verified')
     template = 'messages/funding/activity_manager/public_payout_account_verified'

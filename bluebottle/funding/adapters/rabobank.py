4deaad0684ff8303dfe2dd1aad5cd72eb3ab4b9f
Loek van Gent <loek@goodup.com>
2025-07-17 12:44:35 +0200

    Fix rabo api

--- bluebottle/funding/adapters/rabobank.py
+++ bluebottle/funding/adapters/rabobank.py
@@ -8,12 +8,8 @@ class RabobankAdapter:
 
         self.public_cert = settings.RABOBANK["public_cert"]
         self.private_key = settings.RABOBANK["private_key"]
-        self.cert_pass = settings.RABOBANK["cert_pass"]
-        self.token_url = settings.RABOBANK["token_url"]
         self.client_id = settings.RABOBANK["client_id"]
         self.client_secret = settings.RABOBANK["client_secret"]
-        self.api_key = settings.RABOBANK.get("api_key")  # Add API key
-
         self.iban_check_url = settings.RABOBANK["iban_check_url"]
         self.scope = 'surepay:ibannamecheck:read'
 
@@ -23,10 +19,6 @@ class RabobankAdapter:
             "X-IBM-Client-Id": self.client_id,
             "Content-Type": "application/json",
         }
-
-        # Add API key if available
-        if self.api_key:
-            headers["API-Key"] = self.api_key
 
         payload = {"accountId": {"value": iban, "type": "IBAN"}, "name": name}
         response = requests.post(

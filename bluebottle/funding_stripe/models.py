7c81b5756b62462a929a9fb70958fe8e78595ed0
Loek van Gent <loek@goodup.com>
2025-08-13 08:46:42 +0200

    Prefill dome info for KYC

--- bluebottle/funding_stripe/models.py
+++ bluebottle/funding_stripe/models.py
@@ -398,6 +398,21 @@ class StripePayoutAccount(PayoutAccount):
             "member_id": self.owner.pk,
         }
 
+    def prefill_business_profile(self):
+        business_profile = self.account.business_profile
+        if self.account_id and self.business_type:
+
+            if not business_profile.mcc and self.business_type != BusinessTypeChoices.company:
+                business_profile.mcc = "8398"  # Default MCC for non-profits and crowd-funding
+            if not business_profile.product_description:
+                business_profile.product_description = "Not applicable - raising funds for a do-good project"
+
+            stripe = get_stripe()
+            stripe.Account.modify(
+                self.account_id,
+                business_profile=business_profile,
+            )
+
     def save(self, *args, **kwargs):
         stripe = get_stripe()
 
@@ -410,29 +425,20 @@ class StripePayoutAccount(PayoutAccount):
             self.verification_method = VerificationMethodChoices.personal
 
         if self.country and not self.account_id:
-            if Funding.objects.filter(owner=self.owner).count():
-                url = (
-                    Funding.objects.filter(owner=self.owner).first().get_absolute_url()
-                )
-            else:
-                url = "https://{}".format(connection.tenant.domain_url)
-
-            if "localhost" in url:
-                url = "https://goodup.com"
-
             account = stripe.Account.create(
                 country=self.country,
                 type="custom",
                 settings=self.account_settings,
                 business_type=self.business_type,
                 capabilities=self.capabilities,
-                business_profile={"url": url, "mcc": "8398"},
                 metadata=self.metadata,
                 tos_acceptance={'service_agreement': self.service_agreement},
             )
 
             self.account_id = account.id
             self.update(account)
+
+        self.prefill_business_profile()
 
         super().save(*args, **kwargs)
 

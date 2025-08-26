864b93e4a6c412598565c24762a96268e21214e7
Loek van Gent <loek@goodup.com>
2025-08-26 16:21:50 +0200

    Fix test

--- bluebottle/notifications/tests/test_admin.py
+++ bluebottle/notifications/tests/test_admin.py
@@ -14,16 +14,15 @@ class TestMessageTemplateAdmin(BluebottleAdminTestCase):
     def test_new_mail_template_admin(self):
         self.admin_url = reverse('admin:notifications_messagetemplate_add')
         response = self.client.get(self.admin_url)
-        self.assertNotContains(response, 'Body')
         self.assertNotContains(response, '{first_name}')
 
     def test_mail_template_admin(self):
         template = MessageTemplate.objects.create(
-            message='bluebottle.members.messages.AccoutnActivationMessage'
+            message='bluebottle.members.messages.AccountActivationMessage'
         )
         self.admin_url = reverse('admin:notifications_messagetemplate_change', args=(template.id,))
         response = self.client.get(self.admin_url)
+
         # Check we show placeholder hints
         self.assertContains(response, '{first_name}')
         self.assertContains(response, '{site_name}')
-        self.assertContains(response, 'Body')

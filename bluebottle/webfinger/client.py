c5048bb51edb8de9ec8b3abe88abc173a8c922c3
Ernst Odolphi <ernst@goodup.com>
2025-09-18 10:33:34 +0200

    Add file

---
+++ bluebottle/webfinger/client.py
@@ -0,0 +1,27 @@
+from urllib.parse import urlparse, urlencode
+
+import requests
+
+
+class WebFingerClient:
+    def _do_request(self, url):
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        return response.json()
+
+    def get(self, uri, type='application/activity+josn'):
+        parsed = urlparse(uri)
+
+        params = urlencode({'resource': uri})
+        response = self._do_request(
+            f'{parsed.scheme}://{parsed.netloc}/.well-known/webfinger?{params}'
+        )
+
+        for link in response['links']:
+            if link['type'] == 'application/activity+json' and link['rel'] == 'self':
+                return link['href']
+
+
+client = WebFingerClient()

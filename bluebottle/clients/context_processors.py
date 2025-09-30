fabb4bd5d3946493b7d6700ce3acfd3d10c9d80a
Ernst Odolphi <ernst@goodup.com>
2025-09-30 11:38:08 +0200

    Fix template check

--- bluebottle/clients/context_processors.py
+++ bluebottle/clients/context_processors.py
@@ -5,6 +5,8 @@ from django.db import connection
 from bluebottle.clients import properties
 from bluebottle.looker.models import LookerEmbed
 
+from django.conf import settings
+
 
 def tenant(request):
 
@@ -19,5 +21,6 @@ def tenant(request):
         context['looker_items'] = LookerEmbed.objects.all()
 
     context['client_ip'] = get_client_ip(request)
+    context['VPN_CLIENT_IP'] = getattr(settings, 'VPN_CLIENT_IP', None)
 
     return context

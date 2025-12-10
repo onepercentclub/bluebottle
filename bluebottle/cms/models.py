6b1bb76caeefa24c01b9ce5b13fc27f1a2c8cbb8
gannetson <loek@goodup.com>
2025-12-10 12:22:27 +0100

    Fix

--- bluebottle/cms/models.py
+++ bluebottle/cms/models.py
@@ -697,7 +697,7 @@ class SitePlatformSettings(TranslatableModel, BasePlatformSettings):
 
     @property
     def is_sharing_activities(self):
-        return not self.is_publishing_activities and not self.is_receiving_activities
+        return self.is_publishing_activities or self.is_receiving_activities
 
     @property
     def is_publishing_activities(self):

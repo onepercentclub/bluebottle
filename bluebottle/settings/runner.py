233d67f352ef4930ed36c36af21233a89ab7214b
gannetson <loek@goodup.com>
2026-01-14 14:41:19 +0100

    Fix db user

--- bluebottle/settings/runner.py
+++ bluebottle/settings/runner.py
@@ -6,7 +6,7 @@ DATABASES = {
         "HOST": "postgres",
         "PORT": "5432",
         "NAME": "bluebottle_test",
-        "USER": "postgres",
+        "USER": "testuser",
         "PASSWORD": "password",
         "DISABLE_SERVER_SIDE_CURSORS": True,
         "MIGRATE": False,

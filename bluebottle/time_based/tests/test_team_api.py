9ac545328bb437aa41a5905b00d689e8d52fb081
gannetson <loek@goodup.com>
2026-02-26 15:54:02 +0100

    Fix teammember add and test

--- bluebottle/time_based/tests/test_team_api.py
+++ bluebottle/time_based/tests/test_team_api.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from bluebottle.test.factory_models.accounts import BlueBottleUserFactory
 from bluebottle.test.utils import APITestCase
 from bluebottle.time_based.models import Team
-from bluebottle.time_based.serializers.teams import TeamSerializer
+from bluebottle.time_based.serializers.teams import TeamSerializer, TeamMemberSerializer
 from bluebottle.time_based.tests.factories import ScheduleActivityFactory, TeamFactory
 
 
@@ -39,3 +39,45 @@ class TeamDetailAPIViewTestCase(APITestCase):
     def test_user_captain_email(self):
         self.perform_get(self.user)
         self.assertAttribute('captain-email', None)
+
+
+class TeamMemberListAPIViewTestCase(APITestCase):
+    serializer = TeamMemberSerializer
+
+    def setUp(self):
+        super().setUp()
+        self.captain = BlueBottleUserFactory.create()
+        self.existing_member = BlueBottleUserFactory.create(email='existing.member@example.com')
+        self.activity = ScheduleActivityFactory.create(
+            team_activity='teams',
+            owner=self.captain
+        )
+        self.team = TeamFactory.create(
+            user=self.captain,
+            activity=self.activity
+        )
+        self.url = reverse('team-member-list')
+
+    def test_add_team_member_by_existing_email(self):
+        data = {
+            'data': {
+                'type': 'contributors/time-based/team-members',
+                'attributes': {
+                    'email': self.existing_member.email
+                },
+                'relationships': {
+                    'team': {
+                        'data': {
+                            'id': str(self.team.pk),
+                            'type': 'contributors/time-based/teams'
+                        }
+                    }
+                }
+            }
+        }
+
+        self.perform_create(user=self.captain, data=data)
+        self.assertStatus(201)
+        self.model.refresh_from_db()
+        self.assertEqual(self.model.user, self.existing_member)
+        self.assertEqual(self.model.team, self.team)

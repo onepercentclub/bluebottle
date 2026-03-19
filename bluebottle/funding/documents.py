67925e8f34189001abe1780e5eb8fa6c1d9137f8
gannetson <loek@goodup.com>
2026-03-19 13:02:23 +0100

    Add Funding to distance filter

--- bluebottle/funding/documents.py
+++ bluebottle/funding/documents.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from bluebottle.initiatives.documents import deduplicate, get_translated_list
+
 from django_elasticsearch_dsl import fields
 from django_elasticsearch_dsl.registries import registry
 
 from bluebottle.activities.documents import ActivityDocument, activity
 from bluebottle.funding.models import Funding, Donor
+from bluebottle.initiatives.documents import deduplicate, get_translated_list
 
 SCORE_MAP = {
     'open': 1,
@@ -41,6 +42,32 @@ class FundingDocument(ActivityDocument):
         if isinstance(related_instance, Donor):
             return Funding.objects.filter(contributors=related_instance)
 
+    def prepare_location(self, instance):
+        locations = []
+        if hasattr(instance, 'impact_location') and instance.impact_location:
+            locations.append({
+                'id': instance.impact_location.id,
+                'name': instance.impact_location.formatted_address,
+                'locality': instance.impact_location.locality,
+                'country_code': instance.impact_location.country.alpha2_code,
+                'country': instance.impact_location.country.name,
+                'type': 'location'
+            })
+        elif instance.initiative and instance.initiative.place:
+            if instance.initiative.place.country:
+                locations.append({
+                    'locality': instance.initiative.place.locality,
+                    'country_code': instance.initiative.place.country.alpha2_code,
+                    'country': instance.initiative.place.country.name,
+                    'type': 'impact_location'
+                })
+            else:
+                locations.append({
+                    'locality': instance.initiative.place.locality,
+                    'type': 'impact_location'
+                })
+        return locations
+
     def prepare_country(self, instance):
         countries = super().prepare_country(instance)
         if instance.impact_location and instance.impact_location.country:
@@ -76,4 +103,8 @@ class FundingDocument(ActivityDocument):
         return self.prepare_amount(instance.amount_raised)
 
     def prepare_is_online(self, instance):
+        if instance.impact_location:
+            return False
+        if instance.initiative and instance.initiative.place:
+            return False
         return True

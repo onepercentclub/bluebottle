94194d1a1b5f2453e580ba458b8e33135ec3288b
gannetson <loek@goodup.com>
2026-03-25 16:16:45 +0100

    Fix tests

--- bluebottle/activity_pub/effects.py
+++ bluebottle/activity_pub/effects.py
@@ -180,6 +180,10 @@ class SendJoinEffect(Effect):
         import uuid
         from bluebottle.deeds.models import DeedParticipant
 
+        actor = get_platform_actor()
+        if actor is None:
+            return
+
         contributor = self.instance
         if not isinstance(contributor, DeedParticipant):
             return
@@ -208,7 +212,7 @@ class SendJoinEffect(Effect):
         participant_email = contributor.email_or_user or None
 
         join_activity = Join.objects.create(
-            actor=get_platform_actor(),
+            actor=actor,
             object=deed.origin,
             participant_sync_id=contributor.sync_id,
             participant_name=participant_name,
@@ -221,10 +225,7 @@ class SendJoinEffect(Effect):
 
     @property
     def is_valid(self):
-        return (
-            super().is_valid and
-            get_platform_actor() is not None
-        )
+        return super().is_valid
 
     def __str__(self):
         return str(_('Notify source platform of join'))
@@ -243,6 +244,10 @@ class SendLeaveEffect(Effect):
     def post_save(self, **kwargs):
         from bluebottle.deeds.models import DeedParticipant
 
+        actor = get_platform_actor()
+        if actor is None:
+            return
+
         contributor = self.instance
         if not isinstance(contributor, DeedParticipant):
             return
@@ -252,7 +257,7 @@ class SendLeaveEffect(Effect):
             return
 
         leave_activity = Leave.objects.create(
-            actor=get_platform_actor(),
+            actor=actor,
             object=target_event,
             participant_sync_id=contributor.sync_id or None,
         )
@@ -268,10 +273,7 @@ class SendLeaveEffect(Effect):
 
     @property
     def is_valid(self):
-        return (
-            super().is_valid and
-            get_platform_actor() is not None
-        )
+        return super().is_valid
 
     def __str__(self):
         return str(_('Notify source platform of leave'))

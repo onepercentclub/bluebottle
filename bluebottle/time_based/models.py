56de12026613e06e4f9ea9ba0de66a1ac8ff07ab
Ernst Odolphi <ernst@goodup.com>
2026-05-04 09:44:29 +0200

    More small fixes

--- bluebottle/time_based/models.py
+++ bluebottle/time_based/models.py
@@ -1,5 +1,6 @@
 import uuid
 from urllib.parse import urlencode
+import datetime
 
 import pytz
 from django.core.validators import MaxValueValidator
@@ -376,7 +377,7 @@ class ActivitySlot(TriggerMixin, ValidatedModelMixin, models.Model):
     def google_calendar_link(self):
         def format_date(date):
             if date:
-                return date.astimezone(timezone.utc).strftime('%Y%m%dT%H%M%SZ')
+                return date.astimezone(datetime.timezone.utc).strftime('%Y%m%dT%H%M%SZ')
 
         details = self.activity.details
         if self.is_online and self.online_meeting_url:
@@ -1918,7 +1919,7 @@ class Slot(models.Model):
     def google_calendar_link(self):
         def format_date(date):
             if date:
-                return date.astimezone(timezone.utc).strftime("%Y%m%dT%H%M%SZ")
+                return date.astimezone(datetime.timezone.utc).strftime("%Y%m%dT%H%M%SZ")
 
         details = self.details
         if self.is_online and self.online_meeting_url:

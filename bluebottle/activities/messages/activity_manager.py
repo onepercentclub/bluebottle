747eb04c7e6a31b669603822d65268b966609f77
gannetson <loek@goodup.com>
2026-05-22 13:42:00 +0200

    Simpl

--- bluebottle/activities/messages/activity_manager.py
+++ bluebottle/activities/messages/activity_manager.py
@@ -172,8 +172,7 @@ class ContactActivityManagerNotification(TransitionMessage):
         context = super().get_context(recipient)
         sender = self.obj.sender
         context.update({
-            'sender_first_name': sender.first_name,
-            'sender_name': sender.full_name,
+            'sender_name': sender.first_name,
             'title': self.obj.activity.title,
             'recipient_name': recipient.first_name,
         })

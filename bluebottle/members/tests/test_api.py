bbf8fe6e9bc688dc38a91485d8d2d4c89e1a4ae7
Henneke <nickyhenneke+github@gmail.com>
2026-07-31 13:52:01 +0200

    add test to check reminder quarterly settings are exposed

--- bluebottle/members/tests/test_api.py
+++ bluebottle/members/tests/test_api.py
@@ -1332,6 +1332,32 @@ class MemberSettingsAPITestCase(BluebottleTestCase):
         response = self.client.get(self.url, token=self.user_token)
         self.assertEqual(response.json()['platform']['members']['create_initiatives'], True)
 
+    def test_reminder_emails(self):
+        settings = MemberPlatformSettings.load()
+        settings.reminder_q1 = True
+        settings.reminder_q2 = False
+        settings.reminder_q3 = True
+        settings.reminder_q4 = False
+        settings.save()
+        response = self.client.get(self.url, token=self.user_token)
+        members = response.json()['platform']['members']
+        self.assertEqual(members['reminder_q1'], True)
+        self.assertEqual(members['reminder_q2'], False)
+        self.assertEqual(members['reminder_q3'], True)
+        self.assertEqual(members['reminder_q4'], False)
+
+        settings.reminder_q1 = False
+        settings.reminder_q2 = False
+        settings.reminder_q3 = False
+        settings.reminder_q4 = False
+        settings.save()
+        response = self.client.get(self.url, token=self.user_token)
+        members = response.json()['platform']['members']
+        self.assertEqual(members['reminder_q1'], False)
+        self.assertEqual(members['reminder_q2'], False)
+        self.assertEqual(members['reminder_q3'], False)
+        self.assertEqual(members['reminder_q4'], False)
+
 
 class CurrentMemberAPITestCase(APITestCase):
 

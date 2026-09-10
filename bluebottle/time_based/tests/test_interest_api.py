36c635f03a2f56dc62630237dd1285d1b9d892ae
Henneke <nickyhenneke+github@gmail.com>
2026-09-10 15:59:41 +0200

    fix bug where recurring activity participant could not sign up to future slot waiting list

--- bluebottle/time_based/tests/test_interest_api.py
+++ bluebottle/time_based/tests/test_interest_api.py
@@ -1,9 +1,9 @@
 from datetime import date, timedelta
-from unittest import expectedFailure
 
 from django.contrib.auth.models import Group
 from django.core import mail
-from django.utils.timezone import now, timezone
+from django.utils import timezone
+from django.utils.timezone import now
 from django.urls import reverse
 from rest_framework import status
 
@@ -249,10 +249,8 @@ class InterestListAPITestCase(APITestCase):
 class PeriodicPreviousSlotInterestAPITestCase(APITestCase):
     """
     Users who completed a previous recurring slot should be able to join the
-    waiting list when the activity is full again.
-
-    The tests below document the current bug: succeeded slot participations
-    still count as "already involved" even after the registration was stopped.
+    waiting list when the activity is full again, as long as they no longer
+    have an active registration.
     """
 
     url_name = 'interest-list'
@@ -300,32 +298,8 @@ class PeriodicPreviousSlotInterestAPITestCase(APITestCase):
         self.activity.status = 'full'
         self.activity.save()
 
-    def test_succeeded_previous_slot_participation_blocks_waiting_list(self):
+    def test_former_slot_participant_can_join_waiting_list(self):
         user, registration, participant = self._create_former_slot_participant()
-        self._fill_activity()
-
-        self.perform_create(user=user)
-
-        self.assertStatus(status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            self.response.json()['errors'][0]['code'],
-            'already_involved',
-        )
-        self.assertFalse(
-            Interest.objects.filter(user=user, activity=self.activity).exists()
-        )
-        self.assertEqual(registration.status, 'stopped')
-        self.assertEqual(participant.status, 'succeeded')
-
-    @expectedFailure
-    def test_former_slot_participant_should_be_able_to_join_waiting_list(self):
-        """
-        Desired behaviour once the validator ignores past slot participations.
-
-        Marked expectedFailure while succeeded PeriodicParticipant records are
-        still treated as active involvement.
-        """
-        user, _registration, _participant = self._create_former_slot_participant()
         self._fill_activity()
 
         self.perform_create(user=user)
@@ -333,6 +307,8 @@ class PeriodicPreviousSlotInterestAPITestCase(APITestCase):
         self.assertStatus(status.HTTP_201_CREATED)
         self.assertEqual(self.model.user, user)
         self.assertEqual(self.model.activity, self.activity)
+        self.assertEqual(registration.status, 'stopped')
+        self.assertEqual(participant.status, 'succeeded')
 
     def test_accepted_registration_still_blocks_periodic_waiting_list(self):
         user = BlueBottleUserFactory.create()
@@ -360,9 +336,6 @@ class SchedulePreviousSlotInterestAPITestCase(APITestCase):
     """
     Schedule activities with slot history should allow waiting list signup once
     the registration is no longer active.
-
-    The blocking test documents the current bug; the expectedFailure test
-    captures the desired behaviour after the validator is fixed.
     """
 
     url_name = 'interest-list'
@@ -414,26 +387,8 @@ class SchedulePreviousSlotInterestAPITestCase(APITestCase):
         self.activity.status = 'full'
         self.activity.save()
 
-    def test_succeeded_previous_slot_participation_blocks_waiting_list(self):
+    def test_former_slot_participant_can_join_waiting_list(self):
         user, registration, participant = self._create_former_slot_participant()
-        self._fill_activity()
-
-        self.perform_create(user=user)
-
-        self.assertStatus(status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            self.response.json()['errors'][0]['code'],
-            'already_involved',
-        )
-        self.assertFalse(
-            Interest.objects.filter(user=user, activity=self.activity).exists()
-        )
-        self.assertEqual(registration.status, 'withdrawn')
-        self.assertEqual(participant.status, 'succeeded')
-
-    @expectedFailure
-    def test_former_slot_participant_should_be_able_to_join_waiting_list(self):
-        user, _registration, _participant = self._create_former_slot_participant()
         self._fill_activity()
 
         self.perform_create(user=user)
@@ -441,6 +396,8 @@ class SchedulePreviousSlotInterestAPITestCase(APITestCase):
         self.assertStatus(status.HTTP_201_CREATED)
         self.assertEqual(self.model.user, user)
         self.assertEqual(self.model.activity, self.activity)
+        self.assertEqual(registration.status, 'withdrawn')
+        self.assertEqual(participant.status, 'succeeded')
 
     def test_accepted_registration_still_blocks_schedule_waiting_list(self):
         user = BlueBottleUserFactory.create()
